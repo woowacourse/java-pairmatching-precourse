6aa2570bfc76a32792ab44220bad774d8843a241
gracefulBrown <brown@woowahan.com>
2021-12-18 08:22:53 +0900

    readme

--- src/main/java/pairmatching/Application.java
+++ src/main/java/pairmatching/Application.java
@@ -1,3 +1,5 @@
+package pairmatching;
+
 public class Application {
     public static void main(String[] args) {
         // TODO 구현 진행
--- src/test/java/pairmatching/ApplicationTest.java
+++ src/test/java/pairmatching/ApplicationTest.java
@@ -1,3 +1,5 @@
+package pairmatching;
+
 import static camp.nextstep.edu.missionutils.test.Assertions.assertShuffleTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -5,6 +7,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import java.util.Arrays;
 import org.junit.jupiter.api.Test;
+import pairmatching.Application;
 
 class ApplicationTest extends NsTest {
 
@@ -14,7 +17,7 @@ class ApplicationTest extends NsTest {
     void 짝수_인원_페어_매칭() {
         assertShuffleTest(
             () -> {
-                run("1", "백엔드, 레벨1, 자동차경주", "q");
+                run("1", "백엔드, 레벨1, 자동차경주", "Q");
                 assertThat(output()).contains("태웅 : 백호", "치수 : 태섭");
             },
             Arrays.asList("태웅", "백호", "치수", "태섭")
@@ -25,7 +28,7 @@ class ApplicationTest extends NsTest {
     void 없는_미션에_대한_예외_처리() {
         assertSimpleTest(
             () -> {
-                run("1", "백엔드, 레벨1, 오징어게임");
+                runException("1", "백엔드, 레벨1, 오징어게임");
                 assertThat(output()).contains(ERROR_MESSAGE);
             }
         );
