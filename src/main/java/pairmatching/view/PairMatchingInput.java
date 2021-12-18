af788491137a557edffc840c1d85daabf70e1a2d
zmfl1230 <zmfl123097@gmail.com>
2021-12-18 23:29:03 +0900

    feat[PairMatchingInput-View]: input for rematch

--- src/main/java/pairmatching/view/PairMatchingInput.java
+++ src/main/java/pairmatching/view/PairMatchingInput.java
@@ -1,16 +1,25 @@
 package pairmatching.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import pairmatching.utils.CommonConstant;
 import pairmatching.utils.Validator;
 
 public class PairMatchingInput{
 
+
     public static String[] choiceMatchingInformation() {
-        PairMatchingOutput.matchInstruction();
+        PairMatchingOutput.choiceInstruction();
         String information = Console.readLine();
         Validator.validateInformationFormat(information);
         String[] matchingInformation = information.split(",");
         Validator.validateInformationContent(matchingInformation);
         return matchingInformation;
+    }
+
+    public static boolean inputForRematch() {
+        PairMatchingOutput.rematchInstruction();
+        String rematch = Console.readLine();
+        Validator.validateRematchFormat(rematch);
+        return CommonConstant.YES.equals(rematch);
     }
 }
