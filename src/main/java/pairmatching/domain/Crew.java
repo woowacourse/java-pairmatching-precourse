d2f69425e12bc07e00a8fc4fc5faca1e25b4a95a
Donggyu Kim <a29661498@gmail.com>
2021-12-18 14:19:19 +0900

    feat: Crew 클래스 Getter 정의
    Getter를 추가해주었습니다.

--- src/main/java/pairmatching/domain/Crew.java
+++ src/main/java/pairmatching/domain/Crew.java
@@ -8,4 +8,12 @@ public class Crew {
         this.course = course;
         this.name = name;
     }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
