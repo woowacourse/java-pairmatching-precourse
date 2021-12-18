da914e4f8faf4dd6986cd483f93ed376e869bab2
zmfl1230 <zmfl123097@gmail.com>
2021-12-18 17:49:41 +0900

    feat[PairMatchingController-Controller]: define methods - 기능 입력 받기 - 매칭하기 - 조회하기 - 초기화하기

---
+++ src/main/java/pairmatching/controller/PairMatchingController.java
@@ -0,0 +1,29 @@
+package pairmatching.controller;
+import pairmatching.domain.Pair;
+import pairmatching.domain.PairMatching;
+import pairmatching.view.PairMatchingInput;
+import pairmatching.view.PairMatchingOutput;
+
+import java.io.IOException;
+
+public class PairMatchingController {
+    private PairMatching pairMatching = new PairMatching();
+
+    public int inputFunction() {
+        return 0;
+    }
+
+    public void match() throws IOException {
+        String[] information = PairMatchingInput.choiceMatchingInformation();
+        Pair pair = pairMatching.addPairInPairListOfLevel(information[0], information[1], information[2]);
+        PairMatchingOutput.printPair(pair);
+    }
+
+    public void search() {
+
+    }
+
+    public void initialize() {
+
+    }
+}
