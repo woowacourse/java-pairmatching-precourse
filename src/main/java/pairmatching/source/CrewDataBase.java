940aac96f74288b8460882d29e5ad31e126c2fa9
overnew <ehdls1303@gmail.com>
2021-12-18 14:59:52 +0900

    feat: CrewDataBase class 구현
    레벨 별로 크루들의 객체를 저장하는 클래스
    
    필드
    HashMap<String, Crew> backEndCrewMap
    HashMap<String, Crew> frontEndCrewMap
    
    각 값은 ReadCrewFile를 통해 이름을 읽어와 초기화
     - BackEndInit(), FrontEndInit()

---
+++ src/main/java/pairmatching/source/CrewDataBase.java
@@ -0,0 +1,32 @@
+package pairmatching.source;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class CrewDataBase {
+    public HashMap<String, Crew> backEndCrewMap = new HashMap<>();
+    public HashMap<String, Crew> frontEndCrewMap = new HashMap<>();
+
+    public CrewDataBase(){
+        BackEndInit();
+        FrontEndInit();
+    }
+
+    private void BackEndInit(){
+        List<String> crewNames = ReadCrewFile.getBackEndCrewNames();
+
+        for(String name : crewNames){
+            backEndCrewMap.put(name ,new Crew( Course.BACKEND, name));
+        }
+    }
+
+    private void FrontEndInit(){
+        List<String> crewNames = ReadCrewFile.getFrontEndCrewNames();
+
+        for(String name : crewNames){
+            frontEndCrewMap.put(name ,new Crew( Course.FRONTEND, name));
+        }
+    }
+
+
+}
