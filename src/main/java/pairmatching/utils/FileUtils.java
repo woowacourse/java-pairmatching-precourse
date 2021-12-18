84b5351d09256456fa5006068a47cc5ca504bed1
Kim Seunglae <obey1342@gmail.com>
2021-12-18 14:13:07 +0900

    refactor: IOException부분 IllegalArgumentException으로 변경해서 처리

--- src/main/java/pairmatching/utils/FileUtils.java
+++ src/main/java/pairmatching/utils/FileUtils.java
@@ -2,6 +2,7 @@ package pairmatching.utils;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -12,19 +13,41 @@ public class FileUtils {
 	private FileUtils() {
 	}
 
-	public static List<String> readList(String filePath) throws IOException {
+	public static List<String> readList(String filePath) {
 		File file = new File(filePath);
 		List<String> crewList = new ArrayList<>();
 
 		if (file.exists()) {
-			BufferedReader reader = new BufferedReader(new FileReader(file));
+			BufferedReader reader = null;
+			reader = fineFile(file);
 			String line = null;
 
-			while ((line = reader.readLine()) != null) {
-				crewList.add(line);
-			}
+			extracted(crewList, reader);
 		}
 
 		return Collections.unmodifiableList(crewList);
+	}
+
+	private static BufferedReader fineFile(File file) {
+		BufferedReader reader;
+		try {
+			reader = new BufferedReader(new FileReader(file));
+		} catch (FileNotFoundException e) {
+			throw new IllegalArgumentException("[ERROR] 해당 파일을 찾을 수 없습니다.");
+		}
+		return reader;
+	}
+
+	private static void extracted(List<String> crewList, BufferedReader reader) {
+		String line;
+		while (true) {
+			try {
+				if ((line = reader.readLine()) == null)
+					break;
+			} catch (IOException e) {
+				throw new IllegalArgumentException("[ERROR] 파일 읽는게 잘못되었습니다.");
+			}
+			crewList.add(line);
+		}
 	}
 }
