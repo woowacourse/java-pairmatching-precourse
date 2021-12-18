fcb21dfd62f5a52f5833eeae7b3947916620f181
swcho <progress0407@gmail.com>
2021-12-18 15:13:58 +0900

    test(InputValidatorTest): 서브 메뉴 입력 테스트코드 작성

--- src/test/java/pairmatching/validator/InputValidatorTest.java
+++ src/test/java/pairmatching/validator/InputValidatorTest.java
@@ -2,17 +2,28 @@ package pairmatching.validator;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
+import static pairmatching.validator.InputValidator.*;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class InputValidatorTest {
 
 	@Test
 	void 집입_메뉴_입력_예외() {
-		assertThatThrownBy(() -> {
-			InputValidator.validateEntryMenu("4");
-		})
+		assertThatThrownBy(() -> InputValidator.validateEntryMenu("4"))
 			.isInstanceOf(IllegalArgumentException.class)
-			.hasMessageContaining(InputValidator.ERROR_PREFIX);
+			.hasMessageContaining(InputValidator.ERROR_PREFIX)
+			.hasMessage(INVALID_ENTRY_MENU_CHOICE);
+	}
+
+	@ParameterizedTest
+	@ValueSource(strings = {"프론트엔드, 레벨1", "프론트엔드, 레벨1, 자동차경주,"})
+	void 서브_메뉴_입력_예외() {
+		assertThatThrownBy(() -> InputValidator.validateSubMenu(""))
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessageContaining(InputValidator.ERROR_PREFIX)
+			.hasMessage(INVALID_SUB_MENU_CHOICE);
 	}
 }
