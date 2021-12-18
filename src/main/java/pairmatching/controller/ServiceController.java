d945639d734895160b486e77da5344ed2492d65c
pup-paw <wldusdl0310@gmail.com>
2021-12-18 16:28:18 +0900

    feat: 1번 메뉴인 페어 매칭 기능 추가

--- src/main/java/pairmatching/controller/ServiceController.java
+++ src/main/java/pairmatching/controller/ServiceController.java
@@ -1,16 +1,57 @@
 package pairmatching.controller;
 
+import java.util.ArrayList;
+
+import pairmatching.model.Level;
+import pairmatching.model.Mission;
 import pairmatching.view.InputView;
 import pairmatching.view.OutputView;
 
 public class ServiceController {
+	public static ArrayList<Mission> missions;
+	static String choice;
+	static String[] matchingInfo;
 	public static void run() {
-		String choice = askFunction();
-
+		generateMission();
+		askFunction();
+		doMatching();
 	}
 
-	private static String askFunction() {
+	private static void doMatching() {
+		askMatchingInfo();
+		MatchingController.doMatching(matchingInfo);
+		OutputView.showMatchingResult();
+	}
+
+	private static void generateMission() {
+		missions = new ArrayList<>();
+		missions.add(new Mission(Level.LEVEL1, "자동차경주"));
+		missions.add(new Mission(Level.LEVEL1, "로또"));
+		missions.add(new Mission(Level.LEVEL1, "숫자야구게임"));
+		missions.add(new Mission(Level.LEVEL2, "장바구니"));
+		missions.add(new Mission(Level.LEVEL2, "결제"));
+		missions.add(new Mission(Level.LEVEL2, "지하철노선도"));
+		missions.add(new Mission(Level.LEVEL4, "성능개선"));
+		missions.add(new Mission(Level.LEVEL4, "배포"));
+	}
+
+	private static void askFunction() {
 		OutputView.askFunction();
-		return InputView.askFunction();
+		try {
+			choice = InputView.askFunction();
+		} catch (IllegalArgumentException e) {
+			System.out.println(e.getMessage());
+			askFunction();
+		}
+	}
+
+	private static void askMatchingInfo() {
+		OutputView.askMatchingInfo();
+		try {
+			matchingInfo = InputView.askMatchingInfo();
+		} catch (IllegalArgumentException e) {
+			System.out.println(e.getMessage());
+			askMatchingInfo();
+		}
 	}
 }
