1a0a339f14b64aa42ce9d0873efadfca331fd377
Seongwon97 <osw970325@naver.com>
2021-12-18 17:46:05 +0900

    refactor : 매직 넘버를 상수로 변경

--- src/main/java/pairmatching/view/OutputViews.java
+++ src/main/java/pairmatching/view/OutputViews.java
@@ -5,22 +5,29 @@ import pairmatching.domain.*;
 import java.util.List;
 
 public class OutputViews {
+    public static final String MISSION_INFO_FRONT_BOX = "\n#############################################";
+    public static final String MISSION_INFO_BACK_BOX = "#############################################";
+    public static final String LEVEL_FRONT = "  - ";
+    public static final String LEVEL_BACK = ": ";
+    public static final String MISSION_SEPORATOR = " | ";
+    public static final String MATCHING_SEPORATOR = " : ";
+
     public static void printMissionInfo(Missions missions) {
-        System.out.println("\n#############################################");
+        System.out.println(MISSION_INFO_FRONT_BOX);
         System.out.print("과정: ");
-        System.out.println(String.join(" | ", Course.getList()));
+        System.out.println(String.join(MISSION_SEPORATOR, Course.getList()));
         for (Level level : Level.getList()) {
-            System.out.print("  - " + level.getName() + ": ");
-            System.out.println(String.join(" | ", missions.getValue(level)));
+            System.out.print(LEVEL_FRONT + level.getName() + LEVEL_BACK);
+            System.out.println(String.join(MISSION_SEPORATOR, missions.getValue(level)));
         }
-        System.out.println("#############################################");
+        System.out.println(MISSION_INFO_BACK_BOX);
     }
 
     public static void printMatchPair(MatchInfo matchInfo) {
         System.out.println("\n페어 매칭 결과입니다.");
         for (List<Crew> pair : matchInfo.getPairInfo()) {
             for (int i = 0; i < pair.size() - 1; i++) {
-                System.out.print(pair.get(i) + " : ");
+                System.out.print(pair.get(i) + MATCHING_SEPORATOR);
             }
             System.out.println(pair.get(pair.size() - 1));
         }
