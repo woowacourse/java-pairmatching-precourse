4fbd980dca681f63e487f6f93bfa8955271b338a
KIM <km0192@naver.com>
2021-12-18 14:24:10 +0900

    feat(pairStorage) : 페어를 이뤘던 리스트 생성, 반환 하도록 변경

--- src/main/java/pairmatching/PairStorage.java
+++ src/main/java/pairmatching/PairStorage.java
@@ -1,2 +1,31 @@
-package pairmatching;public class PairStroage {
+package pairmatching;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class PairStorage {
+	private final HashMap<Crew, HashMap<Level, HashSet<Crew>>> pairMap = new HashMap<>();
+
+	public PairStorage() {
+		setPairMapLevel();
+	}
+
+	private void setPairMapLevel() {
+		for (Crew crew : new Crews().getCrewList()) {
+			HashMap<Level, HashSet<Crew>> levelMap = setLevelMapWithCrew(crew);
+			pairMap.put(crew,levelMap);
+		}
+	}
+
+	private HashMap<Level, HashSet<Crew>>  setLevelMapWithCrew(Crew crew) {
+		HashMap<Level, HashSet<Crew>> individualLevelMap = new HashMap<>();
+		for (Level level : Level.values()) {
+			individualLevelMap.put(level, new HashSet<Crew>());
+		}
+		return individualLevelMap;
+	}
+
+	public HashMap<Crew, HashMap<Level, HashSet<Crew>>> getPairMap() {
+		return pairMap;
+	}
 }
