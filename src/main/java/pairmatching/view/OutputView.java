1a2900a5602dadc048d378b48af26b4de90e1fd0
Ohzzi <clover7even@naver.com>
2021-12-18 17:38:22 +0900

    feat: 매칭 결과 출력
    매칭이 끝나고 나면 매칭 결과를 출력하는 기능 추가

--- src/main/java/pairmatching/view/OutputView.java
+++ src/main/java/pairmatching/view/OutputView.java
@@ -1,8 +1,17 @@
 package pairmatching.view;
+import java.util.List;
+
+import pairmatching.domain.pair.Pair;
 
 public class OutputView {
+	private static final String PRINT_PAIRS = "페어 매칭 결과입니다.";
 
 	public static void printError(String errorMessage) {
 		System.out.println(errorMessage);
+	}
+
+	public static void printPairs(List<Pair> pairs) {
+		System.out.println(PRINT_PAIRS);
+		pairs.forEach(pair -> System.out.println(pair.toString()));
 	}
 }
