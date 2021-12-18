30dcd746aae4e0b40443b6f21ed75ac69e1f1606
KIM <km0192@naver.com>
2021-12-18 13:19:16 +0900

    feat(Mission) : 사전 제공정보 그대로 미션목록 작성, 이름/레벨 반환하는 toString, getLevel 메소드

--- src/main/java/pairmatching/Mission.java
+++ src/main/java/pairmatching/Mission.java
@@ -1,2 +1,29 @@
-package pairmatching;public class Mission {
+package pairmatching;
+
+public enum Mission {
+	RACING_CAR("자동차경주", Level.LEVEL1),
+	LOTTO("로또", Level.LEVEL1),
+	BASEBALL("숫자야구게임", Level.LEVEL1),
+	SHOPPING_CART("장바구니", Level.LEVEL2),
+	PAYMENT("결제", Level.LEVEL2),
+	SUBWAY_MAP("지하철노선도", Level.LEVEL2),
+	PERFORMANCE_DEVELOP("성능개선", Level.LEVEL4),
+	DISTRIBUTION("배포", Level.LEVEL4);
+
+	private String name;
+	private Level level;
+
+	Mission(String name, Level level) {
+		this.name = name;
+		this.level = level;
+	}
+
+	@Override
+	public String toString() {
+		return this.name;
+	}
+
+	public Level getLevel() {
+		return this.level;
+	}
 }
