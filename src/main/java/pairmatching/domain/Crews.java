b56e3307b82341bdfe46e118c2bba049feeec355
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2021-12-18 13:25:14 +0900

    feat: Crews 초기화 메소드 추가

---
+++ src/main/java/pairmatching/domain/Crews.java
@@ -0,0 +1,17 @@
+package pairmatching.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Crews {
+
+    private final List<Crew> crews;
+
+    public Crews(List<Crew> crews) {
+        this.crews = crews;
+    }
+
+    public static Crews init() {
+        return new Crews(new ArrayList<>());
+    }
+}
