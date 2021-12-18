b87110be809508d8ea0c06f42da2e62ac621105b
yxxnghwan <younghwan960@gmail.com>
2021-12-18 16:34:42 +0900

    fix: 없는 미션을 검사하는 부분을 통과 못하던 오류 수정

--- src/main/java/pairmatching/util/MissionInfoValidator.java
+++ src/main/java/pairmatching/util/MissionInfoValidator.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class MissionInfoValidator {
 	private static final Integer COURSE_INDEX = 0;
 	private static final Integer LEVEL_INDEX = 1;
-	private static final Integer MISSION_INDEX = 0;
+	private static final Integer MISSION_INDEX = 2;
 
 	public static List<String> validate(String inputString) {
 		inputString = removeBlank(inputString);
