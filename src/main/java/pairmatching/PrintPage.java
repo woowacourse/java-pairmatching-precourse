dcc9b8789444832ff69fc9583e19f1ecfa607340
soulfever <soulfever01@naver.com>
2021-12-18 13:32:28 +0900

    FEAT : 페어매칭 안내 출력 기능

--- src/main/java/pairmatching/PrintPage.java
+++ src/main/java/pairmatching/PrintPage.java
@@ -10,4 +10,15 @@ public class PrintPage {
         Utils.InputStartMenu();
     }
 
+    public static void PairMatching() {
+        System.out.println("#############################################");
+        System.out.println("과정: 백엔드 | 프론트엔드");
+        System.out.println("미션:");
+        System.out.println("- 레벨1: 자동차경주 | 로또 | 숫자야구게임");
+        System.out.println("- 레벨2: 장바구니 | 결제 | 지하철노선도");
+        System.out.println("- 레벨3:");
+        System.out.println("- 레벨4: 성능개선 | 배포");
+        System.out.println("- 레벨5: ");
+        System.out.println("#############################################");
+    }
 }
