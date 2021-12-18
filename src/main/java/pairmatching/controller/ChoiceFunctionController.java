4d45e6af1c31b6bf3d69e9258df327f2584c2034
wooseok <diddmlvkf@gmail.com>
2021-12-18 17:34:38 +0900

    feat(InputValidator) : 기능 선택에서 잘못된 입력을 검증

--- src/main/java/pairmatching/controller/ChoiceFunctionController.java
+++ src/main/java/pairmatching/controller/ChoiceFunctionController.java
@@ -1,10 +1,18 @@
 package pairmatching.controller;
 
+import pairmatching.validator.InputValidator;
+import pairmatching.view.ExceptionView;
 import pairmatching.view.InputView;
 
 public class ChoiceFunctionController {
 	public static String choiceFunction() {
-		String input = InputView.choiceFunction();
-		return input;
+		try {
+			String input = InputView.choiceFunction();
+			InputValidator.containFunctionChoiceList(input);
+			return input;
+		} catch (IllegalArgumentException e) {
+			ExceptionView.exceptionUI(e.getMessage());
+			return choiceFunction();
+		}
 	}
 }
--- src/main/java/pairmatching/validator/InputValidator.java
+++ src/main/java/pairmatching/validator/InputValidator.java
@@ -1,11 +1,26 @@
 package pairmatching.validator;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class InputValidator {
 	private static final String IS_SIZE_THREE_ERROR_MESSAGE = "입력 형식을 맞춰야 합니다.";
+	private static final String CONTAIN_FUNCTION_CHOICE_LIST_ERROR_MESSAGE = "기능 선택 번호를 입력해주세요.";
+
+	private static final List<String> FUNCTION_CHOICE_LIST = Arrays.asList(
+		"1", "2", "3", "Q"
+	);
+
 
 	public static void isSizeThree(String[] stringArray) throws IllegalArgumentException {
 		if (stringArray.length != 3) {
 			throw new IllegalArgumentException(IS_SIZE_THREE_ERROR_MESSAGE);
+		}
+	}
+
+	public static void containFunctionChoiceList(String input) throws IllegalArgumentException {
+		if (!FUNCTION_CHOICE_LIST.contains(input)) {
+			throw new IllegalArgumentException(CONTAIN_FUNCTION_CHOICE_LIST_ERROR_MESSAGE);
 		}
 	}
 }
