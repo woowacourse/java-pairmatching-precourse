62c690f50ce23b22612d251908dde83ba65e4597
yeongunheo <gjduddns@gmail.com>
2021-12-18 14:35:08 +0900

    feat: 크루 목록의 순서를 랜덤으로 섞는 기능 구현

--- src/main/java/pairmatching/controller/PairMatchingController.java
+++ src/main/java/pairmatching/controller/PairMatchingController.java
@@ -11,11 +11,15 @@ import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import pairmatching.View.InputView;
 import pairmatching.domain.Crew;
 
 public class PairMatchingController {
 	private static final int ONE = 1;
+	private static final String DELIMITER = ",";
+	private static final String FRONTEND_COURSE = "프론트엔드";
+	private static final String BACKEND_COURSE = "백엔드";
 
 	private final InputView inputView;
 
@@ -29,15 +33,32 @@ public class PairMatchingController {
 
 	public void matchFunction(int functionNumber) {
 		if (functionNumber == ONE) {
-			inputView.scanCourseAndLevelAndMission();
+			String[] courseAndLevelAndMission = inputView.scanCourseAndLevelAndMission().trim().split(DELIMITER);
+			String course = courseAndLevelAndMission[0];
+			if (course.equals(FRONTEND_COURSE)) {
+				frontendMatchPair(course);
+			}
+			if (course.equals(BACKEND_COURSE)) {
+				backendMatchPair(course);
+			}
 		}
 	}
 
-	public void matchPair() {
+	public void frontendMatchPair(String course) {
+		List<String> crewNames = readFrontendCrewNames();
+		List<String> shuffledCrew = Randoms.shuffle(crewNames);
+
+		// 페어 매칭 & 검증
 
 	}
 
+	public void backendMatchPair(String course) {
+		List<String> crewNames = readBackendCrewNames();
+		List<String> shuffledCrew = Randoms.shuffle(crewNames);
 
+		// 페어 매칭 & 검증
+
+	}
 
 	public List<String> readBackendCrewNames() {
 		List<String> backendCrewNames = new ArrayList<>();
