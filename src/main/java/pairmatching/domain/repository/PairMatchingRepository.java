2dce3ad7bc96c4310b4a824be9e0205d1cace0dc
jochungbeom <cndqjacndqja@gmail.com>
2021-12-18 15:05:10 +0900

    feat: 매칭 결과 가져오기 구현

--- src/main/java/pairmatching/domain/repository/PairMatchingRepository.java
+++ src/main/java/pairmatching/domain/repository/PairMatchingRepository.java
@@ -5,8 +5,6 @@ import static pairmatching.domain.repository.CrewRepository.*;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.sun.glass.ui.Window;
-
 import camp.nextstep.edu.missionutils.Randoms;
 import pairmatching.domain.Course;
 import pairmatching.domain.Crew;
@@ -16,14 +14,21 @@ import pairmatching.domain.Mission;
 import pairmatching.domain.PairMatching;
 
 public class PairMatchingRepository {
-	List<PairMatching> pairMatchingList;
+	static List<PairMatching> pairMatchingList;
 
-	public void add(String course, String level, String mission) {
+	public static void add(String course, String level, String mission) {
 		List<MatchingCrew> crewList = createMatchingCrews(course);
 		pairMatchingList.add(new PairMatching(Course.find(course), new Mission(Level.find(level), mission), crewList));
 	}
 
-	public List<MatchingCrew> createMatchingCrews(String course) {
+	public static PairMatching find(String course, String level, String mission) {
+		return pairMatchingList.stream()
+			.filter(pairMatching -> pairMatching.isSame(course, level, mission))
+			.findFirst()
+			.orElseThrow(() -> new IllegalArgumentException("올바른 페어 매칭 값이 아닙니다."));
+	}
+
+	public static List<MatchingCrew> createMatchingCrews(String course) {
 		List<MatchingCrew> matchingCrewList =  new ArrayList<>();
 		List<Crew> crewList = Randoms.shuffle(getCrewList(course));
 		if ((crewList.size() % 2) == 0) {
@@ -41,7 +46,7 @@ public class PairMatchingRepository {
 	}
 
 
-	private List<Crew> getCrewList(String course) {
+	private static List<Crew> getCrewList(String course) {
 		if (course.equals("백엔드")) {
 			return getBackEndCrewList();
 		}
