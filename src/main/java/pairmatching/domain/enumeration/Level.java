6b410eae6c4cc5c27e4de5a77ee4f5d0471ee889
jayjaehunchoigit config --global user.name jayjaehunchoigit config --global user.name jayjaehunchoi <wogns0108@naver.com>
2021-12-18 17:43:02 +0900

    fix(test) : unchecked, unsafe operations 문제 해결

--- src/main/java/pairmatching/domain/enumeration/Level.java
+++ src/main/java/pairmatching/domain/enumeration/Level.java
@@ -2,6 +2,7 @@ package pairmatching.domain.enumeration;
 
 import pairmatching.utils.ConstantMessages;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -9,9 +10,9 @@ import java.util.List;
 public enum Level {
     LEVEL1("레벨1", Arrays.asList(Mission.CAR_RACING, Mission.LOTTO, Mission.NUMBER_BASEBALL)),
     LEVEL2("레벨2", Arrays.asList(Mission.SHOPPING_CART, Mission.PAYMENT, Mission.SUBWAY_PATH)),
-    LEVEL3("레벨3", Collections.EMPTY_LIST),
+    LEVEL3("레벨3", new ArrayList<>()),
     LEVEL4("레벨4", Arrays.asList(Mission.PERFORMANCE_IMPROVEMENT, Mission.DEPLOYMENT)),
-    LEVEL5("레벨5", Collections.EMPTY_LIST);
+    LEVEL5("레벨5", new ArrayList<>());
 
     private String name;
     private List<Mission> missionList;
--- src/test/java/pairmatching/ApplicationTest.java
+++ src/test/java/pairmatching/ApplicationTest.java
@@ -5,7 +5,11 @@ import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import pairmatching.Application;
 import pairmatching.domain.Crew;
@@ -17,16 +21,22 @@ class ApplicationTest extends NsTest {
 
     @Test
     void 짝수_인원_페어_매칭() {
+        List<Crew> crews = new ArrayList<>();
+        addCrews(crews);
         assertShuffleTest(
             () -> {
                 run("1", "백엔드, 레벨1, 자동차경주", "Q");
                 assertThat(output()).contains("태웅 : 백호", "치수 : 태섭");
             },
-            Arrays.asList(new Crew(Course.BACKEND,"태웅"),
-                    new Crew(Course.BACKEND,"백호"),
-                    new Crew(Course.BACKEND,"치수"),
-                    new Crew(Course.BACKEND,"태섭"))
+            crews
         );
+    }
+
+    private void addCrews(List<Crew> crews) {
+        crews.add(new Crew(Course.BACKEND,"태웅"));
+        crews.add(new Crew(Course.BACKEND,"백호"));
+        crews.add(new Crew(Course.BACKEND,"치수"));
+        crews.add(new Crew(Course.BACKEND,"태섭"));
     }
 
     @Test
