cbb1071bc98a5c61b9dd408ef70e1ae29c91a7ba
hyeonic <dev.hyeonic@gmail.com>
2021-12-18 16:04:58 +0900

    feat: PairMathingMachine PairMission 관리를 위한 인스턴스 변수 추가

--- src/main/java/pairmatching/domain/PairMatchingMachine.java
+++ src/main/java/pairmatching/domain/PairMatchingMachine.java
@@ -4,12 +4,23 @@ import java.util.List;
 
 public class PairMatchingMachine {
     private final RandomPairMatchingGenerator randomPairMatchingGenerator;
+    private final List<PairMission> pairMissions;
 
-    public PairMatchingMachine(RandomPairMatchingGenerator randomPairMatchingGenerator) {
+    public PairMatchingMachine(RandomPairMatchingGenerator randomPairMatchingGenerator,
+        List<PairMission> pairMissions) {
         this.randomPairMatchingGenerator = randomPairMatchingGenerator;
+        this.pairMissions = pairMissions;
     }
 
     public PairInformation share(PairMission pairMission, List<Crew> crews) {
         return randomPairMatchingGenerator.generate(pairMission, crews);
+    }
+
+    public boolean isPairMission(PairMission pairMission) {
+        return pairMissions.contains(pairMission);
+    }
+
+    public void addPairMission(PairMission pairMission) {
+        this.pairMissions.add(pairMission);
     }
 }
