89d70964bc455323b00bc93b6dfdd25fa50c6817
최창준 <aowert6518@gmail.com>
2021-12-18 14:10:12 +0900

    [refactor] 함수 분리 및 함수명 수정
    - makeFilePath(String processName) : 과정에 따라 파일 명을 리턴하는 기능
    
    - match(String processName) : 과정을 입력받으면 해당 크루파일을 읽고 셔플하는 기능 (추가적으로 매칭까지 진행)

--- src/main/java/pairmatching/domain/MatchingService.java
+++ src/main/java/pairmatching/domain/MatchingService.java
@@ -1,5 +1,7 @@
 package pairmatching.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -12,14 +14,12 @@ import static pairmatching.constant.SystemMessage.FRONT_END_PROCESS_FILE_PATH;
 
 public class MatchingService {
     List<String> crewList = new ArrayList<>();
+    List<String> shuffledCrewList;
 
-    public void readCrewListBy(String processName) throws IOException {
-        if (processName.equals("백엔드")) {
-            readCrewList(BACK_END_PROCESS_FILE_PATH);
-        }
-        if (processName.equals("프론트엔드")) {
-            readCrewList(FRONT_END_PROCESS_FILE_PATH);
-        }
+    public void match(String processName) throws IOException {
+        String filePath = makeFilePath(processName);
+        readCrewList(filePath);
+        shuffleCrewList();
     }
 
     private void readCrewList(String filePath) throws IOException {
@@ -30,5 +30,16 @@ public class MatchingService {
         while ((crewName = reader.readLine()) != null) {
             crewList.add(crewName);
         }
+    }
+
+    private void shuffleCrewList() {
+        shuffledCrewList = Randoms.shuffle(crewList);
+    }
+
+    private String makeFilePath(String processName) {
+        if (processName.equals("백엔드")) {
+            return BACK_END_PROCESS_FILE_PATH;
+        }
+        return FRONT_END_PROCESS_FILE_PATH;
     }
 }
