aadabc9696cdf4085e3f0a34798c4d4d8f061730
sure-why-not <pkse1213@gmail.com>
2021-12-18 15:00:46 +0900

    feat: 기능 메뉴 입력받기 구현

--- src/main/java/pairmatching/controller/PairMatchingController.java
+++ src/main/java/pairmatching/controller/PairMatchingController.java
@@ -1,10 +1,17 @@
 package pairmatching.controller;
 
 import pairmatching.domain.CrewMaker;
+import pairmatching.view.InputView;
 
 public class PairMatchingController {
 
 	public void run() {
 		CrewMaker.run();
+		String menu = getFunctionMenuInput();
 	}
+
+	private String getFunctionMenuInput() {
+		return InputView.getFunctionMenuInput();
+	}
+
 }
---
+++ src/main/java/pairmatching/view/InputView.java
@@ -0,0 +1,14 @@
+package pairmatching.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+	public static String getFunctionMenuInput() {
+		System.out.println("기능을 선택하세요.\n"
+			+ "1. 페어 매칭\n"
+			+ "2. 페어 조회\n"
+			+ "3. 페어 초기화\n"
+			+ "Q. 종료");
+		return Console.readLine();
+	}
+}
