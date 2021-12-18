3c66253312fe0156b7cf519b6189d4c28b2c7dc7
minsulee <alkwen0996@naver.com>
2021-12-18 15:16:21 +0900

    feat: 프론트엔드 크루 목록 생성 기능 추가

--- src/main/java/pairmatching/controller/PairMatchingController.java
+++ src/main/java/pairmatching/controller/PairMatchingController.java
@@ -1,6 +1,8 @@
 package pairmatching.controller;
 
 
+import java.util.List;
+
 import pairmatching.view.InputView;
 
 public abstract class PairMatchingController {
@@ -8,6 +10,7 @@ public abstract class PairMatchingController {
     public static void run() {
         String selectFunction = InputView.inputPairMatchingFunction();
         String[] courseInformation = InputView.inputCourseInformation();
+        List<String> FrontendCrewNames = InputView.readFrontendCrew();
     }
 
 }
--- src/main/java/pairmatching/view/InputView.java
+++ src/main/java/pairmatching/view/InputView.java
@@ -1,5 +1,9 @@
 package pairmatching.view;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import camp.nextstep.edu.missionutils.Console;
@@ -11,9 +15,34 @@ public abstract class InputView {
     private static final String NEW_LINE = "\n";
     private static final String ERROR_MESSAGE_NOT_CONTAINS_IN_FUNCTION_LIST = "[ERROR] 기능목록에 있는 기능을 선택해 주세요.";
     private static final String COMMA = ",";
+    private static final String BOUNDARY = "#############################################";
+    private static final String COLON = ":";
+    private static final String MISSION = "미션";
+    private static final String DASH = "-";
+    private static final String SPACE = " ";
+    private static final String VERTICAL_LINE = "|";
+
+    public static List<String> readFrontendCrew() {
+        try {
+            final List<String> crewNames = new ArrayList<>();
+            BufferedReader bufferedReader = new BufferedReader(new FileReader("./src/main/resources/frontend-crew.md"));
+
+            String line = null;
+            while ((line = bufferedReader.readLine()) != null) {
+                crewNames.add(line);
+            }
+
+            return crewNames;
+        } catch (IOException e) {
+            e.printStackTrace();
+
+            return readFrontendCrew();
+        }
+    }
 
     public static String[] inputCourseInformation() {
         try {
+//            System.out.println(createCourseInformationPrintFormat());
             String inputCourseInformation = inputValue();
 
             return inputCourseInformation.split(COMMA);
@@ -23,6 +52,17 @@ public abstract class InputView {
             return inputCourseInformation();
         }
     }
+//
+//    private static boolean createCourseInformationPrintFormat() {
+//        final StringBuilder stringBuilder = new StringBuilder();
+//        stringBuilder.append(BOUNDARY).append(NEW_LINE).append(MISSION).append(COLON).append(NEW_LINE)
+//                .append(DASH).append(SPACE).append(Levels.LEVEL1.getName()).append(Missions.CAR_RACING).append(SPACE).append(VERTICAL_LINE).append(Missions.LOTTO).append(Missions.NUMBER_BASEBALL_GAME)
+//
+//                .append(BOUNDARY).append(NEW_LINE);
+//
+//
+//        return stringBuilder.toString();
+//    }
 
 
     public static String inputPairMatchingFunction() {
