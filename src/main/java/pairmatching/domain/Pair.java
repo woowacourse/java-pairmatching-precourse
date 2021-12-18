67ddfcc9f1604f44662d3e35280c1c28d640ca93
Hongdonggeon <ghd700@naver.com>
2021-12-18 15:14:43 +0900

    feat(pair) : 미션과 팀원의 리스트를 가지고 있는 pair 클래스

---
+++ src/main/java/pairmatching/domain/Pair.java
@@ -0,0 +1,33 @@
+package pairmatching.domain;
+
+import java.util.List;
+import java.util.Objects;
+
+public class Pair {
+	private Mission mission;
+	private List<Crew> crews;
+
+	public Pair(Mission mission, List<Crew> crews) {
+		this.mission = mission;
+		this.crews = crews;
+	}
+
+	public List<Crew> getCrews() {
+		return crews;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		Pair pair = (Pair)o;
+		return Objects.equals(mission, pair.mission);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(mission);
+	}
+}
---
+++ src/main/java/pairmatching/view/OutputView.java
@@ -0,0 +1,2 @@
+package pairmatching.view;public class OutputView {
+}
