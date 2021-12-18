1a22766160ed34ece6b1077319198c252e02e4c0
bcc0830 <bcc0830@naver.com>
2021-12-18 17:32:19 +0900

    feat : 출력 기능 추가

---
+++ src/main/java/pairmatching/view/OutputView.java
@@ -0,0 +1,33 @@
+package pairmatching.view;
+
+public class OutputView {
+
+	public static void menuOutput() {
+		System.out.print("기능을 선택하세요.\n"
+			+ "1. 페어 매칭\n"
+			+ "2. 페어 조회\n"
+			+ "3. 페어 초기화\n"
+			+ "Q. 종료");
+	}
+
+	public static void MatchingOutput() {
+		System.out.print("#############################################\n"
+			+ "과정: 백엔드 | 프론트엔드\n"
+			+ "미션:\n"
+			+ "  - 레벨1: 자동차경주 | 로또 | 숫자야구게임\n"
+			+ "  - 레벨2: 장바구니 | 결제 | 지하철노선도\n"
+			+ "  - 레벨3: \n"
+			+ "  - 레벨4: 성능개선 | 배포\n"
+			+ "  - 레벨5: \n"
+			+ "############################################\n"
+		    + "과정, 레벨, 미션을 선택하세요.\nex) 백엔드, 레벨1, 자동차경주");
+	}
+
+	public static void ReMatchOutput() {
+		System.out.print("매칭 정보가 있습니다. 다시 매칭하시겠습니까?\n");
+	}
+
+	public static void InitOutput() {
+		System.out.print("초기화 되었습니다.\n");
+	}
+}
