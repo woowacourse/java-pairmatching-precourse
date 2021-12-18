63ed7a744b9f83410f9bdf6d61d9ded51f7d1033
erie0210 <eriee0210@gmail.com>
2021-12-18 14:29:15 +0900

    feat(detail input): 3,4 구체적인 선택 질문과 응답 기능 구현

--- src/main/java/pairmatching/controller/MatchingController.java
+++ src/main/java/pairmatching/controller/MatchingController.java
@@ -1,7 +1,12 @@
 package pairmatching.controller;
 
 import jdk.internal.util.xml.impl.Input;
+import pairmatching.utils.InputMessage;
 import pairmatching.view.InputView;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MatchingController {
     
@@ -9,15 +14,34 @@ public class MatchingController {
         //  기능 선택 질문하기
         InputView.askCaegory();
         // 선택지 입력받기
-        InputView.getCaegory();
-
-
-
-        // 선택지 내용 출력하기
-        // 선택지 내용 입력받기
+        String categoryAnswer = InputView.getCaegory();
+        //answer에 맞는 구체적인 질문 내용 가져오기
+        getDetail(categoryAnswer);
     }
 
 
+    public void getDetail (String categoryAnswer) {
+        if(categoryAnswer.equals("1")){
+            //  구체적인 내용 질문
+            InputView.askDetail();
+            String detailAnswer = InputView.getDetail();
+            System.out.println(detailAnswer);
+            String[] detailArray = detailAnswer.split(",");
+            List<String> detailList = new ArrayList<String>(Arrays.asList(detailArray));
+            // 현재 매칭 정보가 있는 지확인 하고 없으면 생성
+
+            // 있으면 매칭 정보 있다고 알려주고 다시 매칭
+        }
+        if(categoryAnswer.equals("2")){
+            // 현재 있는 매칭 출력
+            
+        }
+        if(categoryAnswer.equals("3")){
+            
+        }
+        // 그외에 프로그램 종료
+        return;
+    }
 
 
 
