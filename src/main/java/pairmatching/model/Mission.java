8cfb0eb2b569d3c97403f0c24b9c1c4b01aec5e6
Kim Seunglae <obey1342@gmail.com>
2021-12-18 17:14:42 +0900

    feat: 과정,레벨,미션 선택하는 기능 추가

--- src/main/java/pairmatching/model/Mission.java
+++ src/main/java/pairmatching/model/Mission.java
@@ -1,12 +1,28 @@
 package pairmatching.model;
 
 public class Mission {
+	private Course course;
 	private Level level;
 	private String missionName;
 
 	public Mission(Level level, String missionName) {
 		this.level = level;
 		this.missionName = missionName;
+	}
+
+	public Mission(String[] split) {
+		this(Course.getCourseType(split[0]), Level.getLevelType(split[1]),
+			MissionRepository.findByName(split[2]));
+	}
+
+	public Mission(Course course, Level level, String missionName) {
+		this.course = course;
+		this.level = level;
+		this.missionName = missionName;
+	}
+
+	public Course getCourse() {
+		return course;
 	}
 
 	public Level getLevel() {
--- src/main/java/pairmatching/view/InputView.java
+++ src/main/java/pairmatching/view/InputView.java
@@ -1,6 +1,7 @@
 package pairmatching.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import pairmatching.model.Mission;
 
 public class InputView {
 	private InputView() {
@@ -10,4 +11,16 @@ public class InputView {
 		OutputView.askSelectFunction();
 		return Console.readLine();
 	}
+
+	public static Mission askSelectOption() {
+		try {
+			OutputView.askSelectOption();
+			return new Mission(Console.readLine().split(","));
+		} catch (IllegalArgumentException e) {
+			System.out.println(e.getMessage());
+			return askSelectOption();
+		}
+
+	}
+
 }
