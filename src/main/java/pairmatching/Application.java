c2c9c76d9245b33b760126b63843548df48ef003
Hyunta <mohai2618@gmail.com>
2021-12-18 14:26:16 +0900

    feat : 레벨별 미션을 초기값으로 설정하는 기능 구현

--- src/main/java/pairmatching/Application.java
+++ src/main/java/pairmatching/Application.java
@@ -1,9 +1,11 @@
 package pairmatching;
 
 import pairmatching.controller.MainController;
+import pairmatching.utils.Initializer;
 
 public class Application {
     public static void main(String[] args) {
+        Initializer.init();
         MainController mainController = new MainController();
         mainController.run();
     }
--- src/main/java/pairmatching/utils/Initializer.java
+++ src/main/java/pairmatching/utils/Initializer.java
@@ -1,14 +1,19 @@
 package pairmatching.utils;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
 import java.util.List;
 
 import pairmatching.domain.Course;
+import pairmatching.domain.Level;
 import pairmatching.repository.CourseRepository;
+import pairmatching.repository.MissionRepository;
 
 public class Initializer {
 	public static void init() {
 		initCourse();
+		initMission();
 	}
 
 	private static void initCourse() {
@@ -19,4 +24,13 @@ public class Initializer {
 		CourseRepository.setCourses(courses);
 	}
 
+	private static void initMission() {
+		LinkedHashMap<Level, List<String>> mission = new LinkedHashMap<>();
+		mission.put(Level.LEVEL1, Arrays.asList("자동차경주", "로또", "숫자야구게임"));
+		mission.put(Level.LEVEL2, Arrays.asList("장바구니", "결제", "지하철노선도"));
+		mission.put(Level.LEVEL3, Arrays.asList());
+		mission.put(Level.LEVEL4, Arrays.asList("성능 개선", "배포"));
+		mission.put(Level.LEVEL5, Arrays.asList());
+		MissionRepository.setMission(mission);
+	}
 }
