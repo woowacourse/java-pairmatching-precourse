f4d958b2c76325550882c7aa4e4cf65189ece1b4
jayjaehunchoigit config --global user.name jayjaehunchoigit config --global user.name jayjaehunchoi <wogns0108@naver.com>
2021-12-18 14:43:32 +0900

    feat(InputValidator) : 입력값에 대한 검증 1. enum 하나로 처리하기 어려운 입력값 2. enum으로 생성하지 않은 않은 네, 아니오 입력값

---
+++ src/main/java/pairmatching/utils/validator/InputValidator.java
@@ -0,0 +1,45 @@
+package pairmatching.utils.validator;
+
+import pairmatching.domain.enumeration.Course;
+import pairmatching.domain.enumeration.Level;
+import pairmatching.domain.enumeration.Mission;
+import pairmatching.utils.ErrorMessages;
+
+public class InputValidator {
+    private final static int COURSE_INDEX = 0;
+    private final static int LEVEL_INDEX = 1;
+    private final static int MISSION_INDEX = 2;
+    private final static int SPACING = 1;
+
+    private final static String YES = "네";
+    private final static String NO = "아니오";
+
+    public static void validateCourseLevelMissionInput(String[] input) {
+        String courseName = input[COURSE_INDEX];
+        String level = input[LEVEL_INDEX].substring(SPACING);
+        String mission = input[MISSION_INDEX].substring(SPACING);
+
+        Course.findCourseByInput(courseName);
+        Level levelByInput = Level.findLevelByInput(level);
+        Level missionLevel = validateMission(mission);
+
+        validateLevelAndMissionLevel(levelByInput, missionLevel);
+    }
+
+    public static void validateYesOrNo(String input) {
+        if(!input.equals(YES) && !input.equals(NO)) {
+            throw new IllegalArgumentException(ErrorMessages.PREFIX_ERROR + ErrorMessages.ERROR_INVALID_INPUT);
+        }
+    }
+
+    private static void validateLevelAndMissionLevel(Level levelByInput, Level missionLevel) {
+        if(levelByInput != missionLevel) {
+            throw new IllegalArgumentException(ErrorMessages.PREFIX_ERROR + ErrorMessages.ERROR_INVALID_INPUT);
+        }
+    }
+
+    private static Level validateMission(String mission) {
+        Mission missionByInput = Mission.findMissionByInput(mission);
+        return Level.findLevelByMission(missionByInput);
+    }
+}
