c8fc66b3086749a075d34ad224ad3c58dd90fedc
YJ <soltwood6862@naver.com>
2021-12-18 17:47:42 +0900

    feat : Leve enum 클래스 생성

--- src/main/java/pairmatching/Application.java
+++ src/main/java/pairmatching/Application.java
@@ -1,7 +1,25 @@
 package pairmatching;
 
+import pairmatching.Controller.Controller;
+
+
 public class Application {
     public static void main(String[] args) {
         // TODO 구현 진행
+        /*
+        String names = "백호 태웅 치수 태섭 대만 준호 대협 덕규 태산 경태 수겸 현준 준섭 한나 소연 호열 대남 용팔 구식 달재";
+        List<String> crewNames = new ArrayList<String>(); // 파일에서 로드한 크루 이름 목록
+
+        String[] splitNames = names.split(" ");
+        for(int i = 0; i < splitNames.length; i++) {
+            crewNames.add(splitNames[i]);
+        }
+        System.out.println(crewNames);
+
+        List<String> shuffledCrew = Randoms.shuffle(crewNames); // 섞인 크루 이름 목록
+        System.out.println(shuffledCrew);
+
+        // 백호 태웅 치수 태섭 대만 준호 대협 덕규 태산 경태 수겸 현준 준섭 한나 소연 호열 대남 용팔 구식 달재
+        */
     }
 }
--- src/main/java/pairmatching/Level.java
+++ src/main/java/pairmatching/Level.java
@@ -1,2 +1,38 @@
-package pairmatching;public class Level {
+package pairmatching;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public enum Level {
+    LEVEL1("레벨1", Arrays.asList("자동차경주", "로또", "숫자야구게임")),
+    LEVEL2("레벨2", Arrays.asList("장바구니", "결제", "지하철노선도")),
+    LEVEL3("레벨3", Collections.emptyList()),
+    LEVEL4("레벨4", Arrays.asList("성능개선", "배포")),
+    LEVEL5("레벨5", Collections.emptyList()),
+    NOTHING("없음", Collections.emptyList());
+
+    private String name;
+    private List<String> missionList;
+
+    Level(String name, List<String> missionList) {
+        this.name = name;
+        this.missionList = missionList;
+    }
+
+    public static Level findBymissionCode(String code){
+        return
+                //PayGroup의 Enum 상수들을 순회하며
+                Arrays.stream(Level.values())
+                        //payCode를 갖고 있는게 있는지 확인합니다.
+                        .filter(payGroup -> payGroup.hasPayCode(code))
+                        .findAny()
+                        .orElse(NOTHING);
+    }
+
+    public boolean hasPayCode(String code){
+        return missionList.stream()
+                .anyMatch(pay -> pay.equals(code));
+    }
+    // 추가 기능 구현
 }
