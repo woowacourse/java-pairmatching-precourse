ddeaebae21e4a8f6e1df31942c17896216306807
leokim <kunga077@gmail.com>
2021-12-18 15:05:03 +0900

    :sparkles: feat: 페어 기능 선택 및 수행
    - 페어 매칭, 페어 조회, 페어 초기화, 종료 중 하나를 선택한다.
    - 종료가 선택되면 페어 매칭을 중단한다.
    - 종료가 아니면 입력된 기능에 맞게 기능을 수행한다.

---
+++ src/main/java/pairmatching/Matching.java
@@ -0,0 +1,16 @@
+package pairmatching;
+
+public class Matching {
+
+    public void startMatching() {
+
+    }
+
+    public void printPairs() {
+
+    }
+
+    public void removePairs() {
+
+    }
+}
--- src/main/java/pairmatching/Pair.java
+++ src/main/java/pairmatching/Pair.java
@@ -1,5 +1,11 @@
 package pairmatching;
 
+import static pairmatching.Constant.INITIALIZATION_PAIRS_FUNCTION;
+import static pairmatching.Constant.MATCHING_FUNCTION;
+import static pairmatching.Constant.QUIT_MESSAGE;
+import static pairmatching.Constant.READ_PAIRS_FUNCTION;
+
+import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
 
 public class Pair {
@@ -12,6 +18,37 @@ public class Pair {
     }
 
     public void startPairing() {
+        Matching matching = new Matching();
+        while (true) {
+            String function = selectFunction();
+            if (checkQuit(function)) {
+                break;
+            }
+            performFunction(matching, function);
+        }
+    }
 
+    private String selectFunction() {
+        System.out.println("기능을 선택하세요.");
+        System.out.println("1. 페어 매칭\n2. 페어 조회\n3. 페어 초기화\nQ. 종료");
+        return Console.readLine();
+    }
+
+    private boolean checkQuit(String function) {
+        return function.equals(QUIT_MESSAGE);
+    }
+
+    private void performFunction(Matching matching, String function) {
+        if (function.equals(MATCHING_FUNCTION)) {
+            matching.startMatching();
+            return;
+        }
+        if (function.equals(READ_PAIRS_FUNCTION)) {
+            matching.printPairs();
+            return;
+        }
+        if (function.equals(INITIALIZATION_PAIRS_FUNCTION)) {
+            matching.removePairs();
+        }
     }
 }
