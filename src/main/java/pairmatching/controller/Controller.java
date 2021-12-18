9ad27124f72045daa7b041a6aa3e6196c002bffa
jojogreen91 <iawbg13@gmail.com>
2021-12-18 17:18:15 +0900

    feat: 같은 레벨에서 이미 매칭된적이 있는 경우를 판결하는 기능 구현

--- src/main/java/pairmatching/controller/Controller.java
+++ src/main/java/pairmatching/controller/Controller.java
@@ -1,5 +1,6 @@
 package pairmatching.controller;
 
+import pairmatching.model.PairMatching;
 import pairmatching.model.receiver.PairMatchingReceiver;
 import pairmatching.model.receiver.MainMenuReceiver;
 import pairmatching.model.receiver.RePairMatchingAskReceiver;
@@ -26,33 +27,40 @@ public class Controller {
 		if (mainMenuInput.equals("1")) {
 			pairMatching();
 		}
+
+		if (mainMenuInput.equals("Q")) {
+		}
 	}
 
 	public void pairMatching() {
 		view.courseLevelMissionInfo();
 
+		String pairMatchingInput = "";
+
 		boolean rewindSwitch = true;
 		while (rewindSwitch) {
 			view.selectCourseLevelMission();
 
-			String pairMatchingInput = pairMatchingReceiver.receive();
+			pairMatchingInput = pairMatchingReceiver.receive();
 
 			if (pairService.isAlreadyPairMatching(pairMatchingInput)) {
 				view.rePairMatching();
-
 				String answer = rePairMatchingAskReceiver.receive();
 				if (answer.equals("아니오")) {
 					continue;
 				}
 				if (answer.equals("네")) {
+					PairMatching pairMatching = pairService.findPairMatching(pairMatchingInput);
+					pairMatching.getPairList().clear();
 					rewindSwitch = false;
 				}
 			}
 
 			pairService.makePairMatching(pairMatchingInput);
-			break;
 		}
 
 		view.pairMatchingResult();
+
+		mainMenu();
 	}
 }
--- src/main/java/pairmatching/model/service/PairService.java
+++ src/main/java/pairmatching/model/service/PairService.java
@@ -2,6 +2,7 @@ package pairmatching.model.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import pairmatching.model.CrewList;
@@ -63,6 +64,8 @@ public class PairService {
 				pairMatching.setPairList(makePairList(frontCrewList));
 			}
 
+			///
+
 			PairMatchingRepository.repo.add(pairMatching);
 		}
 	}
@@ -78,6 +81,8 @@ public class PairService {
 				pairMatching.setPairList(makePairList(frontCrewList));
 			}
 
+			///
+
 			PairMatchingRepository.repo.add(pairMatching);
 		}
 	}
@@ -85,5 +90,82 @@ public class PairService {
 	private ArrayList<String> makePairList(ArrayList<String> crewList) {
 		List<String> shuffleList = Randoms.shuffle(crewList);
 		return (ArrayList<String>)shuffleList;
+	}
+
+	public PairMatching findPairMatching(String pairMatchingInput) {
+		String[] split = pairMatchingInput.split(",");
+
+		for (PairMatching pairMatching : PairMatchingRepository.repo) {
+			if (split.length == 3) {
+				if (pairMatching.getCourse().equals(split[0].trim()) && pairMatching.getLevel().equals(split[1].trim())
+					&& pairMatching.getMission().equals(split[2].trim())) {
+					return pairMatching;
+				}
+			}
+
+			if (split.length == 2) {
+				if (pairMatching.getCourse().equals(split[0].trim()) && pairMatching.getLevel()
+					.equals(split[1].trim())) {
+					return pairMatching;
+				}
+			}
+		}
+
+		return new PairMatching("error", "error", "error");
+	}
+
+	public boolean isSamePairInSameLevel(PairMatching pairMatching) {
+		String course = pairMatching.getCourse();
+		String level = pairMatching.getLevel();
+		List<ArrayList<String>> sameLevelPairListList = PairMatchingRepository.repo.stream()
+			.filter(repo -> repo.getLevel().equals(level)).filter(repo -> repo.getCourse().equals(course))
+			.map(repo -> repo.getPairList())
+			.collect(Collectors.toList());
+
+		ArrayList<String> pairList = pairMatching.getPairList();
+
+		if (pairList.size() % 2 == 0) {
+			for (ArrayList<String> sameLevelPairList : sameLevelPairListList) {
+				for (int i = 0; i < pairList.size(); i += 2) {
+					String name = pairList.get(i);
+					String pairName = pairList.get(i + 1);
+
+					for (int j = 0; j < sameLevelPairList.size(); j++) {
+						if (sameLevelPairList.get(i).equals(name)) {
+							if (i % 2 == 0 && sameLevelPairList.get(i + 1).equals(pairName)) {
+								return true;
+							}
+
+							if (i % 2 != 0 && sameLevelPairList.get(i - 1).equals(pairName)) {
+								return true;
+							}
+						}
+					}
+				}
+			}
+		}
+
+		// if (pairList.size() % 2 != 0) {
+		// 	for (ArrayList<String> sameLevelPairList : sameLevelPairListList) {
+		// 		for (int i = 0; i < pairList.size(); i += 2) {
+		// 			String name = pairList.get(i);
+		// 			String pairName = pairList.get(i + 1);
+		//
+		// 			for (int j = 0; j < sameLevelPairList.size(); j++) {
+		// 				if (sameLevelPairList.get(i).equals(name)) {
+		// 					if (i % 2 == 0 && sameLevelPairList.get(i + 1).equals(pairName)) {
+		// 						return true;
+		// 					}
+		//
+		// 					if (i % 2 != 0 && sameLevelPairList.get(i - 1).equals(pairName)) {
+		// 						return true;
+		// 					}
+		// 				}
+		// 			}
+		// 		}
+		// 	}
+		// }
+
+		return false;
 	}
 }
