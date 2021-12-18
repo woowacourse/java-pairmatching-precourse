52fabddf98f094b6fccc867e62c0ce70e5ec3800
pup-paw <wldusdl0310@gmail.com>
2021-12-18 16:22:17 +0900

    feat: 프로젝트 내에서 사용되는 파일 주소 저장

---
+++ src/main/java/pairmatching/util/Constant.java
@@ -0,0 +1,6 @@
+package pairmatching.util;
+
+public class Constant {
+	public final static String BACKEND_CREW_FILE_PATH = "src/main/resources/backend-crew.md";
+	public final static String FRONTEND_CREW_FILE_PATH = "src/main/resources/frontend-crew.md";
+}
