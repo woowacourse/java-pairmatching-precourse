30ff2d4545c234dcf9d0ce218fdb87ff6994676a
pup-paw <wldusdl0310@gmail.com>
2021-12-18 17:33:44 +0900

    style: 컨벤션에 맞게 스타일 조정

--- src/test/java/pairmatching/ApplicationTest.java
+++ src/test/java/pairmatching/ApplicationTest.java
@@ -5,37 +5,40 @@ import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+
 import java.util.Arrays;
+
 import org.junit.jupiter.api.Test;
+
 import pairmatching.Application;
 
 class ApplicationTest extends NsTest {
 
-    private static final String ERROR_MESSAGE = "[ERROR]";
+	private static final String ERROR_MESSAGE = "[ERROR]";
 
-    @Test
-    void 짝수_인원_페어_매칭() {
-        assertShuffleTest(
-            () -> {
-                run("1", "백엔드, 레벨1, 자동차경주", "Q");
-                assertThat(output()).contains("태웅 : 백호", "치수 : 태섭");
-            },
-            Arrays.asList("태웅", "백호", "치수", "태섭")
-        );
-    }
+	@Test
+	void 짝수_인원_페어_매칭() {
+		assertShuffleTest(
+			() -> {
+				run("1", "백엔드, 레벨1, 자동차경주", "Q");
+				assertThat(output()).contains("태웅 : 백호", "치수 : 태섭");
+			},
+			Arrays.asList("태웅", "백호", "치수", "태섭")
+		);
+	}
 
-    @Test
-    void 없는_미션에_대한_예외_처리() {
-        assertSimpleTest(
-            () -> {
-                runException("1", "백엔드, 레벨1, 오징어게임");
-                assertThat(output()).contains(ERROR_MESSAGE);
-            }
-        );
-    }
+	@Test
+	void 없는_미션에_대한_예외_처리() {
+		assertSimpleTest(
+			() -> {
+				runException("1", "백엔드, 레벨1, 오징어게임");
+				assertThat(output()).contains(ERROR_MESSAGE);
+			}
+		);
+	}
 
-    @Override
-    public void runMain() {
-        Application.main(new String[]{});
-    }
+	@Override
+	public void runMain() {
+		Application.main(new String[] {});
+	}
 }
