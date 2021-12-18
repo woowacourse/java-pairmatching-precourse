bdb454afee78c8f91d7ba5ae6abc63e2bd1ad3f3
overnew <ehdls1303@gmail.com>
2021-12-18 16:26:33 +0900

    feat: FunctionInputChecker class 구현
    올바른 기능 심볼을 입력받아 반환, 예외처리
    
    String getFunctionInput()
     - 올바른 입력을 받을 떄까지 반복 입력 요청

---
+++ src/main/java/pairmatching/utils/FunctionInputChecker.java
@@ -0,0 +1,47 @@
+package pairmatching.utils;
+
+import camp.nextstep.edu.missionutils.Console;
+import pairmatching.constants.StringConstant;
+
+public class FunctionInputChecker {
+    private static final String FUNCTION_INPUT_ERROR = "올바른 기능을 선택해 주세요";
+    private static final String FUNCTION_INPUT_REQUEST = "기능을 선택하세요.\n" +
+            "1. 페어 매칭\n" +
+            "2. 페어 조회\n" +
+            "3. 페어 초기화\n" +
+            "Q. 종료";
+
+
+    public static String getFunctionInput(){
+        String input;
+        do{
+            System.out.println(FUNCTION_INPUT_REQUEST);
+            input = Console.readLine();
+        }while(checkIsRightInput(input));
+
+        return input;
+    }
+
+    private static boolean checkIsRightInput(String input){
+
+        try {
+            checkExistFunction(input);
+        }catch (IllegalArgumentException exception){
+            System.out.println(exception.getMessage());
+            return false;
+        }
+
+        return true;
+    }
+
+    private static void checkExistFunction(String input) throws IllegalArgumentException{
+
+        for(String symbol : StringConstant.FUNCTION_SYMBOL){
+            if(symbol == input){
+                return;
+            }
+        }
+
+        throw new IllegalArgumentException(StringConstant.ERROR_MESSAGE_PREFIX + FUNCTION_INPUT_ERROR);
+    }
+}
