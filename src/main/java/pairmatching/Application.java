dd6c635868ef0052f859bb905f375d030183917b
BeomsooKim <kbsat@naver.com>
2021-12-18 13:45:11 +0900

    feat: 이름 입력 기능 구현

--- src/main/java/pairmatching/Application.java
+++ src/main/java/pairmatching/Application.java
@@ -1,7 +1,29 @@
 package pairmatching;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO 구현 진행
-    }
+	public static void main(String[] args) {
+		// 백엔드 크루 획득
+		List<String> backCrewNames = getCrewName("backend-crew.md");
+		List<String> frontCrewNames = getCrewName("frontend-crew.md");
+
+	}
+
+	private static List<String> getCrewName(String fileName) {
+		List<String> backCrewNames = null;
+		URL resource = Application.class.getClassLoader().getResource(fileName);
+		Path path = new File(resource.getPath()).toPath();
+		try {
+			backCrewNames = Files.readAllLines(path);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return backCrewNames;
+	}
 }
