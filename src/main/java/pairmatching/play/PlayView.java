b47ce9a52d261ce32c2b0d28835d3e595deb3eb4
hwihwi99 <chamgrace@naver.com>
2021-12-18 15:26:20 +0900

    feat : 입력받은 숫자 확인하고 조건에 맞게 임시실행

--- src/main/java/pairmatching/play/PlayView.java
+++ src/main/java/pairmatching/play/PlayView.java
@@ -11,6 +11,26 @@ public class PlayView {
         System.out.println("3. 페어 초기화");
         System.out.println("Q. 종료");
         String input = checkInputNumAndPlay();
+        System.out.println(input);
+        playPairProgrammming(input);
+    }
+
+    public void playPairProgrammming(String input) {
+        if(input.equals("1")) {
+            game1();
+            playView();
+        }
+        if(input.equals("2")) {
+            game2();
+            playView();
+        }
+        if(input.equals("3")) {
+            game3();
+            playView();
+        }
+        if(input.equals("Q")) {
+            return;
+        }
     }
 
     public String chooseMenu() {
@@ -18,13 +38,29 @@ public class PlayView {
     }
 
     public String checkInputNumAndPlay() {
-        String input = chooseMenu();
+        String input = "";
         try {
+            input = chooseMenu();
             Validation.checkInputForm(input);
         }catch (IllegalArgumentException e) {
             System.out.println("[ERROR] 1,2,3,Q 중 하나를 입력해주세요");
-            checkInputNumAndPlay();
+            input = checkInputNumAndPlay();
         }
         return input;
+    }
+
+    public void game1() {
+        System.out.println("1번게임 시작");
+        System.out.println();
+    }
+
+    public void game2() {
+        System.out.println("2번게임 시작");
+        System.out.println();
+    }
+
+    public void game3() {
+        System.out.println("3번게임 시작");
+        System.out.println();
     }
 }
