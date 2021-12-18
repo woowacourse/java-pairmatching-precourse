019f2ab058a571237207eccc13ebe8bf8dceffb6
Chan Kim <chankim@Chanui-MacBookPro.local>
2021-12-18 14:26:03 +0900

    docs(README.md) : 구현 기능 목록 수정

---
+++ src/main/java/pairmatching/utils/Constant.java
@@ -0,0 +1,21 @@
+package pairmatching;
+
+public class Constant {
+	public static final String SELECT_FUNCTION = "기능을 선택하세요.";
+	public static final String PAIR_MATCHING = "1. 페어 매칭";
+	public static final String PAIR_SEARCHING = "2. 페어 조회";
+	public static final String PAIR_INITIALIZING = "3. 페어 초기화";
+	public static final String QUIT = "Q. 종료";
+	public static final String ERROR = "[ERROR] ";
+
+	public static final String BOUNDARY = "#############################################";
+	public static final String COURSE = "과정: 백엔드 | 프론트엔드";
+	public static final String MISSION = "미션:";
+	public static final String LEVEL1 = "  - 레벨1: 자동차경주 | 로또 | 숫자야구게임";
+	public static final String LEVEL2 = "  - 레벨2: 장바구니 | 결제 | 지하철노선도";
+	public static final String LEVEL3 = "  - 레벨3:";
+	public static final String LEVEL4 = "  - 레벨4: 성능개선 | 배포";
+	public static final String LEVEL5 = "  - 레벨5:";
+	public static final String SELECT_COURSE_LEVEL_MISSION = "과정, 레벨, 미션을 선택하세요.";
+	public static final String EXAMPELE = "ex) 백엔드, 레벨1, 자동차경주";
+}
