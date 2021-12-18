9a3f0ad52dd27b33fb005f6c39266ab4aebfe61e
harinn1014 <gkfla7555@gmail.com>
2021-12-18 17:52:27 +0900

    feat(ExceptionMessage): 누락된 메시지 추가
    <description>
    누락된 에러메시지 추가

--- src/main/java/pairmatching/utils/ExceptionMessage.java
+++ src/main/java/pairmatching/utils/ExceptionMessage.java
@@ -9,6 +9,8 @@ public class ExceptionMessage {
 	public static final String INPUT_COURSE = ERROR + "과정의 이름이 잘못되었습니다.";
 	public static final String INPUT_LEVEL = ERROR + "레벨의 이름이 잘못되었습니다.";
 	public static final String INPUT_MISSION = ERROR + "미션의 이름이 잘못되었습니다.";
+	
+	public static final String INPUT_ANSWER = ERROR + "네 또는 아니오로 대답해주세요.";
 
 	// 매칭
 	public static final String MATCING_FAIL = ERROR + "매칭에 실패하였습니다."; // 3회 시도까지 매칭x, 경우의 수 없음
