0e49f8234616473a4a48b219ca538d226bfa912f
minsulee <alkwen0996@naver.com>
2021-12-18 14:05:42 +0900

    feat: 프로그램 기능 선택 메시지 작성 기능 추가

--- src/main/java/pairmatching/view/InputView.java
+++ src/main/java/pairmatching/view/InputView.java
@@ -4,33 +4,49 @@ import camp.nextstep.edu.missionutils.Console;
 import pairmatching.model.ProgramFunction;
 
 public class InputView {
+
+    private static final String INPUT_MESSAGE_CHOOSE_FUNCTION = "기능을 선택하세요.";
+    private static final String NEW_LINE = "\n";
     private static final String ERROR_MESSAGE_NOT_CONTAINS_IN_FUNCTION_LIST = "[ERROR] 기능목록에 있는 기능을 선택해 주세요.";
 
     public String inputPairMatchingFunction() {
         try {
-
             String inputFunctionSignal = inputValue();
+            String printFormatProvidingFunction = createProvidingFunctionPrintFormat();
+
             validateInputFunctionSignal(inputFunctionSignal);
 
+
             return inputFunctionSignal;
-        }catch (IllegalArgumentException illegalArgumentException){
+        } catch (IllegalArgumentException illegalArgumentException) {
             System.out.println(illegalArgumentException.getMessage());
 
             return inputPairMatchingFunction();
         }
     }
 
+    private String createProvidingFunctionPrintFormat() {
+        final StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(INPUT_MESSAGE_CHOOSE_FUNCTION + NEW_LINE);
+
+        for (ProgramFunction programFunction : ProgramFunction.values()) {
+            stringBuilder.append(programFunction.getSignal()).append(NEW_LINE);
+        }
+
+        return stringBuilder.toString();
+    }
+
     private void validateInputFunctionSignal(final String inputFunctionSignal) {
         boolean isInputFunctionSignal = true;
 
-        for (ProgramFunction programFunction:ProgramFunction.values()) {
+        for (ProgramFunction programFunction : ProgramFunction.values()) {
             if (programFunction.getSignal().contains(inputFunctionSignal)) {
                 isInputFunctionSignal = false;
                 break;
             }
         }
 
-        if(isInputFunctionSignal){
+        if (isInputFunctionSignal) {
             throw new IllegalArgumentException(ERROR_MESSAGE_NOT_CONTAINS_IN_FUNCTION_LIST);
         }
     }
