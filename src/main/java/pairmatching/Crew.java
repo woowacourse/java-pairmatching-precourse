f514c99ad97a3c7905156aeb8ec9d8d997f42e9e
PanghoChao <Sobrightlf@naver.com>
2021-12-18 16:27:07 +0900

    feat(Crew) : 매칭된크루와 같이인스턴스화

--- src/main/java/pairmatching/Crew.java
+++ src/main/java/pairmatching/Crew.java
@@ -3,6 +3,16 @@ package pairmatching;
 public class Crew {
     private Course course;
     private String name;
+    private String matchCrewName;
+    private String finalCrewName;
     
-   
+    Crew(String name, String matchCrewName){
+    	this.name =name;
+    	this.matchCrewName= matchCrewName;
+    }
+    Crew(String name, String matchCrewName, String finalCString){
+    	this.name =name;
+    	this.matchCrewName= matchCrewName;
+    	this.finalCrewName = finalCString;
+    }
 }
