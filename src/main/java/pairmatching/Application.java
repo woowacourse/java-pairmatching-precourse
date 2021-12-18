e2f250a146f6f58009e0425a86546c768ce2e169
pup-paw <wldusdl0310@gmail.com>
2021-12-18 17:33:21 +0900

    feat: 페어 매칭 프로그램 구동을 위한 코드 추가

--- src/main/java/pairmatching/Application.java
+++ src/main/java/pairmatching/Application.java
@@ -1,7 +1,9 @@
 package pairmatching;
 
+import pairmatching.controller.ServiceController;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO 구현 진행
-    }
+	public static void main(String[] args) {
+		ServiceController.run();
+	}
 }
