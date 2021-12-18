5b210620a39b6cd117a557fdc5c179c338437a04
YuJeong <yjfflq4872@gmail.com>
2021-12-18 17:58:27 +0900

    style: 코드 format 수정

--- src/main/java/pairmatching/Application.java
+++ src/main/java/pairmatching/Application.java
@@ -1,17 +1,10 @@
 package pairmatching;
 
-import java.util.Arrays;
-
 import pairmatching.controller.PairMatchingController;
-import pairmatching.domain.Course;
-import pairmatching.domain.Level;
-import pairmatching.domain.PairResult;
-import pairmatching.domain.PairResults;
-import pairmatching.utill.CrewFileReader;
 
 public class Application {
-    public static void main(String[] args) {
-        PairMatchingController pairMatchingController = new PairMatchingController();
-        pairMatchingController.runPairMatching();
-    }
+	public static void main(String[] args) {
+		PairMatchingController pairMatchingController = new PairMatchingController();
+		pairMatchingController.runPairMatching();
+	}
 }
--- src/main/java/pairmatching/domain/PairResults.java
+++ src/main/java/pairmatching/domain/PairResults.java
@@ -1,8 +1,6 @@
 package pairmatching.domain;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class PairResults {
