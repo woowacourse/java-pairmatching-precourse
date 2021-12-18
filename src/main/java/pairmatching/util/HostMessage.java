c21b89d6724c7b0ce6bfd49cffb1a93c76530616
pup-paw <wldusdl0310@gmail.com>
2021-12-18 16:26:15 +0900

    feat: 매칭 기능에서 사용자에게 제공되는 메세지 작성

--- src/main/java/pairmatching/util/HostMessage.java
+++ src/main/java/pairmatching/util/HostMessage.java
@@ -7,5 +7,12 @@ public class HostMessage {
 	public static final String FUNCTION_3 = "3. 페어 초기화";
 	public static final String FUNCTION_Q = "Q. 종료";
 	public static final String LINE_SEPARATOR = "#############################################";
-	public static
+	public static final String SHOW_COURSE = "과정: ";
+	public static final String SHOW_MISSION = "미션: ";
+	public static final String[] MISSION_NAMES = {
+		": 자동차경주 | 로또 | 숫자야구게임", ": 장바구니 | 결제 | 지하철노선도", ":", ": 성능개선 | 배포", ":"
+	};
+	public static final String ASK_MATCHING_INFO = "과정, 레벨, 미션을 선택하세요.";
+	public static final String SHOW_MATCHING_INFO_EX = "ex) 백엔드, 레벨1, 자동차경주";
+	public static final String SHOW_MATCHING_RESULT = "페어 매칭 결과입니다.";
 }
