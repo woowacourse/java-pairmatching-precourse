a7f4cd4dda331ba4da87a2c2283ec63ce3cb3c9b
jayjaehunchoigit config --global user.name jayjaehunchoigit config --global user.name jayjaehunchoi <wogns0108@naver.com>
2021-12-18 14:58:59 +0900

    feat(Pairs) : pair를 담는 일급 컬렉션 생성

---
+++ src/main/java/pairmatching/domain/enumeration/Pairs.java
@@ -0,0 +1,23 @@
+package pairmatching.domain.enumeration;
+
+import pairmatching.domain.Pair;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Pairs {
+    private final static int FIRST_INDEX = 0;
+
+    private List<Pair> pairList = new ArrayList<>();
+
+    public void addPair(Pair pair) {
+        pairList.add(pair);
+    }
+
+    public void removeAll() {
+        int size = pairList.size();
+        for (int i = 0; i < size; i++) {
+            pairList.remove(FIRST_INDEX);
+        }
+    }
+}
