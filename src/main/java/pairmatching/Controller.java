ed0ad653bae303ded26703a7c7b5abb48ceeddb4
DWL21 <nggus5@naver.com>
2021-12-18 15:19:26 +0900

    feat: 페어 매칭을 선택한 경우 입력받고 올바른 입력인지 확인하는 기능

--- src/main/java/pairmatching/Controller.java
+++ src/main/java/pairmatching/Controller.java
@@ -1,5 +1,7 @@
 package pairmatching;
 
+import java.util.ArrayList;
+
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
@@ -7,22 +9,27 @@ public class Controller {
     Crews crews = new Crews();
 
     String input;
+    String[] courses;
 
     public void start() {
         crews.readFrontend();
         crews.readBackend();
+        program();
     }
 
     private void program() {
-
+        selectFunctions();
     }
 
     public void selectFunctions() {
-        System.out.println(Message.INPUT_FUNCTIONS);
         boolean check = false;
         while (!check) {
+            System.out.println(Message.INPUT_FUNCTIONS);
             input();
             check = inputFunctions();
+        }
+        if (input.charAt(0) == '1') {
+            selectCourses();
         }
 
     }
@@ -34,14 +41,14 @@ public class Controller {
                 throw new IllegalArgumentException();
             }
         } catch (IllegalArgumentException e) {
-            System.out.println(Message.ERROR_INPUT_FUNCTIONS);
+            System.out.println(Message.ERROR_INPUT);
             return false;
         }
         return true;
     }
 
     public void input() {
-        input = Console.readLine();
+        input = String.valueOf(Console.readLine());
     }
 
     private boolean checkFunctions(){
@@ -51,5 +58,44 @@ public class Controller {
         return false;
     }
 
+    private void selectCourses() {
+        System.out.println(Message.PRINT_COURSES);
+        boolean check = false;
+        while (!check) {
+            System.out.println(Message.INPUT_COURSES);
+            input();
+            check = inputCourses();
+        }
 
+    }
+
+    private boolean inputCourses() {
+        try {
+            courses = input.split(", ");
+            boolean check = checkCourses();
+            if (!check) {
+                throw new IllegalArgumentException();
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println(Message.ERROR_INPUT);
+            return false;
+        }
+        return true;
+    }
+
+    private boolean checkCourses() {
+        if (courses.length != 3 && !(courses[0].equals("벡엔드") || courses.equals("프론트엔드"))) {
+            return false;
+        }
+        if (courses[1].equals("레벨1") && (courses[2].equals("자동차경주") || courses[2].equals("로또") || courses[2].equals("숫자야구게임"))) {
+            return true;
+        }
+        if (courses[1].equals("레벨2") && (courses[2].equals("장바구니") || courses[2].equals("결제") || courses[2].equals("지하철노선도"))) {
+            return true;
+        }
+        if (courses[1].equals("레벨4") && (courses[2].equals("성능개선") || courses[2].equals("배포"))) {
+            return true;
+        }
+        return false;
+    }
 }
