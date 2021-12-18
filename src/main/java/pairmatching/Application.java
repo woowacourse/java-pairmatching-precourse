efdc8267d1d98ad6b9d4329ce85eb62cf90a75b3
Gunwoong Kim <rjsdnd980316@naver.com>
2021-12-18 13:54:32 +0900

    feature: 크루 정보 읽어오기

--- src/main/java/pairmatching/Application.java
+++ src/main/java/pairmatching/Application.java
@@ -1,7 +1,10 @@
 package pairmatching;
 
+import pairmatching.Controller.MatchingController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO 구현 진행
+
+        MatchingController.run();
     }
 }
--- src/main/java/pairmatching/Controller/MatchingController.java
+++ src/main/java/pairmatching/Controller/MatchingController.java
@@ -1,10 +1,32 @@
 package pairmatching.Controller;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class MatchingController {
-    public MatchingController(){
+    public MatchingController() {
 
     }
-    public void run(){
-        
+
+    public static void run() {
+        try {
+            readCrewData();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static void readCrewData() throws IOException {
+        Path pathBackEndCrew = Paths.get("C:\\Users\\rjsdnd0316\\Desktop\\java-pairmatching-precourse\\src\\main\\resources\\backend-crew.md");
+        Path pathFrontEndCrew = Paths.get("C:\\Users\\rjsdnd0316\\Desktop\\java-pairmatching-precourse\\src\\main\\resources\\frontend-crew.md");
+        Charset cs = StandardCharsets.UTF_8;
+        List<String> backEndCrewNames = Files.readAllLines(pathBackEndCrew, cs);
+        List<String> frontEndCrewNames = Files.readAllLines(pathFrontEndCrew, cs);
+
     }
 }
