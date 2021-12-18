b90b1190cfc2a9fbc16e47c2a72e538a8064a1ce
hyeonic <dev.hyeonic@gmail.com>
2021-12-18 16:04:17 +0900

    feat: PairMission 비교를 위한 equals, hashCode 메서드 추가

--- src/main/java/pairmatching/domain/PairMission.java
+++ src/main/java/pairmatching/domain/PairMission.java
@@ -1,5 +1,7 @@
 package pairmatching.domain;
 
+import java.util.Objects;
+
 public class PairMission {
     private final Course course;
     private final Level level;
@@ -22,5 +24,22 @@ public class PairMission {
 
     public Course getCourse() {
         return course;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        PairMission that = (PairMission)o;
+        return course == that.course && level == that.level && Objects.equals(mission, that.mission);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(course, level, mission);
     }
 }
