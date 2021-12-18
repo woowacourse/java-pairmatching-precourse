bb4fc99435a46c67bf7e62192a80ee9dc107de50
zmfl1230 <zmfl123097@gmail.com>
2021-12-18 15:21:09 +0900

    feat[PairListOfLevel-Domain]: create class pair list of level

---
+++ src/main/java/pairmatching/domain/PairListOfLevel.java
@@ -0,0 +1,19 @@
+package pairmatching.domain;
+
+import java.util.List;
+
+public class PairListOfLevel {
+    Level level;
+    List<Pair> pairList;
+
+    public PairListOfLevel(Level level) {
+        this.level = level;
+    }
+
+    // 이미 쌍이 존재하는지 검사
+    // 값 초기화
+    // 과정 삭제
+    // 과정 조사
+
+
+}
