17664ec6d7e428ded2e26662f29b17007458e23a
Hongdonggeon <ghd700@naver.com>
2021-12-18 14:20:57 +0900

    feat(missions,initservice) : 미션 초기화 하기

--- src/main/java/pairmatching/InitService.java
+++ src/main/java/pairmatching/InitService.java
@@ -4,16 +4,30 @@ import java.util.Arrays;
 import java.util.List;
 
 import pairmatching.domain.Crews;
+import pairmatching.domain.Level;
+import pairmatching.domain.Missions;
 
 public class InitService {
 	private static List<String> backendCrewNames = Arrays.asList("백호", "태웅", "치수", "태섭", "대만", "준호", "대협", "덕규", "태산",
 		"경태", "수겸", "현준", "준섭", "한나", "소연", "호열", "대남", "용팔", "구식", "달재");
 	private static List<String> frontendCrewNames = Arrays.asList("보노", "시저", "쉐리", "신디", "다비", "덴버", "이브", "제시", "라라",
 		"린다", "리사", "니콜", "로드", "윌터", "제키");
+	private static List<String> level1Missions = Arrays.asList("자동차경주", "로또", "숫자야구게임");
+	private static List<String> level2Missions = Arrays.asList("장바구니", "결제", "지하철노선도");
+	private static List<String> level4Missions = Arrays.asList("성능개선", "배포");
 
 	public static void initCrew() {
 		Crews crews = new Crews();
 		crews.addBackEndCrews(backendCrewNames);
 		crews.addFrontEndCrews(frontendCrewNames);
+		crews.addMapCrewList();
+	}
+
+	public static void initMission() {
+		Missions missions = new Missions();
+		missions.addLevel1Mission(level1Missions);
+		missions.addLevel2Mission(level2Missions);
+		missions.addLevel4Mission(level4Missions);
+		missions.addMapCrewList();
 	}
 }
--- src/main/java/pairmatching/domain/Missions.java
+++ src/main/java/pairmatching/domain/Missions.java
@@ -1,2 +1,42 @@
-package pairmatching.domain;public class Missions {
+package pairmatching.domain;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Missions {
+	private static Map<Level, List<Mission>> levelListMap = new HashMap<>();
+	private List<Mission> level1Missions = new ArrayList<>();
+	private List<Mission> level2Missions = new ArrayList<>();
+	private List<Mission> level4Missions = new ArrayList<>();
+
+	public void addLevel1Mission(List<String> missionNames) {
+		for (String missionName : missionNames) {
+			level1Missions.add(new Mission(Level.LEVEL1, missionName));
+		}
+	}
+
+	public void addLevel2Mission(List<String> missionNames) {
+		for (String missionName : missionNames) {
+			level1Missions.add(new Mission(Level.LEVEL2, missionName));
+		}
+	}
+
+	public void addLevel4Mission(List<String> missionNames) {
+		for (String missionName : missionNames) {
+			level1Missions.add(new Mission(Level.LEVEL4, missionName));
+		}
+	}
+
+	public void addMapCrewList() {
+		levelListMap.put(Level.LEVEL1, level1Missions);
+		levelListMap.put(Level.LEVEL2, level2Missions);
+		levelListMap.put(Level.LEVEL4, level4Missions);
+	}
+
+	public static List<Mission> getCrewList(Level level) {
+		return levelListMap.get(level);
+	}
+
 }
