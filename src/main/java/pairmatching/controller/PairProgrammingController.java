319bf0330e043992d82af55334ad0944549b6e52
jaeyoungjang2 <lion85488@gmail.com>
2021-12-18 18:01:56 +0900

    refactor: 컨벤션 수정

--- src/main/java/pairmatching/controller/PairProgrammingController.java
+++ src/main/java/pairmatching/controller/PairProgrammingController.java
@@ -16,9 +16,8 @@ public class PairProgrammingController {
     private String targetLevel;
     private String targetMission;
 
-//    private List<String> backEndCrew = Arrays.asList("백호", "태웅", "치수", "태섭", "대만", "준호", "대협", "덕규", "태산", "경태", "수겸", "현준", "준섭", "한나", "소연", "호열", "대남", "용팔", "구식", "달재");
-    private List<String> backEndCrew = Arrays
-        .asList("백호", "태웅", "치수", "태섭", "대만");
+    private List<String> backEndCrew = Arrays.asList("백호", "태웅", "치수", "태섭", "대만", "준호", "대협", "덕규", "태산", "경태", "수겸", "현준", "준섭", "한나", "소연", "호열", "대남", "용팔", "구식", "달재");
+//    private List<String> backEndCrew = Arrays.asList("백호", "태웅", "치수", "태섭", "대만");
     private List<String> frontEndCrew = Arrays
         .asList("보노", "시저", "쉐리", "신디", "다비", "덴버", "이브", "제시", "라라", "린다", "리사", "니콜", "로드",
             "윌터", "제키");
