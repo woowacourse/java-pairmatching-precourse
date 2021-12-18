fca6c366586248c614084d41f42652b2ce1cc45d
Seungmin Lim <seungmin9066@gmail.com>
2021-12-18 15:25:36 +0900

    [추가] 페어매칭을 실행시켜 랜덤으로 추가하는 메서드 추가

--- src/main/java/pairmatching/domain/PairMatching.java
+++ src/main/java/pairmatching/domain/PairMatching.java
@@ -1,4 +1,29 @@
 package pairmatching.domain;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 public class PairMatching {
+	private HashMap<Curriculum, List<Crew>> matchingLog;
+	private CrewGroup crewGroup = new CrewGroup();
+
+	public PairMatching() {
+
+	}
+
+	public void run(Curriculum curriculum) {
+		for (String name : crewGroup.getShuffledCrews()) {
+			List<Crew> crews = new LinkedList<>();
+			crews.add(new Crew(curriculum.getCourse(), name));
+			matchingLog.put(curriculum, crews);
+		}
+	}
+
+
+
+
 }
