dfda5e32b61749fe8336a03609ab8d4bbaca6e8a
delphox60 <delphox60@gmail.com>
2021-12-18 13:37:52 +0900

    feat(view): 백엔드, 프론트엔드 크루들의 이름을 입력받는 기능 구현

--- src/main/java/pairmatching/view/FileInputView.java
+++ src/main/java/pairmatching/view/FileInputView.java
@@ -8,6 +8,9 @@ import java.util.List;
 
 public class FileInputView {
 
+    private static final String BE_CREWS_FILE_PATH = "src/main/resources/backend-crew.md";
+    private static final String FE_CREWS_FILE_PATH = "src/main/resources/frontend-crew.md";
+
     public static List<String> read(final String filePath) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath));
         List<String> inputCrewLineList = new ArrayList<String>();
@@ -20,6 +23,14 @@ public class FileInputView {
         }
         bufferedReader.close();
         return inputCrewLineList;
+    }
+
+    public static List<String> readBackEndCrews() throws IOException {
+        return read(BE_CREWS_FILE_PATH);
+    }
+
+    public static List<String> readFrontEndCrews() throws IOException {
+        return read(FE_CREWS_FILE_PATH);
     }
 }
 
