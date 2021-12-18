97b17dd7e215944314338d732d48e173fe497c0e
soulfever <soulfever01@naver.com>
2021-12-18 16:42:50 +0900

    FEAT : 페어 초기화 기능 추가
    - 다른 기능을 만들다가 아이디어가 생각나지 않아 초기화 기능을 먼저 올렸습니다.
    - ClearPair() 가 초기화 기능입니다.

--- src/main/java/pairmatching/Mission.java
+++ src/main/java/pairmatching/Mission.java
@@ -1,2 +1,50 @@
-package pairmatching;public class Mission {
+package pairmatching;
+
+import java.util.ArrayList;
+
+public enum Mission {
+    CAR("레벨1","자동차경주"),
+    LOTTO("레벨1","로또"),
+    BASEBALL("레벨1", "숫자야구게임"),
+    BUCKET("레벨2","장바구니"),
+    PAY("레벨2","결제"),
+    SUBWAY("레벨2","지하철 노선도"),
+    LEVEL3("레벨3"),
+    REFORM("레벨4", "성능개선"),
+    DISTRIBUTE("레벨4", "배포"),
+    LEVEL5("레벨5");
+
+    private String level;
+    private String missionName = "";
+    private static ArrayList<Crew> attendanceBook = new ArrayList<>();
+
+    Mission(String level, String missionName) {
+        this.level = level;
+        this.missionName = missionName;
+    }
+
+    Mission(String level) {
+        this.level = level;
+    }
+
+    public static void ClearPair() {
+        for (Mission mission : Mission.values()) {
+            attendanceBook = new ArrayList<>();
+        }
+    }
+
+    public static boolean CheckTheAttendanceBook(ArrayList<Crew> CrewList) {
+
+        return true;
+    }
+
+    public static boolean MakeAttendanceBook(ArrayList<Crew> CrewList) {
+        if (attendanceBook.size() == 0) {
+            attendanceBook = new ArrayList<>();
+            attendanceBook.addAll(CrewList);
+            return true;
+        }
+        return false;
+    }
+
 }
