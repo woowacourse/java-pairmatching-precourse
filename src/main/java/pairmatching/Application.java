04bbf399a9b8c4250189a696aef35480ebee458d
harinn1014 <gkfla7555@gmail.com>
2021-12-18 17:57:12 +0900

    feat(Application): 함수 사용
    <description>
    MainController 사용을 위한 함수 사용

--- src/main/java/pairmatching/Application.java
+++ src/main/java/pairmatching/Application.java
@@ -1,7 +1,11 @@
 package pairmatching;
 
+import pairmatching.controller.MainController;
+
 public class Application {
     public static void main(String[] args) {
         // TODO 구현 진행
+    	MainController mainController= new MainController();
+    	mainController.run();
     }
 }
