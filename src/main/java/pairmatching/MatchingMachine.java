5260a77e89c720ac7dda23d8d50ddcb1d853bf75
kanamycine <jinyeong5320@gmail.com>
2021-12-18 16:52:07 +0900

    feat(Matching) : 매칭 안되는 문제 발견

---
+++ src/main/java/pairmatching/MatchingMachine.java
@@ -0,0 +1,66 @@
+package pairmatching;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import pairmatching.domain.Course;
+import pairmatching.domain.Crews;
+import pairmatching.domain.Level;
+import pairmatching.domain.MainView;
+import pairmatching.domain.Matching;
+import pairmatching.domain.Mission;
+import pairmatching.domain.Missions;
+import pairmatching.view.InputView;
+import pairmatching.view.OutputView;
+
+public class MatchingMachine {
+
+	Missions missions;
+	Crews crews;
+
+	public MatchingMachine(){
+		this.missions = new Missions();
+		this.crews = new Crews();
+	}
+	public void run(){
+		MainView mainView = MainView.valueOf(InputView.askFeature());
+		runFeature(mainView);
+	}
+
+	private void runFeature(MainView mainView) {
+		if (mainView.isMatching()){
+			matching();
+			return;
+		}
+		// if(mainView.isSearching()){
+		// 	searching();
+		// 	return;
+		// }
+		// if(mainView.isInitializing()){
+		// 	initializing();
+		// 	return;
+		// }
+		// if(mainView.isExit()){
+		// 	exit();
+		// }
+	}
+
+	private void matching(){
+		Matching matching = new Matching();
+		Set<List> pairsCrew = new HashSet<>();
+		List<String> crewNames = new ArrayList<>();
+		List<String> matchInformation = Arrays.asList((InputView.askWantedMatchingInformation().split(", ")));
+		System.out.println(matchInformation.get(0));
+		System.out.println(Course.getCourse(matchInformation.get(0)));
+		Mission mission = missions.getMission(Course.getCourse(matchInformation.get(0)), Level.getLevel(matchInformation.get(1)), matchInformation.get(2));
+		crewNames = crews.getCrews(mission.getCourse());
+		if(mission.getPairCrews() == null) {
+			pairsCrew = matching.matching(crewNames);
+			OutputView.printPairCrews(pairsCrew);
+		}
+	}
+}
+//백엔드, 레벨1, 자동차경주
---
+++ src/main/java/pairmatching/domain/Matching.java
@@ -0,0 +1,44 @@
+package pairmatching.domain;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Matching {
+
+	private Set<List> pairCrews;
+
+	public Matching(){
+		this.pairCrews = new HashSet<>();
+	}
+
+	public Set<List> matching(List<String> crewNames) {
+		Randoms.shuffle(crewNames);
+		if (crewNames.size() % 2 == 0) {
+			for (int i = 0; i < crewNames.size() / 2; i += 2) {
+				List<String> pair = new ArrayList<>();
+				pair.add(crewNames.get(i));
+				pair.add(crewNames.get(i + 1));
+				pairCrews.add(pair);
+			}
+		}
+		if (crewNames.size() % 2 != 0) {
+			for (int i = 0; i < (crewNames.size() / 2) - 1; i += 2) {
+				List<String> pair = new ArrayList<>();
+				pair.add(crewNames.get(i));
+				pair.add(crewNames.get(i + 1));
+				pairCrews.add(pair);
+			}
+			List<String> pair = new ArrayList<>();
+			pair.add(crewNames.get(crewNames.size() - 1));
+			pair.add(crewNames.get(crewNames.size() - 2));
+			pair.add(crewNames.get(crewNames.size() - 3));
+		}
+		return pairCrews;
+	}
+}
