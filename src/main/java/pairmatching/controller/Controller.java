fec7583b287ecbd72d60b7a310916b49522a07b0
최창준 <aowert6518@gmail.com>
2021-12-18 13:31:54 +0900

    [feat] 1을 입력받으면 과정을 보여준다
    - choicePairMatching() : 1번 페어매칭을 입력받으면 페어매칭을 시작하는 기능

--- src/main/java/pairmatching/controller/Controller.java
+++ src/main/java/pairmatching/controller/Controller.java
@@ -1,10 +1,15 @@
 package pairmatching.controller;
 
 import pairmatching.view.InputView;
+import pairmatching.view.OutputView;
 
 public class Controller {
 
     public void getUserChoice() {
         String userChoice = InputView.getUserChoice();
+    }
+
+    public void choicePairMatching() {
+        OutputView.printProcess();
     }
 }
---
+++ src/main/java/pairmatching/view/OutputView.java
@@ -0,0 +1,9 @@
+package pairmatching.view;
+
+import static pairmatching.constant.OutputViewMessage.GUIDE_PROCESS_MESSAGE;
+
+public class OutputView {
+    public static void printProcess() {
+        System.out.println(GUIDE_PROCESS_MESSAGE);
+    }
+}
