f5d640751265f87ecc4749b5e8345f3a00cae6df
YuJeong <yjfflq4872@gmail.com>
2021-12-18 17:54:38 +0900

    fix: 파일 경로 수정

--- src/main/java/pairmatching/utill/CrewFileReader.java
+++ src/main/java/pairmatching/utill/CrewFileReader.java
@@ -8,25 +8,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 import pairmatching.domain.Course;
+import pairmatching.view.OutputView;
 
 public class CrewFileReader {
-	public static List<String> getCrewList(Course course) throws IOException {
-		File file = getFile(course);
-		BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+	private static final String FILE_READ_ERROR_MSG = "파일을 읽을 수 없습니다.";
 
+	public static List<String> getCrewList(Course course) {
 		List<String> crewList = new ArrayList<>();
-		String crewName = "";
-		while ((crewName = bufferedReader.readLine()) != null) {
-			crewList.add(crewName);
+		try {
+			File file = getFile(course);
+			BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+			String crewName = "";
+			while ((crewName = bufferedReader.readLine()) != null) {
+				crewList.add(crewName);
+			}
+		} catch (IOException e) {
+			OutputView.displayErrorMsg(FILE_READ_ERROR_MSG);
 		}
-
 		return crewList;
 	}
 
 	private static File getFile(Course course) {
 		if (course.equals(Course.BACKEND)) {
-			return new File("/src/main/resources/backend-crew.md");
+			return new File("src/main/resources/backend-crew.md");
 		}
-		return new File("/src/main/resources/frontend-crew.md");
+		return new File("src/main/resources/frontend-crew.md");
 	}
 }
