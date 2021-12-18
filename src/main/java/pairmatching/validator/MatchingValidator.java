3e614c07912ee6345a7d1e1032b50611fbc3e169
Gyuchool <balance0591@naver.com>
2021-12-18 17:20:11 +0900

    refactor: 페어 매칭 결과 안내 문구 출력 추가

--- src/main/java/pairmatching/validator/MatchingValidator.java
+++ src/main/java/pairmatching/validator/MatchingValidator.java
@@ -7,10 +7,9 @@ import pairmatching.view.InputView;
 public class MatchingValidator {
 
 	public static void checkDuplicatedValidation(Level level, Matching matching) {
-		//존재하지 않는다면
 		if (matching.isExsistLevel(level)) {
-			return;
+			InputView.askRematching();
 		}
-		InputView.askRematching();
+		return;
 	}
 }
--- src/main/java/pairmatching/view/OutputView.java
+++ src/main/java/pairmatching/view/OutputView.java
@@ -2,15 +2,12 @@ package pairmatching.view;
 
 import pairmatching.domain.Matching;
 import pairmatching.domain.Mission;
-import pairmatching.validator.MatchingValidator;
 
 public class OutputView {
 	private static final String LINE = "#############################################";
 	private static final String MISSION = "미션:";
 	private static final String COURSE = "과정: 백엔드 | 프론트엔드";
-	private static final String OPEN_BRACKET = "[";
-	private static final String CLOSE_BRACKET = "]";
-
+	private static final String PAIR_RESULT = "페어 매칭 결과입니다.";
 
 	public static void printAdvanceInformation() {
 		final Mission mission = new Mission();
@@ -25,6 +22,7 @@ public class OutputView {
 		String matchingInfoList = matching.toString();
 		String matchingInfo = matchingInfoList.substring(1, matchingInfoList.length() - 1);
 		String[] crewMatchingList = matchingInfo.split(",");
+		System.out.println(PAIR_RESULT);
 		for (String crewMatching : crewMatchingList) {
 			System.out.println(crewMatching);
 		}
