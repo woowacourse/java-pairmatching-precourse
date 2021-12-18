aa9a5205ee5e13930bc7d10f42e1b98ff88286f2
overnew <ehdls1303@gmail.com>
2021-12-18 17:49:25 +0900

    faet: 어플리케이션 실행 구현
    어플리케이션 실행 구현

--- src/main/java/pairmatching/Application.java
+++ src/main/java/pairmatching/Application.java
@@ -1,7 +1,11 @@
 package pairmatching;
 
+import pairmatching.source.*;
+
+
 public class Application {
     public static void main(String[] args) {
-        // TODO 구현 진행
+        PairMatchingSystem pairMatchingSystem = new PairMatchingSystem();
+        pairMatchingSystem.runStart();
     }
 }
