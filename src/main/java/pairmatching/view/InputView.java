9cd9745349fdaccc403247667c9631e657b1e80a
minsulee <alkwen0996@naver.com>
2021-12-18 13:36:43 +0900

    feat: 사용할 프로그램 기능 선택 기능 추가

--- src/main/java/pairmatching/view/InputView.java
+++ src/main/java/pairmatching/view/InputView.java
@@ -1,2 +1,20 @@
-package pairmatching.view;public class InputView {
+package pairmatching.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    private static final String INPUT_MESSAGE_CHOOSE_FUNCTION = "기능을 선택하세요.";
+
+    public String inputPairMatchingFunction() {
+        System.out.println(INPUT_MESSAGE_CHOOSE_FUNCTION);
+        String inputFunctionSignal = inputValue();
+
+        return inputFunctionSignal;
+    }
+
+    public String inputValue() {
+        return Console.readLine();
+    }
+
 }
