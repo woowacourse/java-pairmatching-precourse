4646ad32d337524c299ef74671b998bb142ebb19
TaeHyeon Kim <kth990303@naver.com>
2021-12-18 17:27:06 +0900

    fix: Q 누를 때 바로 종료 안되는 현상 수정

--- src/main/java/pairmatching/Application.java
+++ src/main/java/pairmatching/Application.java
@@ -4,7 +4,7 @@ public class Application {
     public static void main(String[] args) {
         PairMatching pairMatching = new PairMatching();
         do {
-            pairMatching.run();
+
         } while (pairMatching.run());
     }
 }
