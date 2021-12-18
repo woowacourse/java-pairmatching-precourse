c795a3e0b2da10436d2c59365651cc62d05e7548
sun <syoun602@gmail.com>
2021-12-18 14:07:09 +0900

    feat: add method for printing and selecting mode

--- src/main/java/pairmatching/controller/MainController.java
+++ src/main/java/pairmatching/controller/MainController.java
@@ -5,6 +5,10 @@ import pairmatching.view.InputView;
 import pairmatching.view.OutputView;
 
 public class MainController {
+    private static final String MATCHING_MODE = "1";
+    private static final String REFERENCE_MODE = "2";
+    private static final String CLEAR_MODE = "3";
+    private static final String EXIT_MODE = "Q";
     public static MainController instance;
 
     public static MainController getInstance() {
@@ -19,9 +23,26 @@ public class MainController {
     }
 
     private void initMain() {
-        InputView.printSelectMode();
-        String mode = mainScreenInput();
+        String mode;
+        do {
+            InputView.printSelectMode();
+            mode = mainScreenInput();
+        } while(moveController(mode));
     }
+
+    private boolean moveController(String mode) {
+        if (mode.equals(MATCHING_MODE)) {
+            System.out.println("1");
+        }
+        if (mode.equals(REFERENCE_MODE)) {
+            System.out.println("2");
+        }
+        if (mode.equals(CLEAR_MODE)) {
+            System.out.println("3");
+        }
+        return !mode.equals(EXIT_MODE);
+    }
+
 
     private String mainScreenInput() {
         try {
