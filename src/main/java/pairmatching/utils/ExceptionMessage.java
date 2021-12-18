95b64538b94bbcccb078e495d54382610f99ce0d
harinn1014 <gkfla7555@gmail.com>
2021-12-18 17:21:36 +0900

    feat(ExceptionMessage): 누락된 에러 메시지 추가
    <description>
    [과정/레벨/미션]: 누락된 에러 메시지 추가

--- src/main/java/pairmatching/utils/ExceptionMessage.java
+++ src/main/java/pairmatching/utils/ExceptionMessage.java
@@ -6,7 +6,9 @@ public class ExceptionMessage {
 
 	public static final String INPUT_FUNCTION = ERROR + "잘못된 입력입니다. 보기를 다시 확인해주세요.";
 	public static final String INPUT_LENGTH = ERROR + "구분자 " + Constant.DELIMITER_SELECT + "로 과정,레벨,미션 순서로 입력해주세요";
-	public static final String INPUT_COURSE = ERROR + "잘못된 이름의 [과정/레벨/미션]입니다.";
+	public static final String INPUT_COURSE = ERROR + "과정의 이름이 잘못되었습니다.";
+	public static final String INPUT_LEVEL = ERROR + "레벨의 이름이 잘못되었습니다.";
+	public static final String INPUT_MISSION = ERROR + "미션의 이름이 잘못되었습니다.";
 
 	// 매칭
 	public static final String MATCING_FAIL = ERROR + "매칭에 실패하였습니다."; // 3회 시도까지 매칭x, 경우의 수 없음
