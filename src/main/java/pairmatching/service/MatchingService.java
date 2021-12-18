7a548939db73b00e4cd49d0f42f206ac97c3879b
erie0210 <eriee0210@gmail.com>
2021-12-18 15:03:34 +0900

    feat: 파일을 읽어온다

--- src/main/java/pairmatching/service/MatchingService.java
+++ src/main/java/pairmatching/service/MatchingService.java
@@ -1,2 +1,48 @@
-package pairmatching.service;public class MatchingService {
+package pairmatching.service;
+
+import pairmatching.utils.Path;
+import pairmatching.utils.ReadFile;
+import java.util.List;
+
+public class MatchingService {
+    private String courseName;
+    private String levelName;
+    private String testName;
+    private List<String> nameList;
+
+    public MatchingService(String courseName, String levelName, String testName){
+        this.courseName = courseName;
+        this.levelName = levelName;
+        this.testName = testName;
+    }
+
+//    파일을 읽어온다
+    public void getFileName(){
+        String path = getPath();
+        this.nameList = ReadFile.readFile(path);
+        return;
+    }
+
+    public String getPath(){
+        if(courseName.equals("백엔드")) {
+            return Path.BACKEND_PATH;
+        }
+        return Path.FRONTEND_PATH;
+    }
+
+    // 페어링을 한다
+    public void getMatching(){
+        return;
+    }
+
+    // 기존에 페어링이 있는 지 확인한다
+    public void checkMatchingExist(){
+        return;
+    }
+
+//    페어링이 제대로 되지 않았을 때 다시 페어링을 하는 기능
+    public void matchAgain(){
+        return;
+    }
+
 }
