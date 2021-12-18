71f956d045086fd86d72e255a0c4a037b85d89d0
yxxnghwan <younghwan960@gmail.com>
2021-12-18 16:03:57 +0900

    feat: 페어 조회에서 과정과 미션을 출력하고 확인하고자하는 과정, 레벨, 미션을 입력

--- src/main/java/pairmatching/view/PairMatchingSearchView.java
+++ src/main/java/pairmatching/view/PairMatchingSearchView.java
@@ -1,13 +1,37 @@
 package pairmatching.view;
 
+import java.util.List;
+
+import camp.nextstep.edu.missionutils.Console;
+import pairmatching.Application;
+import pairmatching.controller.ViewMappingKey;
+import pairmatching.domain.MatchParams;
+import pairmatching.util.MissionInfoValidator;
+import pairmatching.util.SystemMessage;
+
 public class PairMatchingSearchView implements View {
 	@Override
 	public void flow() {
-
+		String missionInfo = readMissionInfo();
+		List<String> params = MissionInfoValidator.validate(missionInfo);
+		MatchParams matchParams = Application.controller.setMatchParams(params);
+		
 	}
 
 	@Override
 	public void printViewMessage() {
-		View.super.printViewMessage();
+		System.out.println(SystemMessage.INFO_LINE);
+		String courseListString = Application.controller.getCourseListString();
+		System.out.println(SystemMessage.COURSE + courseListString);
+		System.out.println(SystemMessage.MISSION);
+		String missionListString = Application.controller.getMissionListString();
+		System.out.println(missionListString);
+		System.out.println(SystemMessage.INFO_LINE);
+		System.out.println(SystemMessage.INPUT_MISSION_INFO);
+		System.out.println(SystemMessage.MISSION_INFO_EX);
+	}
+
+	private String readMissionInfo() {
+		return Console.readLine();
 	}
 }
