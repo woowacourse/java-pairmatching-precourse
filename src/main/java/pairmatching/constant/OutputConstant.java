301eecae95016881fa2065b9b0baa0afb1b0b49c
sejung-hong <sejungh@naver.com>
2021-12-18 15:44:22 +0900

    feat : 과정, 레벨, 미션을 출력하는 기능

--- src/main/java/pairmatching/constant/OutputConstant.java
+++ src/main/java/pairmatching/constant/OutputConstant.java
@@ -1,4 +1,11 @@
 package pairmatching.constant;
 
 public class OutputConstant {
+    public static final String MSG_OUTPUT_HASH = "#############################################";
+    public static final String MSG_OUTPUT_COURSE = "과정: ";
+    public static final String MSG_OUTPUT_MISSION = "미션: ";
+    public static final String STICK = "  - ";
+    public static final String PARTITION = " | ";
+    public static final String COLON = ": ";
+
 }
--- src/main/java/pairmatching/view/OutputView.java
+++ src/main/java/pairmatching/view/OutputView.java
@@ -1,4 +1,46 @@
 package pairmatching.view;
 
+import pairmatching.model.Course;
+import pairmatching.model.CourseLevelInfo;
+import pairmatching.model.Game;
+import pairmatching.model.Level;
+
+import java.util.List;
+import java.util.Map;
+
+import static pairmatching.constant.OutputConstant.*;
+
 public class OutputView {
+    public static void functionInfo(){
+        System.out.println(MSG_OUTPUT_HASH);
+        outputCourse();
+        outputMission();
+        System.out.println(MSG_OUTPUT_HASH);
+    }
+
+    private static void outputCourse(){
+        System.out.print(MSG_OUTPUT_COURSE);
+        List<Course> courseList = CourseLevelInfo.getCourseList();
+        for(Course course : courseList){
+            System.out.print(course.getName());
+            System.out.print(PARTITION);
+        }
+        System.out.println();
+
+    }
+
+    private static void outputMission(){
+        System.out.println(MSG_OUTPUT_MISSION);
+        Map<Level, List<Game>> levelMission = CourseLevelInfo.getLevelMission();
+        for(Map.Entry<Level, List<Game>> m: levelMission.entrySet()){
+            System.out.print(STICK + m.getKey().getName() + COLON);
+            for(Game game : m.getValue()){
+                System.out.print(game.getName());
+                System.out.print(PARTITION);
+            }
+            System.out.println();
+        }
+    }
+
+
 }
