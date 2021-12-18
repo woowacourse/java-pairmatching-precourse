300cc8d4781f99cbc303b1f1373166c09c8d2c4b
Wishoon <heonga2@gmail.com>
2021-12-18 14:29:57 +0900

    feat: 과정의 이름을 구성하는 필드와 입력 받은 과정이 있는지 확인하는 기능 추가

---
+++ src/main/java/pairmatching/domain/Course.java
@@ -0,0 +1,22 @@
+package pairmatching.domain;
+
+import java.util.Arrays;
+
+public enum Course {
+	BACKEND("백엔드"),
+	FRONTEND("프론트엔드");
+
+	private static final String ERROR_NOT_COURSE = " 과정은 없습니다.";
+	private String name;
+
+	Course(String name) {
+		this.name = name;
+	}
+
+	public static pairmatching.domain.Course findByCourse(String command) {
+		return Arrays.stream(pairmatching.domain.Course.values())
+			.filter(course -> course.name.equals(command))
+			.findAny()
+			.orElseThrow(() -> new IllegalArgumentException(command + ERROR_NOT_COURSE));
+	}
+}
