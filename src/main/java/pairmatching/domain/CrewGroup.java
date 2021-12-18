08c8e9a0f6c0a64d3fc4379f6c7c2ac9c2b95e15
Seungmin Lim <seungmin9066@gmail.com>
2021-12-18 13:46:21 +0900

    [추가] 크루 목록을 가져오는 메서드 추가

--- src/main/java/pairmatching/domain/CrewGroup.java
+++ src/main/java/pairmatching/domain/CrewGroup.java
@@ -1,2 +1,25 @@
-package pairmatching.domain;public class CrewGroup {
+package pairmatching.domain;
+
+import java.util.List;
+
+public class CrewGroup {
+
+	private static final String backEndCrewDirPath =
+		"C:\\Users\\LG\\java-pairmatching-precourse\\src\\main\\resources\\backend-crew.md";
+	private static final String frontEndCrewDirPath =
+		"C:\\Users\\LG\\java-pairmatching-precourse\\src\\main\\resources\\frontend-crew.md";
+
+	private List<String> backEndCrews;
+	private List<String> frontEndCrews;
+
+	public CrewGroup() {
+		bringCrews();
+	}
+
+	private void bringCrews() {
+		bringBackEndCrews();
+		bringFrontEndCrews();
+	}
+
+
 }
