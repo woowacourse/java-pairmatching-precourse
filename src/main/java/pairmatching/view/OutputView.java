22e6b82036bc1de7aafbf98842d315dd10e18ebc
YuJeong <yjfflq4872@gmail.com>
2021-12-18 14:12:10 +0900

    feat: 과정과 미션 출력 구현

--- src/main/java/pairmatching/view/OutputView.java
+++ src/main/java/pairmatching/view/OutputView.java
@@ -1,23 +1,54 @@
 package pairmatching.view;
 
+import java.util.List;
+
 public class OutputView {
 	private static final String NEW_LINE = "\n";
 	private static final String ERROR_MSG_PREFIX = "[ERROR] ";
+
 	private static final String MAIN_OPTION_MATCHING = "1. 페어 매칭";
 	private static final String MAIN_OPTION_GET_INFO = "2. 페어 조회";
 	private static final String MAIN_OPTION_INIT = "3. 페어 초기화";
 	private static final String MAIN_OPTION_QUIT = "Q. 종료";
 
+	private static final String DELIMIT_LINE = "#############################################";
+	private static final String COURSE_MSG = "과정: ";
+	private static final String MISSION_MSG = "미션: ";
+
 	public static void displayMainView() {
-		StringBuilder stringBuilder = new StringBuilder();
-		stringBuilder.append(MAIN_OPTION_MATCHING).append(NEW_LINE)
-			.append(MAIN_OPTION_GET_INFO).append(NEW_LINE)
-			.append(MAIN_OPTION_INIT).append(NEW_LINE)
-			.append(MAIN_OPTION_QUIT).append(NEW_LINE);
-		System.out.println(stringBuilder);
+		String mainViewMsg = MAIN_OPTION_MATCHING + NEW_LINE
+			+ MAIN_OPTION_GET_INFO + NEW_LINE
+			+ MAIN_OPTION_INIT + NEW_LINE
+			+ MAIN_OPTION_QUIT + NEW_LINE;
+		System.out.println(mainViewMsg);
 	}
 
 	public static void displayErrorMsg(String errorMsg) {
 		System.out.println(ERROR_MSG_PREFIX + errorMsg);
+	}
+
+	public static void displayInfo(List<String> course, List<String> mission) {
+		String resultMsg = DELIMIT_LINE + NEW_LINE
+			+ COURSE_MSG + getCourseInfo(course) + NEW_LINE
+			+ MISSION_MSG + NEW_LINE + getMissionInfo(mission)
+			+ DELIMIT_LINE;
+		System.out.println(resultMsg);
+	}
+
+	private static StringBuilder getCourseInfo(List<String> course) {
+		StringBuilder stringBuilder = new StringBuilder();
+		for (int i = 0; i < course.size() - 1; i++) {
+			stringBuilder.append(course.get(i)).append(" | ");
+		}
+		return stringBuilder.append(course.get(course.size() - 1));
+	}
+
+	private static StringBuilder getMissionInfo(List<String> mission) {
+		StringBuilder stringBuilder = new StringBuilder();
+		for (int i = 0; i < mission.size() - 1; i++) {
+			stringBuilder.append("레벨").append(i + 1).append(": ")
+				.append(mission.get(i)).append(NEW_LINE);
+		}
+		return stringBuilder;
 	}
 }
