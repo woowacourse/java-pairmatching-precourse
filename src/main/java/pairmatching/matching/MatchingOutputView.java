edabb1187e863a749cfa6e7a7c0b8712871e5b11
TaeHyeon Kim <kth990303@naver.com>
2021-12-18 14:14:55 +0900

    refactor: 결과 안내 가독성 향상

--- src/main/java/pairmatching/matching/MatchingOutputView.java
+++ src/main/java/pairmatching/matching/MatchingOutputView.java
@@ -1,11 +1,17 @@
 package pairmatching.matching;
 
 public class MatchingOutputView {
-    public static void seeMatchingResult(){
+
+    public static void seeMatchingResult() {
+        System.out.println();
         System.out.println("페어 매칭 결과입니다.");
+        // 결과 출력
+        System.out.println();
     }
 
     public static void resetFinishedPrint() {
+        System.out.println();
         System.out.println("초기화 되었습니다. ");
+        System.out.println();
     }
 }
