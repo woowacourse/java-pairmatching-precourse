60008631dacba698c5fa7cc66cc7d162a15ebb66
grace_goose <choieungi@gm.gist.ac.kr>
2021-12-18 14:31:05 +0900

    feat: 파일을 입력받는 기능 및 Crew, Course 도메인 추가

--- src/main/java/pairmatching/InputView.java
+++ src/main/java/pairmatching/InputView.java
@@ -2,22 +2,32 @@ package pairmatching;
 
 
 import camp.nextstep.edu.missionutils.Console;
+import pairmatching.domain.Course;
+import pairmatching.domain.Crew;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class InputView {
 
+    private static final ClassLoader loader = Application.class.getClassLoader();
     private static final String SELECT_FUNCTION_INSTRUCTION = "기능을 선택하세요. ";
     private static final String PAIR_MATCHING_INSTRUCTION = "1. 페어 매칭 ";
     private static final String PAIR_CHECK_INSTRUCTION = "2. 페어 조회 ";
     private static final String PAIR_INITALIZATION = "3. 페어 초기화 ";
     private static final String QUIT_INSTRUCTION = "Q. 종료";
-
     private static final int MIN_SELECTION_NUMBER = 1;
     private static final int MAX_SELECTION_NUMBER = 3;
     private static final String QUIT_SELECTION_STRING = "Q";
     private static final String ERROR_HEADER = "[ERROR] ";
     private static final String NOT_IN_RANGE_NUMEBR_ERROR = "선택할 수 없는 숫자입니다. 기능 숫자 범위 내의 숫자를 입력해주세요. ";
     private static final String NOT_IN_RANGE_STRING_ERROR = "선택할 수 없는 문자입니다. 기능 범위 내의 숫자를 입력해주세요. ";
-
+    private static final String NOT_EXIST_FILE = "존재하지 않는 파일입니다. ";
+    private static final char PATH_CHAR = '/' ;
 
 
     public String inputSelectionNumber() {
@@ -29,6 +39,35 @@ public class InputView {
             printMessage(e.getMessage());
             return inputSelectionNumber();
         }
+    }
+
+    public List<Crew> getCrewFromFile(String fileName, Course course) {
+        List<Crew> crews = new ArrayList<>();
+        File file = getFileByPath(fileName);
+        for (String crew : readFile(file)) {
+            crews.add(new Crew(course, crew));
+        }
+        return crews;
+    }
+
+    private List<String> readFile(File file) {
+        List<String> stringList = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                stringList.add(line);
+            }
+        } catch (IOException e) {
+            printMessage(ERROR_HEADER + e.getMessage());
+        }
+        return stringList;
+    }
+
+    private File getFileByPath(String path) {
+        if (path.charAt(0) != PATH_CHAR) {
+            path = PATH_CHAR + path;
+        }
+        return new File(getClass().getResource(path).getFile());
     }
 
     private void printSelectionInstruction() {
