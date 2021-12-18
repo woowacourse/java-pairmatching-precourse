3f9d34704536ca145bea6b001d4f86fc5763a1e7
2young <yh20studio@gmail.com>
2021-12-18 17:59:17 +0900

    refactor(): 파일 이름과 관련된 하드코딩 제거

--- src/main/java/pairmatching/crew/CrewInitiator.java
+++ src/main/java/pairmatching/crew/CrewInitiator.java
@@ -9,6 +9,8 @@ import pairmatching.view.ErrorView;
 
 public class CrewInitiator {
 	private static final String ERROR_READ_FILE = "해당 파일을 읽어 올 수 없습니다.";
+	private static final String BACKEND_CREW_FILE = "src/main/resources/backend-crew.md";
+	private static final String FRONTEND_CREW_FILE = "src/main/resources/frontend-crew.md";
 
 	public void readFile() {
 		try {
@@ -20,7 +22,7 @@ public class CrewInitiator {
 	}
 
 	private void readBackendCrewFile() throws IOException {
-		BufferedReader reader = new BufferedReader(new FileReader("src/main/resources/backend-crew.md"));
+		BufferedReader reader = new BufferedReader(new FileReader(BACKEND_CREW_FILE));
 		String name;
 		while ((name = reader.readLine()) != null) {
 			CrewRepository.addCrews(new Crew(Course.BACKEND, name));
@@ -29,7 +31,7 @@ public class CrewInitiator {
 	}
 
 	private void readFrontendCrewFile() throws IOException {
-		BufferedReader reader = new BufferedReader(new FileReader("src/main/resources/frontend-crew.md"));
+		BufferedReader reader = new BufferedReader(new FileReader(FRONTEND_CREW_FILE));
 		String name;
 		while ((name = reader.readLine()) != null) {
 			CrewRepository.addCrews(new Crew(Course.FRONTEND, name));
