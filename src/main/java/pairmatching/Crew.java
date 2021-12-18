d22d0bc1e0ea103164fe72be72266ec3677fc5e5
KIM <km0192@naver.com>
2021-12-18 14:09:20 +0900

    refacto(Crew) : name, course를 갖는 Class로 변경. 이름을 반환하는 toStirng, 과정 반환하는 getCourse 추가

--- src/main/java/pairmatching/Crew.java
+++ src/main/java/pairmatching/Crew.java
@@ -1,20 +1,20 @@
 package pairmatching;
 
-import java.util.Arrays;
-import java.util.List;
-
 public class Crew {
-	private final List<String> BACKEND_CREW  = Arrays.asList("백호", "태웅", "치수", "태섭", "대만", "준호", "대협", "덕규",
-		"태산", "경태", "수겸", "현준", "준섭", "한나", "소연", "호열", "대남", "용팔", "구식", "달재");
+	private Course course;
+	private String name;
 
-	private final List<String> FRONTEND_CREW = Arrays.asList("보노", "시저", "쉐리", "신디", "다비", "덴버", "이브", "제시",
-		"라라", "린다", "리사", "니콜", "로드", "윌터", "제키");
-
-	public List<String> getBackCrewList() {
-		return BACKEND_CREW;
+	public Crew(String name, Course course) {
+		this.name = name;
+		this.course = course;
 	}
 
-	public List<String> getFrontCrewList() {
-		return FRONTEND_CREW;
+	@Override
+	public String toString() {
+		return this.name;
+	}
+
+	public Course getCourse() {
+		return course;
 	}
 }
