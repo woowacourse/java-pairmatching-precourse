4138379b7fb6362d8621b9a61e18d776598380c1
Donggyu Kim <a29661498@gmail.com>
2021-12-18 14:50:53 +0900

    fix: Mission 클래스 -> 열거형으로 수정
    Mission을 클래스가 아닌 열거형으로 수정하였습니다. 각 열거형 상수는 미션의 이름과 레벨을 가집니다. 또한 각 미션의 레벨 별로 Mission 리스트를 반환하는 메소드 그리고 인자로 받은 레벨에 해당하는 미션을 출력해주는 메소드를 정의하였습니다.

--- src/main/java/pairmatching/domain/Mission.java
+++ src/main/java/pairmatching/domain/Mission.java
@@ -1,19 +1,53 @@
 package pairmatching.domain;
 
-public class Mission {
-    private String missionName;
+import java.util.ArrayList;
+import java.util.List;
+
+import static pairmatching.domain.Level.*;
+
+public enum Mission {
+
+    CAR_RACING("자동차경주", LEVEL1),
+    LOTTO("로또", LEVEL1),
+    BASEBALL("숫자야구게임", LEVEL1),
+    SHOPPING_BASKET("장바구니", LEVEL2),
+    PAYMENT("결제", LEVEL2),
+    SUBWAY_MAP("지하철노선도", LEVEL2),
+    PERFORMANCE("성능개선", LEVEL4),
+    DISTRIBUTE("배포", LEVEL4);
+
+
+    private String name;
     private Level level;
 
-    public Mission(String missionName, Level level) {
-        this.missionName = missionName;
+    Mission(String name, Level level) {
+        this.name = name;
         this.level = level;
     }
 
-    public String getMissionName() {
-        return missionName;
+    public static void printMissionByLevel(Level level) {
+        List<Mission> missions = getMissionByLevel(level);
+
+        for (int i = 0; i < missions.size(); i++) {
+            System.out.println(missions.get(i).name);
+
+            if (i != missions.size() - 1) {
+                System.out.println(" | ");
+            }
+        }
     }
 
-    public Level getLevel() {
-        return level;
+    public static List<Mission> getMissionByLevel(Level level) {
+        List<Mission> missionList = new ArrayList<>();
+
+        Mission[] missions = Mission.values();
+
+        for (Mission mission : missions) {
+            if (mission.level == level) {
+                missionList.add(mission);
+            }
+        }
+
+        return missionList;
     }
 }
