414ddc33ae20b29f5f7d813b916f36ede1d0fcea
DK <ldk980130@gmail.com>
2021-12-18 16:18:24 +0900

    feat : md 파일에서 크루원 읽기 기능 구현

---
+++ src/main/java/pairmatching/repository/CrewRepository.java
@@ -0,0 +1,47 @@
+package pairmatching.repository;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import pairmatching.domain.Course;
+import pairmatching.domain.Crew;
+
+public class CrewRepository {
+
+	private static final List<Crew> backendCrewList = new ArrayList<>();
+	private static final List<Crew> frontEndCrewList = new ArrayList<>();
+
+	public CrewRepository() throws IOException{
+		if (backendCrewList.isEmpty() || frontEndCrewList.isEmpty()) {
+			setBackendCrewList();
+			setFrontendCrewList();
+		}
+	}
+
+	private void setBackendCrewList() throws IOException{
+		BufferedReader reader = new BufferedReader(
+			new FileReader("C:\\java-pairmatching-precourse\\src\\main\\resources\\backend-crew.md")
+		);
+
+		String name;
+		while ((name = reader.readLine()) != null) {
+			backendCrewList.add(new Crew(Course.BACKEND, name));
+		}
+		reader.close();
+	}
+
+	private void setFrontendCrewList() throws IOException{
+		BufferedReader reader = new BufferedReader(
+			new FileReader("C:\\java-pairmatching-precourse\\src\\main\\resources\\frontend-crew.md")
+		);
+
+		String name;
+		while ((name = reader.readLine()) != null) {
+			frontEndCrewList.add(new Crew(Course.FRONTEND, name));
+		}
+		reader.close();
+	}
+}
