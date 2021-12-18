676fb1e9973fa842de33f30cfb796fa64e5f8872
BeomsooKim <kbsat@naver.com>
2021-12-18 16:19:56 +0900

    feat: Matching, Pair 도메인 생성

---
+++ src/main/java/pairmatching/domain/Matching.java
@@ -0,0 +1,8 @@
+package pairmatching.domain;
+
+import java.util.List;
+
+public class Matching {
+	private List<Pair> pairs;
+	private MatchingOption matchingOption;
+}
---
+++ src/main/java/pairmatching/domain/Pair.java
@@ -0,0 +1,7 @@
+package pairmatching.domain;
+
+import java.util.List;
+
+public class Pair {
+	public List<Crew> pairCrews;
+}
