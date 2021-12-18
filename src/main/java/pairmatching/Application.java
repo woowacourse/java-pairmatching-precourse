f9f6f9e1262956eb214c36dd823ebbbfd4a7ab7e
Kim Seunglae <obey1342@gmail.com>
2021-12-18 14:12:35 +0900

    feat: md파일 읽어서 Crew원들 정보 설정하는 기능 추가

--- src/main/java/pairmatching/Application.java
+++ src/main/java/pairmatching/Application.java
@@ -1,7 +1,38 @@
 package pairmatching;
 
+import static pairmatching.model.CrewRepository.*;
+
+import java.util.List;
+
+import pairmatching.model.Course;
+import pairmatching.model.Crew;
+import pairmatching.utils.FileUtils;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO 구현 진행
-    }
+	public static void main(String[] args) {
+		init();
+	}
+
+	private static void init() {
+		initBackendCrews();
+		initFrontendCrews();
+	}
+
+	private static void initFrontendCrews() {
+		String filePath = "./src/main/resources/frontend-crew.md";
+		List<String> frontendCrews = FileUtils.readList(filePath);
+
+		for (String frontendCrew : frontendCrews) {
+			crews.add(new Crew(Course.FRONTEND, frontendCrew));
+		}
+	}
+
+	private static void initBackendCrews() {
+		String filePath = "./src/main/resources/backend-crew.md";
+		List<String> backendCrews = FileUtils.readList(filePath);
+
+		for (String backendCrew : backendCrews) {
+			crews.add(new Crew(Course.BACKEND, backendCrew));
+		}
+	}
 }
