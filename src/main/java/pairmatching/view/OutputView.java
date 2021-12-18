cbbc8f2b6fc5b45fb705b721cac3ad2f0f3207c2
YJ <soltwood6862@naver.com>
2021-12-18 17:50:27 +0900

    feat :Output 클래스 생성, 코스 레벨 미션 출력메소드, 초기화 메세지 출력 메소드 생성

---
+++ src/main/java/pairmatching/view/OutputView.java
@@ -0,0 +1,15 @@
+package pairmatching.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import static pairmatching.utils.Constant.*;
+
+public class OutputView {
+    public static void printCourseAndMissionList() {
+        System.out.println(COURSE_AND_MISSION_LIST);
+    }
+
+    public static void printReset() {
+        System.out.println(RESET_MESSAGE);
+    }
+}
