502803bb750feb87ee4a5b277ca16557ed116b69
overnew <ehdls1303@gmail.com>
2021-12-18 17:42:20 +0900

    feat: 기능별 심볼 상수화
    기능별 심볼 상수화

--- src/main/java/pairmatching/constants/StringConstant.java
+++ src/main/java/pairmatching/constants/StringConstant.java
@@ -2,6 +2,10 @@ package pairmatching.constants;
 
 public class StringConstant {
     public static final String[] FUNCTION_SYMBOL = {"1","2","3","Q"};
+    public static final String FUNCTION_MATCHING = "1";
+    public static final String FUNCTION_SEARCHING = "2";
+    public static final String FUNCTION_INIT = "3";
+    public static final String FUNCTION_END = "Q";
 
     public static final String ERROR_MESSAGE_PREFIX = "[ERROR]: ";
 }
