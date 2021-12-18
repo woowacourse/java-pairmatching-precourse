b31db622bc43a96a68ee60fac0e1dc3c8d117538
zmfl1230 <zmfl123097@gmail.com>
2021-12-18 15:09:00 +0900

    feat[Pair-Domain]: insert matched information in hashmap

--- src/main/java/pairmatching/domain/Pair.java
+++ src/main/java/pairmatching/domain/Pair.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Objects;
 
@@ -13,12 +14,14 @@ public class Pair {
     Course course;
     String mission;
     List<String> crewNames;
+    HashMap<String, String> alreadyMatched;
 
     public Pair(Course course, String mission) throws IOException {
         this.course = course;
         this.mission = mission;
 
         shuffledCrew();
+        insertMatchedMap();
     }
 
     private void shuffledCrew() throws IOException {
@@ -29,6 +32,34 @@ public class Pair {
         br.close();
 
         crewNames = Randoms.shuffle(crewNames);
+    }
+
+    private void insertMatchedMap() {
+        int totalSize = calculateTotalSize();
+
+        for (int crewNumber = 0; crewNumber < totalSize;) {
+            // TODO: 중복 검사
+            alreadyMatched.put(crewNames.get(crewNumber), crewNames.get(crewNumber+1));
+            alreadyMatched.put(crewNames.get(crewNumber+1), crewNames.get(crewNumber));
+            crewNumber = crewNumber + 2;
+        }
+        insertRestCrew(totalSize);
+    }
+
+    private int calculateTotalSize() {
+        if (crewNames.size() % 2 != 0) return crewNames.size() - 3;
+        return crewNames.size();
+    }
+
+    private void insertRestCrew(int total) {
+        if (total ==  crewNames.size()) return;
+        int[] right = {1, -1, -2};
+        int[] left = {2, 1, -1};
+
+        for (int index = 0; index < 3; index++) {
+            alreadyMatched.put(crewNames.get(crewNames.size()-3+index), crewNames.get(crewNames.size()-3+right[index]));
+            alreadyMatched.put(crewNames.get(crewNames.size()-3+index), crewNames.get(crewNames.size()-3+left[index]));
+        }
     }
 
     @Override
