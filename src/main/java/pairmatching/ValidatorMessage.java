0fd8d0d794ed626270991a41232f97681f52015d
TaeHyeon Kim <kth990303@naver.com>
2021-12-18 16:56:30 +0900

    feat: 이미 페어로 만난 적 있을 때 에러메시지 작성

--- src/main/java/pairmatching/ValidatorMessage.java
+++ src/main/java/pairmatching/ValidatorMessage.java
@@ -5,7 +5,7 @@ public class ValidatorMessage {
 
     public static final String INVALID_FUNCTION = "존재하지 않는 기능입니다.";
     public static final String NO_MISSION = "존재하지 않는 미션입니다.";
-    public static final String NO_MATCHING_RECORD = "매칭 이력이 없습니다.";
+    public static final String DUPLICATE_MATCHING = "매칭에 실패하였습니다.";
 
     public static void printError(String errorMessage) {
         System.out.println(errorMessage);
