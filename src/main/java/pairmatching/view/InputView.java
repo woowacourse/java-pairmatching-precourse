a33f010b3f741a22c492f8bde7aae4dd72282a2a
pup-paw <wldusdl0310@gmail.com>
2021-12-18 16:27:06 +0900

    feat: 매칭 기능에서 사용자 입력값을 받아오는 기능 생성, 메뉴 선택의 예외처리 추가

--- src/main/java/pairmatching/view/InputView.java
+++ src/main/java/pairmatching/view/InputView.java
@@ -1,9 +1,30 @@
 package pairmatching.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import pairmatching.controller.ServiceController;
+import pairmatching.validator.ChoiceFormatValidator;
+import pairmatching.validator.MatchingInfoFormatValidator;
 
 public class InputView {
 	public static String askFunction() {
-		return Console.readLine();
+		return ChoiceFormatValidator.isValidChoice(Console.readLine());
+	}
+
+	public static String[] askMatchingInfo() {
+		String input = Console.readLine();
+		String[] matchingInfo = splitMatchingInfo(input);
+		return isExist(matchingInfo);
+	}
+
+	private static String[] isExist(String[] matchingInfo) {
+		MatchingInfoFormatValidator.isInCourse(matchingInfo[0]);
+		MatchingInfoFormatValidator.isInLevel(matchingInfo[1]);
+		MatchingInfoFormatValidator.isInMission(ServiceController.missions, matchingInfo[2]);
+		return matchingInfo;
+	}
+
+	private static String[] splitMatchingInfo(String input) {
+		String[] matchingInfo = input.split(", ");
+		return MatchingInfoFormatValidator.isNumberOfInfo3(matchingInfo);
 	}
 }
