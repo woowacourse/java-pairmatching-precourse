d0fba292d47cbfd3819d7165f7e1b324c251789f
nailseong <nnis9857@gmail.com>
2021-12-18 14:27:33 +0900

    feat: Menu 클래스 추가

---
+++ src/main/java/pairmatching/domain/Menu.java
@@ -0,0 +1,17 @@
+package pairmatching.domain;
+
+public class Menu {
+	private Course course;
+	private Level level;
+	private Mission mission;
+
+	public Menu(Course course, Level level, Mission mission) {
+		this.course = course;
+		this.level = level;
+		this.mission = mission;
+	}
+
+	public Menu() {
+	}
+
+}
