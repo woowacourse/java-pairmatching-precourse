d1c4fa6765fd92e3a514d8c05004010d76672f9e
juwon <58173061+juwon0605@users.noreply.github.com>
2021-12-18 13:34:16 +0900

    style(root): 네이버 핵데이 자바 컨벤션 적용

--- src/main/java/pairmatching/Application.java
+++ src/main/java/pairmatching/Application.java
@@ -1,7 +1,7 @@
 package pairmatching;
 
 public class Application {
-    public static void main(String[] args) {
-        // TODO 구현 진행
-    }
+	public static void main(String[] args) {
+		// TODO 구현 진행
+	}
 }
