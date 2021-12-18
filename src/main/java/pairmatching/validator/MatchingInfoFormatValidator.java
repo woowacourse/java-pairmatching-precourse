d5979f25ebca91dfe2f243f1ef972fb01c30b97f
pup-paw <wldusdl0310@gmail.com>
2021-12-18 16:25:45 +0900

    feat: 매칭 조건에 대한 입력값이 유효한 값인지 검사

---
+++ src/main/java/pairmatching/validator/MatchingInfoFormatValidator.java
@@ -0,0 +1,56 @@
+package pairmatching.validator;
+
+import java.util.ArrayList;
+
+import pairmatching.model.Course;
+import pairmatching.model.Level;
+import pairmatching.model.Mission;
+import pairmatching.util.ErrorMessage;
+
+public class MatchingInfoFormatValidator {
+	public static String[] isNumberOfInfo3(String[] input) {
+		if (input.length != 3) {
+			throw new IllegalArgumentException(ErrorMessage.NUMBER_OF_INFO_IS_NOT_3);
+		}
+		return input;
+	}
+
+	public static void isInCourse(String input) {
+		int count = 0;
+		for (Course course : Course.values()) {
+			if (course.getName().equals(input)) {
+				count += 1;
+				break;
+			}
+		}
+		if (count == 0) {
+			throw new IllegalArgumentException(ErrorMessage.IS_NOT_IN_COURSE);
+		}
+	}
+
+	public static void isInLevel(String input) {
+		int count = 0;
+		for (Level level : Level.values()) {
+			if (level.getName().equals(input)) {
+				count += 1;
+				break;
+			}
+		}
+		if (count == 0) {
+			throw new IllegalArgumentException(ErrorMessage.IS_NOT_IN_LEVEL);
+		}
+	}
+
+	public static void isInMission(ArrayList<Mission> missions, String input) {
+		int count = 0;
+		for (Mission mission : missions) {
+			if (mission.getName().equals(input)) {
+				count += 1;
+				break;
+			}
+		}
+		if (count == 0) {
+			throw new IllegalArgumentException(ErrorMessage.IS_NOT_IN_MISSION);
+		}
+	}
+}
