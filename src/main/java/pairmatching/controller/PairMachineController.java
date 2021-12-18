cff1a4f21ec4de2006f4573621147d3f2d8995cf
jochungbeom <cndqjacndqja@gmail.com>
2021-12-18 13:38:10 +0900

    feat: 기능 선택에 따라 수행 구현

--- src/main/java/pairmatching/controller/PairMachineController.java
+++ src/main/java/pairmatching/controller/PairMachineController.java
@@ -1,9 +1,14 @@
 package pairmatching.controller;
 
+import static pairmatching.view.InputView.*;
 import static pairmatching.view.OutputView.*;
+
+import pairmatching.domain.PairMachineFunction;
 
 public class PairMachineController {
 	public void run() {
 		printMainScreen();
+		PairMachineFunction pairMachineFunction = PairMachineFunction.findFunction(inputPairFunction());
+		pairMachineFunction.apply();
 	}
 }
--- src/main/java/pairmatching/domain/PairMachineFunction.java
+++ src/main/java/pairmatching/domain/PairMachineFunction.java
@@ -1,5 +1,7 @@
 package pairmatching.domain;
 
+import java.util.Arrays;
+
 public enum PairMachineFunction {
 	PAIR_MATCHING("1"){
 		@Override
@@ -24,13 +26,27 @@ public enum PairMachineFunction {
 		void function() {
 
 		}
-	}
-	;
+	};
 
 	private String command;
 	abstract void function();
 
 	PairMachineFunction(String command) {
 		this.command = command;
+	}
+
+	public String getCommand() {
+		return command;
+	}
+
+	public static PairMachineFunction findFunction(String command){
+		return Arrays.stream(values())
+			.filter(function -> function.command.equals(command))
+			.findFirst()
+			.orElseThrow(() -> new IllegalArgumentException("올바르지 않은 명령어 입니다."));
+	}
+
+	public void apply() {
+		function();
 	}
 }
