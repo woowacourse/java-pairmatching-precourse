ef923b6fe5c18b940199fd7fcaedc2a3b0c8bba8
jurlring <78091011+jurlring@users.noreply.github.com>
2021-12-18 15:46:57 +0900

    feat : 매칭된 경우 정보 출력 기능 구현

--- src/main/java/pairmatching/controller/PairMatchingController.java
+++ src/main/java/pairmatching/controller/PairMatchingController.java
@@ -4,6 +4,9 @@ import static pairmatching.exception.EmptyException.*;
 import static pairmatching.exception.FunctionException.*;
 import static pairmatching.exception.SourceFormatException.*;
 
+import java.io.IOException;
+
+import pairmatching.model.Match;
 import pairmatching.service.PairService;
 import pairmatching.view.InputView;
 import pairmatching.view.OutputView;
@@ -33,7 +36,7 @@ public class PairMatchingController {
 		} while (!function.equals("Q"));
 	}
 
-	private void proceedFunction(String function) {
+	private void proceedFunction(String function) throws IOException {
 		if (function.equals("1")) {
 			matchPair(checkEmptyInput(inputView.requestSource()));
 		}
@@ -45,7 +48,7 @@ public class PairMatchingController {
 		}
 	}
 
-	private void matchPair(String source) {
+	private void matchPair(String source) throws IOException {
 		checkSourceFormat(source);
 		String[] sourceSplitByComma = source.replaceAll(" ", "").split(",");
 		if (pairService.findMatch(sourceSplitByComma[0], sourceSplitByComma[1], sourceSplitByComma[2])) {
@@ -54,6 +57,7 @@ public class PairMatchingController {
 				run();
 			}
 		}
-		pairService.match(sourceSplitByComma[0], sourceSplitByComma[1], sourceSplitByComma[2]);
+		Match match = pairService.match(sourceSplitByComma[0], sourceSplitByComma[1], sourceSplitByComma[2]);
+		outputView.reportMatch(match.getPair());
 	}
 }
--- src/main/java/pairmatching/view/OutputView.java
+++ src/main/java/pairmatching/view/OutputView.java
@@ -1,10 +1,16 @@
 package pairmatching.view;
 
+import java.util.List;
+
 public class OutputView {
 
 	static final String ERROR_MSG = "[ERROR] ";
 
 	public void reportException(String message) {
 		System.out.println(ERROR_MSG + message);
+	}
+
+	public void reportMatch(List<String> pair) {
+		System.out.println(pair);
 	}
 }
