ebfc099ae6775233acba556967d2c5ba3bdeb554
xi_jjun <rlawowns97@naver.com>
2021-12-18 17:49:39 +0900

    feat: add program run

--- src/main/java/pairmatching/Application.java
+++ src/main/java/pairmatching/Application.java
@@ -1,7 +1,16 @@
 package pairmatching;
 
+import static pairmatching.utils.Constant.*;
+
+import pairmatching.controller.MainController;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO 구현 진행
-    }
+	public static void main(String[] args) {
+		// TODO 구현 진행
+		MainController pairMatching = new MainController();
+		String run = START;
+		while (run.equals(AGAIN)) {
+			run = pairMatching.run();
+		}
+	}
 }
