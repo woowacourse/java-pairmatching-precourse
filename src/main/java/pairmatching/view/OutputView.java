4affac92e72f722f6164d1f8e924c3a96ee44528
pup-paw <wldusdl0310@gmail.com>
2021-12-18 16:27:45 +0900

    feat: 사용자에게 매칭할 조건을 물어보고, 매칭된 값을 반환

--- src/main/java/pairmatching/view/OutputView.java
+++ src/main/java/pairmatching/view/OutputView.java
@@ -1,5 +1,8 @@
 package pairmatching.view;
 
+import pairmatching.controller.MatchingController;
+import pairmatching.model.Course;
+import pairmatching.model.Level;
 import pairmatching.util.HostMessage;
 
 public class OutputView {
@@ -11,7 +14,32 @@ public class OutputView {
 		System.out.println(HostMessage.FUNCTION_Q);
 	}
 
-	public static void showDetailInfo() {
+	public static void askMatchingInfo() {
+		System.out.println("\n" + HostMessage.LINE_SEPARATOR);
+		showCourse();
+		showMission();
+		System.out.println(HostMessage.LINE_SEPARATOR);
+		System.out.println(HostMessage.ASK_MATCHING_INFO);
+		System.out.println(HostMessage.SHOW_MATCHING_INFO_EX);
+	}
 
+	public static void showCourse() {
+		System.out.println(HostMessage.SHOW_COURSE + Course.BACKEND.getName() + " | " + Course.FRONTEND.getName());
+	}
+
+	public static void showMission() {
+		System.out.println(HostMessage.SHOW_MISSION);
+		int mission_num = 0;
+		for (Level level : Level.values()) {
+			System.out.println("  - " + level.getName() + HostMessage.MISSION_NAMES[mission_num]);
+			mission_num++;
+		}
+	}
+
+	public static void showMatchingResult() {
+		System.out.println("\n" + HostMessage.SHOW_MATCHING_RESULT);
+		for (String pair : MatchingController.pairs) {
+			System.out.println(pair);
+		}
 	}
 }
