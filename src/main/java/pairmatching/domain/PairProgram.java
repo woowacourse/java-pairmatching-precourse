73cea63226507511ae4cbe47be21a27a6ab86252
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2021-12-18 15:00:07 +0900

    feat: Match 생성기능 구현 (짝수, 홀수 인원인 경우 체크)

--- src/main/java/pairmatching/domain/PairProgram.java
+++ src/main/java/pairmatching/domain/PairProgram.java
@@ -1,5 +1,10 @@
 package pairmatching.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class PairProgram {
 
     private final Crews crews;
@@ -13,5 +18,46 @@ public class PairProgram {
     public boolean isAlreadyMatch(Level level, Mission mission) {
         levelMission.checkExistMission(level, mission);
         return levelMission.isAlreadyMatch(level, mission);
+    }
+
+    public List<Match> createMatch(List<Crew> crews, Course course) {
+        List<String> crewNames = crews.stream()
+            .map(Crew::name)
+            .collect(Collectors.toList());
+        crewNames = Randoms.shuffle(crewNames);
+        return createMatch(course, crewNames);
+    }
+
+    private List<Match> createMatch(Course course, List<String> crewNames) {
+        if (crewNames.size() % 2 == 0) {
+            return createEvenMatch(course, crewNames);
+        }
+        return createOddMatch(course, crewNames);
+    }
+
+    private List<Match> createEvenMatch(Course course, List<String> crewNames) {
+        List<Match> matches = new ArrayList<>();
+        for (int i = 0; i < crewNames.size(); i += 2) {
+            String firstName = crewNames.get(i);
+            String secondName = crewNames.get(i + 1);
+            matches.add(new Match(this.crews.findCrew(firstName, course), this.crews.findCrew(secondName, course)));
+        }
+        return matches;
+    }
+
+    private List<Match> createOddMatch(Course course, List<String> crewNames) {
+        List<Match> matches = new ArrayList<>();
+        int i = 0;
+        for (; i < crewNames.size() - 3; i += 2) {
+            String firstName = crewNames.get(i);
+            String secondName = crewNames.get(i + 1);
+            matches.add(new Match(this.crews.findCrew(firstName, course), this.crews.findCrew(secondName, course)));
+        }
+        String firstName = crewNames.get(i);
+        String secondName = crewNames.get(i + 1);
+        String thirdName = crewNames.get(i + 2);
+        matches.add(new OddMatch(this.crews.findCrew(firstName, course), this.crews.findCrew(secondName, course),
+            this.crews.findCrew(thirdName, course)));
+        return matches;
     }
 }
