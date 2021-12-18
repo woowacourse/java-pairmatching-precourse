f71195b1e220ab58d674d8720adc12ba4ceb3dec
Kim Seunglae <obey1342@gmail.com>
2021-12-18 13:51:46 +0900

    feat: Level클래스 추가

---
+++ src/main/java/pairmatching/model/Level.java
@@ -0,0 +1,15 @@
+package pairmatching.model;
+
+public enum Level {
+	LEVEL1("레벨1"),
+	LEVEL2("레벨2"),
+	LEVEL3("레벨3"),
+	LEVEL4("레벨4"),
+	LEVEL5("레벨5");
+
+	private String name;
+
+	Level(String name) {
+		this.name = name;
+	}
+}
