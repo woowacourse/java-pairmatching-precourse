dc9d56fdccfbd9cae7a6285d1c627206e2724c47
delphox60 <delphox60@gmail.com>
2021-12-18 14:11:47 +0900

    feat(view): 과정, 미션 정보를 입력받는 기능 구현

--- src/main/java/pairmatching/view/ConsoleInputView.java
+++ src/main/java/pairmatching/view/ConsoleInputView.java
@@ -2,13 +2,24 @@ package pairmatching.view;
 
 public class ConsoleInputView {
 
-    private static  final String NEW_LINE = "\n";
-    private static  final String INPUT_FUNCTION_REQUEST_MESSAGE = "기능을 선택하세요.";
-    private static  final String INPUT_FUNCTION_INFO_MESSAGE = "\n1. 페어 매칭\n2. 페어 조회\n3. 페어 초기화\nQ. 종료";
+    private static final String NEW_LINE = "\n";
+    private static final String INPUT_FUNCTION_REQUEST_MESSAGE = "기능을 선택하세요.";
+    private static final String INPUT_MATCHING_INFO_REQUEST_MESSAGE = "과정, 레벨, 미션을 선택하세요.";
+    private static final String INPUT_FUNCTION_INFO_MESSAGE = "\n1. 페어 매칭\n2. 페어 조회\n3. 페어 초기화\nQ. 종료";
 
     public static String inputFunction() {
         System.out.println(INPUT_FUNCTION_REQUEST_MESSAGE);
         System.out.println(INPUT_FUNCTION_INFO_MESSAGE);
+
+        String input = camp.nextstep.edu.missionutils.Console.readLine();
+
+        System.out.println(NEW_LINE);
+        return input;
+    }
+
+    public static String inputMatchinInfo() {
+        OutputView.printCourseLevelMissionInfo();
+        System.out.println(INPUT_MATCHING_INFO_REQUEST_MESSAGE);
 
         String input = camp.nextstep.edu.missionutils.Console.readLine();
 
--- src/main/java/pairmatching/view/OutputView.java
+++ src/main/java/pairmatching/view/OutputView.java
@@ -3,8 +3,21 @@ package pairmatching.view;
 public class OutputView {
 
     private static final String ERROR_SYMBOL = "[ERROR] ";
+    private static final String COURSE_LENEL_MISSION_INFO = "#############################################\n"
+        + "과정: 백엔드 | 프론트엔드\n"
+        + "미션:\n"
+        + "  - 레벨1: 자동차경주 | 로또 | 숫자야구게임\n"
+        + "  - 레벨2: 장바구니 | 결제 | 지하철노선도\n"
+        + "  - 레벨3: \n"
+        + "  - 레벨4: 성능개선 | 배포\n"
+        + "  - 레벨5: \n"
+        + "############################################";
 
     public static void printErrorMessage(final String errorMessage) {
         System.out.println(ERROR_SYMBOL + errorMessage);
+    }
+
+    public static void printCourseLevelMissionInfo() {
+        System.out.println(COURSE_LENEL_MISSION_INFO);
     }
 }
