31b129c87d8ff4272590a724e7255316511890c8
Wishoon <heonga2@gmail.com>
2021-12-18 17:59:15 +0900

    feat: 미션 이름 추가

--- src/main/java/pairmatching/domain/MissionType.java
+++ src/main/java/pairmatching/domain/MissionType.java
@@ -5,7 +5,13 @@ import java.util.Arrays;
 public enum MissionType {
 
 	RACING_CAR("자동차경주"),
-	LOTTO("로또");
+	LOTTO("로또"),
+	NUMBER_BASEBALL("숫자야구게임"),
+	BASKET("장바구니"),
+	PAYMENT("결제"),
+	SUBWAY_LINE_MAP("지하철노선도"),
+	PERFORMANCE_IMPROVEMENT("성능개선"),
+	DISTRIBUTION("배포");
 
 	private static final String ERROR_NOT_MISSION = " 은 등록되지 않은 미션입니다.";
 	private final String name;
