52a26f79d11b489ff348b8289479af14c7a878ff
Jeong Satchel <bugod96@gmail.com>
2021-12-18 13:39:22 +0900

    feat(PairMatchingController): 메인 화면에서 프로그램 종료될 때까지 계속 사용자 입력 받기
    - 예외 발생시, 다시 사용자 입력 요청

--- src/main/java/pairmatching/controller/PairMatchingController.java
+++ src/main/java/pairmatching/controller/PairMatchingController.java
@@ -1,12 +1,44 @@
 package pairmatching.controller;
 
+import static pairmatching.constants.SystemConstants.*;
 import static pairmatching.utils.UserChoiceValidator.validateMainMenuActionInput;
 import static pairmatching.view.InputView.requestMainMenuInput;
 
 public class PairMatchingController {
 
     public void run() {
-        String userInput = requestMainMenuInput();
-        validateMainMenuActionInput(userInput);
+        boolean isRunning = true;
+
+        while (isRunning) {
+            String userChoice = getUserInput();
+            isRunning = runUserChoice(userChoice);
+        }
+    }
+
+    private boolean runUserChoice(String userChoice) {
+        if (userChoice.equals(EXIT_Q)) return false;
+
+        if (userChoice.equals(ONE)) {
+            // GO TO PAIR MATCHING SCREEN
+        }
+        if (userChoice.equals(TWO)) {
+            // GO TO PAIR READ SCREEN
+        }
+        if (userChoice.equals(THREE)) {
+            // 초기화
+        }
+        return true;
+    }
+
+    private String getUserInput() {
+        while(true) {
+            try {
+                String userInput = requestMainMenuInput();
+                validateMainMenuActionInput(userInput);
+                return userInput;
+            } catch (IllegalArgumentException e) {
+                System.out.print(e.getMessage());
+            }
+        }
     }
 }
