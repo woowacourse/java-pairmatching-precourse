236ebf3396dd8f5f0cd5744c3157ccc894b87d85
pup-paw <wldusdl0310@gmail.com>
2021-12-18 13:32:52 +0900

    feat: 기능 선택 목록 출력

---
+++ src/main/java/pairmatching/view/OutputView.java
@@ -0,0 +1,17 @@
+package pairmatching.view;
+
+import pairmatching.util.HostMessage;
+
+public class OutputView {
+	public static void askFunction() {
+		System.out.println(HostMessage.ASK_FUNCTION);
+		System.out.println(HostMessage.FUNCTION_1);
+		System.out.println(HostMessage.FUNCTION_2);
+		System.out.println(HostMessage.FUNCTION_3);
+		System.out.println(HostMessage.FUNCTION_Q);
+	}
+
+	public static void showDetailInfo() {
+
+	}
+}
