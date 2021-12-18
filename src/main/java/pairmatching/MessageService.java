0479d585092544e4d08976fe4e33cc4524e3870a
qbang <vhfhddl0101@naver.com>
2021-12-18 15:00:37 +0900

    feat: 기능 선택하기

---
+++ src/main/java/pairmatching/MessageService.java
@@ -0,0 +1,9 @@
+package pairmatching;
+
+import static pairmatching.Message.*;
+
+public class MessageService {
+    public void printWithMessage(String message) {
+        System.out.println(message);
+    }
+}
