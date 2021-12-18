c08d736fe74b38fcd5890e0782577282bdac80c6
DWL21 <nggus5@naver.com>
2021-12-18 14:46:43 +0900

    feat: 기능목록을 출력, 입력받는 기능

--- src/main/java/pairmatching/Controller.java
+++ src/main/java/pairmatching/Controller.java
@@ -1,9 +1,55 @@
 package pairmatching;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Controller {
     Crews crews = new Crews();
+
+    String input;
+
     public void start() {
         crews.readFrontend();
         crews.readBackend();
     }
+
+    private void program() {
+
+    }
+
+    public void selectFunctions() {
+        System.out.println(Message.INPUT_FUNCTIONS);
+        boolean check = false;
+        while (!check) {
+            input();
+            check = inputFunctions();
+        }
+
+    }
+
+    private boolean inputFunctions() {
+        try {
+            boolean check = checkFunctions();
+            if (!check) {
+                throw new IllegalArgumentException();
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println(Message.ERROR_INPUT_FUNCTIONS);
+            return false;
+        }
+        return true;
+    }
+
+    public void input() {
+        input = Console.readLine();
+    }
+
+    private boolean checkFunctions(){
+        if (input.length() == 1 && (input.charAt(0) == '1' || input.charAt(0) == '2' || input.charAt(0) == '3' || input.charAt(0) == 'Q')) {
+            return true;
+        }
+        return false;
+    }
+
+
 }
---
+++ src/main/java/pairmatching/Message.java
@@ -0,0 +1,7 @@
+package pairmatching;
+
+public class Message {
+    public static String INPUT_FUNCTIONS = "기능을 선택하세요.\n1. 페어 매칭\n2. 페어 조회\n3. 페어 초기화\nQ. 종료";
+    public static String ERROR_INPUT_FUNCTIONS = "잘못된 입력입니다.";
+
+}
