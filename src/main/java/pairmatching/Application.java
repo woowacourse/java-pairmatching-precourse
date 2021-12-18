dd7498384337249dfd317c03857dfc19963d8624
Seungmin Lim <seungmin9066@gmail.com>
2021-12-18 15:29:18 +0900

    [추가] 리스트의 목록이 잘 가져와는지 확인 코드

--- src/main/java/pairmatching/Application.java
+++ src/main/java/pairmatching/Application.java
@@ -1,7 +1,10 @@
 package pairmatching;
 
+import pairmatching.domain.CrewGroup;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO 구현 진행
-    }
+	public static void main(String[] args) {
+		CrewGroup crewGroup = new CrewGroup();
+		System.out.println(crewGroup.getShuffledCrews());
+	}
 }
