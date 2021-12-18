5f4e93ff8fbc204102a15a45378c03d8f834a0d5
jayjaehunchoigit config --global user.name jayjaehunchoigit config --global user.name jayjaehunchoi <wogns0108@naver.com>
2021-12-18 15:04:54 +0900

    test(PairRepositoryTest) : Pair 저장, 탐색 테스트 통과

---
+++ src/test/java/pairmatching/repository/PairRepositoryTest.java
@@ -0,0 +1,34 @@
+package pairmatching.repository;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import pairmatching.domain.Pair;
+import pairmatching.domain.enumeration.Mission;
+import pairmatching.domain.enumeration.Pairs;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class PairRepositoryTest {
+
+    PairRepository pairRepository;
+
+    @BeforeEach
+    void setUp() {
+        pairRepository = new PairRepository();
+    }
+
+    @Test
+    void 페어_저장() {
+        Pairs pairs = new Pairs();
+        pairs.addPair(new Pair());
+        pairRepository.createPairs(Mission.CAR_RACING, pairs);
+
+        assertThat(pairRepository.findByMission(Mission.CAR_RACING)
+                .getPairList()
+                .size())
+                .isEqualTo(1);
+
+    }
+
+}
