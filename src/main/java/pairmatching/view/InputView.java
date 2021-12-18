66890154294ad17cc72557f00b2e1558b82be434
Richard JEON <ztzy1907@gmail.com>
2021-12-18 15:38:52 +0900

    feat(inputview): print course and mission info

--- src/main/java/pairmatching/view/InputView.java
+++ src/main/java/pairmatching/view/InputView.java
@@ -1,5 +1,10 @@
 package pairmatching.view;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import pairmatching.model.Course;
+
 public class InputView {
 	public static final String REQUEST_FOR_FUNCTION_OPTION_INPUT = "기능을 선택하세요.";
 
@@ -9,5 +14,18 @@ public class InputView {
 		System.out.println("2. 페어 조회");
 		System.out.println("3. 페어 초기화");
 		System.out.println("Q. 종료");
+	}
+
+	public static void printCourseInfo() {
+		System.out.println("\n#############################################");
+		String courseInfo = Arrays.stream(Course.values()).map(Course::getName).collect(Collectors.joining(" | "));
+		System.out.printf("과정: %s\n", courseInfo);
+		System.out.println("미션:");
+		System.out.println("  - 레벨1: 자동차경주 | 로또 | 숫자야구게임");
+		System.out.println("  - 레벨2: 장바구니 | 결제 | 지하철노선도");
+		System.out.println("  - 레벨3:");
+		System.out.println("  - 레벨4: 성능개선 | 배포");
+		System.out.println("  - 레벨5:");
+		System.out.println("#############################################");
 	}
 }
