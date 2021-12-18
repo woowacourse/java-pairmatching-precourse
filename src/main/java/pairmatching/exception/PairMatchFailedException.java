80b7da4cd36bb41a7021c0d6ac7b5754ed29ae65
CptBluebear <cptbluebear@naver.com>
2021-12-18 17:08:07 +0900

    feat: 페어 매칭 실패 예외 추가

---
+++ src/main/java/pairmatching/exception/PairMatchFailedException.java
@@ -0,0 +1,7 @@
+package pairmatching.exception;
+
+public class PairMatchFailedException extends IllegalArgumentException {
+	public PairMatchFailedException(String message) {
+		super(message);
+	}
+}
