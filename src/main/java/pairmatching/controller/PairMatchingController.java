f0bc6b4a0a362474be62992120e91c482f9df9eb
SuyeonChoi <tndus970120@gmail.com>
2021-12-18 16:52:55 +0900

    fix(PairMatchingController): 페어매칭을 선택했을때만 매칭이 실행되도록 변경

--- src/main/java/pairmatching/controller/PairMatchingController.java
+++ src/main/java/pairmatching/controller/PairMatchingController.java
@@ -7,14 +7,24 @@ import pairmatching.view.PairMatchingInputView;
 
 public class PairMatchingController {
 	private List<String> crewNames; // 역대 매칭된 페어정보만
+
 	public PairMatchingController() {
 	}
 
 	public void run() {
-		while (!PairMatchingInputView.readFunction().equals("Q")) {
-			// 페어 매칭
-			String inputMatchingCondition = PairMatchingInputView.readCourseAndMission();
-			Matching pair = new Matching(inputMatchingCondition);
+		while (true) {
+			String inputFunction = PairMatchingInputView.readFunction();
+			if (inputFunction.equals("1")) {
+				match();
+			}
+			if (inputFunction.equals("Q")) {
+				break;
+			}
 		}
+	}
+
+	private void match() {
+		String inputMatchingCondition = PairMatchingInputView.readCourseAndMission();
+		Matching matching = new Matching(inputMatchingCondition);
 	}
 }
