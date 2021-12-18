0de4c569ae9c873b2566e18bb3afa6a62ee7f239
khj1 <74335027+khj1@users.noreply.github.com>
2021-12-18 14:35:47 +0900

    feat: 과정을 관리하는 클래스 추가

---
+++ src/main/java/pairmatching/model/Course.java
@@ -0,0 +1,27 @@
+package pairmatching.model;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public enum Course {
+	BACKEND("백엔드"),
+	FRONTEND("프론트엔드");
+
+	private String name;
+
+	Course(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public static List<String> getNames() {
+		return Arrays.stream(values())
+			.map(Course::getName)
+			.collect(Collectors.toList());
+	}
+
+}
