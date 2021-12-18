5d9dbb2746a2605aca9d76e027cdcaaba9788ccb
khj1 <74335027+khj1@users.noreply.github.com>
2021-12-18 14:36:31 +0900

    feat: 레벨을 관리하는 클래스 추가

---
+++ src/main/java/pairmatching/model/Level.java
@@ -0,0 +1,29 @@
+package pairmatching.model;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public enum Level {
+	LEVEL1("레벨1"),
+	LEVEL2("레벨2"),
+	LEVEL3("레벨3"),
+	LEVEL4("레벨4"),
+	LEVEL5("레벨5");
+
+	private String name;
+
+	Level(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public static List<String> getNames() {
+		return Arrays.stream(values())
+			.map(Level::getName)
+			.collect(Collectors.toList());
+	}
+}
