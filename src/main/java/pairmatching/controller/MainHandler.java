ec65404fb784354e998e66c70bed5426c8910051
Wishoon <heonga2@gmail.com>
2021-12-18 14:32:52 +0900

    feat: 입력 받은 매칭 명령으로 페어 매칭 프로세스의 유효성을 검증하는 기능 추가

--- src/main/java/pairmatching/controller/MainHandler.java
+++ src/main/java/pairmatching/controller/MainHandler.java
@@ -1,7 +1,27 @@
 package pairmatching.controller;
 
+import pairmatching.domain.Course;
+import pairmatching.domain.Level;
+import pairmatching.view.InputView;
+
 public class MainHandler {
+	private static final String COMMAND_SPLIT = ",";
+	private static final int COMMAND_SPLIT_LIMIT = -1;
+	private static final int COURSE_INDEX = 0;
+	private static final int LEVEL_INDEX = 1;
+	private static final int MISSION_INDEX = 2;
+
 	public static void matchingManager() {
+		try {
+			final String[] command = InputView.getInputPairMatching().split(COMMAND_SPLIT, COMMAND_SPLIT_LIMIT);
+			Course.findByCourse(command[COURSE_INDEX]);
+			Level.findByLevel(command[LEVEL_INDEX]);
+			Level.findByMission(command[MISSION_INDEX]);
+
+			// 페어 매칭 결과입니다.
+		} catch (IllegalArgumentException e) {
+			matchingManager();
+		}
 	}
 
 	public static void referenceManager() {
