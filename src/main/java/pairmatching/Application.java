04ebdb2935bde664293ec826aae3355df471ef6d
jaeyoungjang2 <lion85488@gmail.com>
2021-12-18 14:01:55 +0900

    feat: pairProgramming controller 생성  - 프로트엔드, 백엔드 크루를 파일로 읽어들이는 과정에서 오류가 있어 현재는 임시 리스트로 구현

--- src/main/java/pairmatching/Application.java
+++ src/main/java/pairmatching/Application.java
@@ -1,7 +1,11 @@
 package pairmatching;
 
+import pairmatching.controller.PairProgrammingController;
+
 public class Application {
     public static void main(String[] args) {
         // TODO 구현 진행
+        PairProgrammingController pairProgrammingController = new PairProgrammingController();
+        pairProgrammingController.run();
     }
 }
