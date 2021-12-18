04724636aa01ada080c65908bf28c7acd1761240
pup-paw <wldusdl0310@gmail.com>
2021-12-18 16:23:03 +0900

    feat: 프로젝트 내에서 발생하는 예외 메세지 저장

---
+++ src/main/java/pairmatching/util/ErrorMessage.java
@@ -0,0 +1,12 @@
+package pairmatching.util;
+
+public class ErrorMessage {
+	public static final String ERROR = "[ERROR] ";
+	public static final String NUMBER_OF_INFO_IS_NOT_3 = ERROR + "과정, 레벨, 미션 3가지만 입력해 주세요.";
+	public static final String IS_NOT_IN_COURSE = ERROR + "제공된 과정 중 선택하여 입력해 주세요.";
+	public static final String IS_NOT_IN_LEVEL = ERROR + "제공된 레벨 중 선택하여 입력해 주세요.";
+	public static final String IS_NOT_IN_MISSION = ERROR + "제공된 미션 중 선택하여 입력해 주세요.";
+	public static final String IS_NOT_VALID_CHOICE = ERROR + "제공된 보기 중 선택하여 입력해 주세요.";
+	public static final String FILE_NOT_FOUND = ERROR + "크루 명단을 찾을 수 없습니다.";
+	public static final String NO_MORE_COMBINATION = ERROR + "더 이상 매칭할 수 있는 경우의 수가 없습니다.";
+}
