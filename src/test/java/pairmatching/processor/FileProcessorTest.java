ef3312da04e9a1ecf251dae4ff50d8f384350376
GYUTAEKIM <kkt219a@gmail.com>
2021-12-18 14:58:09 +0900

    test: 크루 이름 파일입력 테스트코드 작성
    - 크루 이름이 정상적으로 입력될 수 있는지 테스트한다.

---
+++ src/test/java/pairmatching/processor/FileProcessorTest.java
@@ -0,0 +1,32 @@
+package pairmatching.processor;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import pairmatching.domain.Course;
+
+class FileProcessorTest {
+	@Test
+	public void crewMemberRead() {
+		FileProcessor fileProcessor = new FileProcessor();
+		try {
+			List<String> backEndCrewNames = fileProcessor.readCrewNames(Course.BACKEND);
+			List<String> frontEndCrewNames = fileProcessor.readCrewNames(Course.FRONTEND);
+			printReadNames(backEndCrewNames,Course.BACKEND);
+			printReadNames(frontEndCrewNames,Course.FRONTEND);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void printReadNames(List<String> crewNames, Course course) {
+		System.out.println(course.getName() + " 조회 결과");
+		for (String crewName : crewNames) {
+			System.out.println(crewName);
+		}
+	}
+}
