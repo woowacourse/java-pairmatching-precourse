27d5fcdbccabddf9f6fcf7d0c2be4b0daebf7f49
GYUTAEKIM <kkt219a@gmail.com>
2021-12-18 15:30:29 +0900

    feat: 기능이 실제로 존재하는지 검증 후 반환
    - 해당하는 키가 실제로 존재하는 기능인지 검증 후 반환한다.

--- src/main/java/pairmatching/constant/Notification.java
+++ src/main/java/pairmatching/constant/Notification.java
@@ -2,8 +2,8 @@ package pairmatching.constant;
 
 public enum Notification {
 	FILE_STATE_INVALID("[ERROR] 파일을 정상적으로 조회할 수 없습니다."),
-	CREW_NAME_DUPLICATED("[ERROR] 파일을 정상적으로 조회할 수 없습니다.");
-
+	CREW_NAME_DUPLICATED("[ERROR] 파일을 정상적으로 조회할 수 없습니다."),
+	NOT_SUPPORTED_FUNCTION("[ERROR] 지원하지 않는 기능입니다.");
 
 	private final String message;
 
--- src/main/java/pairmatching/view/Function.java
+++ src/main/java/pairmatching/view/Function.java
@@ -1,10 +1,25 @@
 package pairmatching.view;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+import pairmatching.constant.Notification;
+
 public enum Function {
+
 	PAIR_MATCHING('1', "페어 매칭"),
 	PAIR_INQUIRY('2', "페어 조회"),
 	PAIR_INITIALIZATION('3', "페어 초기화"),
 	TERMINATION('Q', "종료");
+
+	private static final Map<Character, Function> BY_KEY = new HashMap<>();
+
+	static {
+		for (Function function : values()) {
+			BY_KEY.put(function.key, function);
+		}
+	}
 
 	private final char key;
 	private final String message;
@@ -17,5 +32,12 @@ public enum Function {
 	@Override
 	public String toString() {
 		return key + ". " + message;
+	}
+
+	public static Function valueOf(char key) {
+		if(BY_KEY.containsKey(key)) {
+			return BY_KEY.get(key);
+		}
+		throw new IllegalArgumentException(Notification.NOT_SUPPORTED_FUNCTION.getMessage());
 	}
 }
