fcf644ae9efe8ead8dbae705c8c39530562d02c2
is2js <tingstyle1@gmail.com>
2021-12-18 17:47:41 +0900

    feat: 과정와 미션을 출력하고 매칭하고자 하는 과정, 레벨, 미션을 입력 받기

--- src/main/java/pairmatching/Application.java
+++ src/main/java/pairmatching/Application.java
@@ -1,7 +1,10 @@
 package pairmatching;
 
+import pairmatching.controller.Controller;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO 구현 진행
-    }
+	public static void main(String[] args) {
+		Controller controller = new Controller();
+		controller.runPairMatching();
+	}
 }
