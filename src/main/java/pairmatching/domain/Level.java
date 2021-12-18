19862bcfc970d6bdddfee1da611a07d6f6d3b4c0
cjlee38 <gptpem38@gmail.com>
2021-12-18 13:57:09 +0900

    feat(domain): 힌트로 제공된 클래스 작성

---
+++ src/main/java/pairmatching/domain/Level.java
@@ -0,0 +1,17 @@
+package pairmatching.domain;
+
+public enum Level {
+	LEVEL1("레벨1"),
+	LEVEL2("레벨2"),
+	LEVEL3("레벨3"),
+	LEVEL4("레벨4"),
+	LEVEL5("레벨5");
+
+	private String name;
+
+	Level(String name) {
+		this.name = name;
+	}
+
+	// 추가 기능 구현
+}
