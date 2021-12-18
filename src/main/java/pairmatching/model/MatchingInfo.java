d928b6666f7803d83085ec7a9375e3bf9f9f3b5b
EastHShin <xldk78@gmail.com>
2021-12-18 15:12:45 +0900

    feat: 과정은 백엔드, 프론트엔드만 입력가능, 아니면 에러 출력하는 기능 구현

--- src/main/java/pairmatching/model/MatchingInfo.java
+++ src/main/java/pairmatching/model/MatchingInfo.java
@@ -1,5 +1,6 @@
 package pairmatching.model;
 
+import java.util.Arrays;
 import java.util.List;
 
 import pairmatching.constants.Exceptions;
@@ -15,6 +16,23 @@ public class MatchingInfo {
 	}
 
 	private void validateMatchingInfo(String matchingInfoString) {
+		try {
+			isValidMatchingInfoString(matchingInfoString);
+			List<String> infoStringList = Arrays.asList(matchingInfoString.split(","));
+			isValidCourseInfo(infoStringList);
+		} catch (IllegalArgumentException exception) {
+			System.out.println(exception.getMessage());
+		}
+	}
+
+	private void isValidCourseInfo(List<String> matchingInfoString) {
+		String courseString = matchingInfoString.get(0);
+		if (!(courseString.equals("백엔드") || courseString.equals("프론트엔드"))) {
+			throw new IllegalArgumentException(Exceptions.ERROR_PREFIX + "과정은 백엔드 혹은 프론트엔드만 있습니다.");
+		}
+	}
+
+	private void isValidMatchingInfoString(String matchingInfoString) {
 		String regex = "[가-힣]+,\\s[가-힣0-9]+,\\s[가-힣]+";
 		if (!matchingInfoString.matches(regex)) {
 			throw new IllegalArgumentException(Exceptions.ERROR_PREFIX + "잘못된 입력 양식입니다.");
