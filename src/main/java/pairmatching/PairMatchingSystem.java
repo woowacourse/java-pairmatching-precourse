dd137f0e525fba87bddeeca2e0056ffdb76dfc46
nayoung <evelyn82ny@gmail.com>
2021-12-18 17:01:04 +0900

    refactor: 매칭 시스템 메뉴별 메소드 분리

--- src/main/java/pairmatching/PairMatchingSystem.java
+++ src/main/java/pairmatching/PairMatchingSystem.java
@@ -17,8 +17,25 @@ public class PairMatchingSystem {
             if(menu.equals(-1))
                 break;
 
+            if(menu.equals(1)) {
+                printMatchingMenu();
+                createMatching();
+            }
+            if(menu.equals(2)) {
+                printMatchingMenu();
+                printMatching();
+            }
+            if(menu.equals(3)) {
+                initializeMatching();
+            }
         }
     }
+
+    private void createMatching() {}
+
+    private void printMatching() {}
+
+    private void initializeMatching() {}
 
     private Integer inputMenu() {
         printInputMenu();
@@ -35,5 +52,19 @@ public class PairMatchingSystem {
         System.out.println("2. 페어 조회");
         System.out.println("3. 페어 초기화");
         System.out.println("Q. 종료");
+    }
+
+    private void printMatchingMenu() {
+        System.out.println("#############################################");
+        System.out.println("과정: 백엔드 | 프론트엔드");
+        System.out.println("미션:");
+        System.out.println("  - 레벨1: 자동차경주 | 로또 | 숫자야구게임");
+        System.out.println("  - 레벨2: 장바구니 | 결제 | 지하철노선도");
+        System.out.println("  - 레벨3:");
+        System.out.println("  - 레벨4: 성능개선 | 배포");
+        System.out.println("  - 레벨5:");
+        System.out.println("############################################");
+        System.out.println("과정, 레벨, 미션을 선택하세요.");
+        System.out.println("ex) 백엔드, 레벨1, 자동차경주");
     }
 }
