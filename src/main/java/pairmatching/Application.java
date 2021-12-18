5f263d42271f6016c5fd9cbf12b48bbd6558f81f
woojaang <woojaang@naver.com>
2021-12-18 13:53:36 +0900

    과정과 미션 출력하고 선택해서 입력받기

--- src/main/java/pairmatching/Application.java
+++ src/main/java/pairmatching/Application.java
@@ -2,6 +2,8 @@ package pairmatching;
 
 public class Application {
     public static void main(String[] args) {
+        String a = View.selectCourse();
+        System.out.println(a);
         // TODO 구현 진행
     }
 }
