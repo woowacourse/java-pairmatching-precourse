094677c953d390ffbaeb2dbcd565662fa4e82e59
yeon-06 <rnjstldus2@hs.ac.kr>
2021-12-18 17:17:45 +0900

    test: 메소드명 변경으로 인한 테스트 코드 수정

--- src/test/java/pairmatching/utils/ValidatorTest.java
+++ src/test/java/pairmatching/utils/ValidatorTest.java
@@ -2,7 +2,7 @@ package pairmatching.utils;
 
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ValidatorTest {
@@ -11,27 +11,27 @@ class ValidatorTest {
     @Test
     void 올바른_입력() {
         String input = "백엔드, 레벨1, 자동차경주";
-        assertThat(validator.isValidDetails(input));
+        assertDoesNotThrow(() -> validator.checkValidDetails(input));
     }
 
     @Test
     void 잘못된_입력_Course() {
         String input = "풀스택, 레벨1, 자동차경주";
         assertThrows(IllegalArgumentException.class,
-                () -> validator.isValidDetails(input));
+                () -> validator.checkValidDetails(input));
     }
 
     @Test
     void 잘못된_입력_Level() {
         String input = "백엔드, 레벨8, 자동차경주";
         assertThrows(IllegalArgumentException.class,
-                () -> validator.isValidDetails(input));
+                () -> validator.checkValidDetails(input));
     }
 
     @Test
     void 잘못된_입력_Mission() {
         String input = "백엔드, 레벨2, 자동차경주";
         assertThrows(IllegalArgumentException.class,
-                () -> validator.isValidDetails(input));
+                () -> validator.checkValidDetails(input));
     }
 }
