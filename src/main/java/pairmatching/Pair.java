785a2a4147fffc34b49cda41a25bc6d1438ef73b
Byunghoon <nbalance__@kyonggi.ac.kr>
2021-12-18 15:07:07 +0900

    feat: pair 클래스 구성

---
+++ src/main/java/pairmatching/Pair.java
@@ -0,0 +1,11 @@
+package pairmatching;
+
+public class Pair {
+    private Crew crew_1;
+    private Crew crew_2;
+
+    public Pair(Crew crew_1, Crew crew_2) {
+        this.crew_1 = crew_1;
+        this.crew_2 = crew_2;
+    }
+}
