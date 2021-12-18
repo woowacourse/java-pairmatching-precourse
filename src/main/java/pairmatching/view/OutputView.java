4a4dfe400682d996ecd27fa4b3ed7af730f86ebc
khj1 <74335027+khj1@users.noreply.github.com>
2021-12-18 14:37:39 +0900

    feat: 과정, 레벨, 미션 정보를 출력하는 기능 추가

--- src/main/java/pairmatching/view/OutputView.java
+++ src/main/java/pairmatching/view/OutputView.java
@@ -1,5 +1,12 @@
 package pairmatching.view;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import pairmatching.model.Course;
+import pairmatching.model.Level;
+import pairmatching.model.Mission;
+
 public class OutputView {
 
 	public static final String SELECT_MENU_INIT = "기능을 선택하세요.";
@@ -7,10 +14,77 @@ public class OutputView {
 	public static final String MENU_CHECK_PAIR_RESULT = "2. 페어 조회";
 	public static final String MENU_PAIR_CLEAR = "3. 페어 초기화";
 	public static final String MENU_QUIT = "Q. 종료";
+	public static final String BOUNDARY = "#############################################";
+	public static final String COURSE_PREFIX = "과정: ";
+	public static final String DELIMITER = " | ";
+	public static final String MISSION_PREFIX = "미션:";
+	public static final String NEW_LINE = "\n";
+	public static final String TAP = "\t";
+	public static final String COLON = ": ";
 
 	public static void printMenu() {
 		printMenuInit();
 		printMenuCategories();
+	}
+
+	public static void printBasicInfo() {
+		printBoundary();
+		printCourseInfo();
+		printMissionInfo();
+		printBoundary();
+	}
+
+	private static void printMissionInfo() {
+		printMissionPrefix();
+		printLevelInfo();
+	}
+
+	private static void printLevelInfo() {
+		StringBuilder stringBuilder = new StringBuilder();
+		Arrays.stream(Level.values()).forEach(level -> stringBuilder
+			.append(TAP)
+			.append(getLevelPrefix(level))
+			.append(getMissionNamesByLevel(level))
+			.append(NEW_LINE)
+		);
+
+		System.out.println(stringBuilder);
+	}
+
+	private static String getMissionNamesByLevel(Level level) {
+		return Arrays.stream(Mission.values())
+			.filter(mission -> mission.isLevel(level))
+			.map(Mission::getName)
+			.collect(Collectors.joining(DELIMITER));
+	}
+
+	private static String getLevelPrefix(Level level) {
+		return level.getName() + COLON;
+	}
+
+	private static void printMissionPrefix() {
+		System.out.println(MISSION_PREFIX);
+	}
+
+	private static void printCourseInfo() {
+		printCoursePrefix();
+		printCourseNames();
+	}
+
+	private static void printCourseNames() {
+		System.out.println(getCourseNamesString());
+	}
+
+	private static String getCourseNamesString() {
+		return String.join(DELIMITER, Course.getNames());
+	}
+
+	private static void printCoursePrefix() {
+		System.out.print(COURSE_PREFIX);
+	}
+
+	private static void printBoundary() {
+		System.out.println(BOUNDARY);
 	}
 
 	private static void printMenuInit() {
