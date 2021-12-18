6e71ab21aa6b3a4388af4f22bd7fa0e869301332
Hongdonggeon <ghd700@naver.com>
2021-12-18 14:20:20 +0900

    feat(mission) : level과 미션 명을 가지고 있는 미션 객체

---
+++ src/main/java/pairmatching/domain/Mission.java
@@ -0,0 +1,28 @@
+package pairmatching.domain;
+
+import java.util.Objects;
+
+public class Mission {
+	private Level level;
+	private String name;
+
+	public Mission(Level level, String name) {
+		this.level = level;
+		this.name = name;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		Mission mission = (Mission)o;
+		return Objects.equals(name, mission.name);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(name);
+	}
+}
