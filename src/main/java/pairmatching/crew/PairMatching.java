46869bac7cc8df6c60b59368fee79e90270cca61
iamsseaa <homingsea@gmail.com>
2021-12-18 14:58:56 +0900

    feat: 모든 크루를 페어 매칭

--- src/main/java/pairmatching/crew/PairMatching.java
+++ src/main/java/pairmatching/crew/PairMatching.java
@@ -1,8 +1,17 @@
 package pairmatching.crew;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class PairMatching {
+    public static List<List<String>> getPairedCrew(List<String> crew) {
+        if (isOdd(crew)) {
+            return matchOdd(crew);
+        }
+        return matchEven(crew);
+    }
+
     public static boolean isOdd(List<String> crew) {
         if (crew.size() % 2 == 0) {
             return false;   // 짝수
@@ -10,5 +19,35 @@ public class PairMatching {
         return true;
     }
 
-    
+    public static List<List<String>> matchOdd(List<String> crew) {
+        List<List<String>> pairCrew = new ArrayList<>();
+        List<String> crews = crew;
+        while (true) {
+            if (crews.size() == 3) {
+                List<String> pair = Arrays.asList(crews.get(0), crews.get(1), crews.get(2));
+                pairCrew.add(pair);
+                break;
+            }
+            List<String> pair = Arrays.asList(crews.get(0), crews.get(1));
+            pairCrew.add(pair);
+            crews.remove(0);
+            crews.remove(1);
+        }
+        return pairCrew;
+    }
+
+    public static List<List<String>> matchEven(List<String> crew) {
+        List<List<String>> pairCrew = new ArrayList<>();
+        List<String> crews = crew;
+        while (true) {
+            if (crews.size() == 0) {
+                break;
+            }
+            List<String> pair = Arrays.asList(crews.get(0), crews.get(1));
+            pairCrew.add(pair);
+            crews.remove(0);
+            crews.remove(1);
+        }
+        return pairCrew;
+    }
 }
