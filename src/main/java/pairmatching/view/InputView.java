b5442bfb3d1e89c1406e4b6cf6ca63e05eaff460
iamsseaa <homingsea@gmail.com>
2021-12-18 13:54:50 +0900

    feat: 사용자에게 입력받는 클래스

--- src/main/java/pairmatching/view/InputView.java
+++ src/main/java/pairmatching/view/InputView.java
@@ -1,4 +1,22 @@
 package pairmatching.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+    public static String askMain() {
+        OutputView.printMain();
+        return Console.readLine();
+    }
+
+    public static String askCategory() {
+        System.out.println(Constant.ASK_PAIR);
+        System.out.println(Constant.EX);
+        return Console.readLine();
+    }
+
+    public static String askRePairMatching() {
+        System.out.println(Constant.RE_PAIR_MATCHING);
+        System.out.println(Constant.Y_N);
+        return Console.readLine();
+    }
 }
