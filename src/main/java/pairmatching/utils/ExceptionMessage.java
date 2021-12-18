6f5c286ebd5d89bc28c527250fc9ee180e992aa7
zmfl1230 <zmfl123097@gmail.com>
2021-12-18 17:50:33 +0900

    feat[ExceptionMessage]: define messages for exception

---
+++ src/main/java/pairmatching/utils/ExceptionMessage.java
@@ -0,0 +1,8 @@
+package pairmatching.utils;
+
+public class ExceptionMessage {
+    public static final String INVALID_NUMBER = "[ERROR] 숫자를 입력하세요.";
+    public static final String INVALID_INFORMATION_DATA = "[ERROR] 올바른 데이터가 아닙니다.";
+    public static final String INCORRECT_INFORMATION_FORMAT = "[ERROR] 올바른 정보 포멧이 아닙니다.";
+    public static final String NOT_MATCHABLE = "[ERROR] 매칭이 불가능합니다.";
+}
