b0d3c85abfa249e45a175579a4a05638e7ff3b5a
bomi <bonita9931@gmail.com>
2021-12-18 15:18:46 +0900

    feat - 과정, 레벨, 미션 split이 잘 되었는가에 대한 예외처리 추가

--- src/main/java/pairmatching/validator/CourseLevelMissionValidator.java
+++ src/main/java/pairmatching/validator/CourseLevelMissionValidator.java
@@ -1,12 +1,20 @@
 package pairmatching.validator;
 
 public class CourseLevelMissionValidator extends Validator {
+	private static final String ERROR_INVALID_INPUT = "[ERROR] 잘못된 입력입니다.";
+
 	@Override
 	public String validate(String input) {
+		isCourseLevelMissionSplitValid(input);
 		return input;
 	}
 
+	private static void isCourseLevelMissionSplitValid(String input) {
+		String[] courseLevelMission = input.split(", ");
+		if (courseLevelMission.length != 3) {
+			throw new IllegalArgumentException(ERROR_INVALID_INPUT);
+		}
+	}
 
-
-
+	//TODO 각각의 과정, 레벨, 미션이 존재하는가에 대한 예외처리 필요
 }
