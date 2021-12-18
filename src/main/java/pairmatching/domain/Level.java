7dc60814203e37938459c33714ec4ebc0250ada9
hyeonic <dev.hyeonic@gmail.com>
2021-12-18 13:39:18 +0900

    feat: Level별 미션 추가

--- src/main/java/pairmatching/domain/Level.java
+++ src/main/java/pairmatching/domain/Level.java
@@ -1,17 +1,30 @@
 package pairmatching.domain;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public enum Level {
-    LEVEL1("레벨1"),
-    LEVEL2("레벨2"),
-    LEVEL3("레벨3"),
-    LEVEL4("레벨4"),
-    LEVEL5("레벨5");
+    LEVEL1("레벨1", new ArrayList<>(Arrays.asList("자동차경주", "로또", "숫자야구게임"))),
+    LEVEL2("레벨2", new ArrayList<>(Arrays.asList("장바구니", "결제", "지하철노선도"))),
+    LEVEL3("레벨3", new ArrayList<>()),
+    LEVEL4("레벨4", new ArrayList<>(Arrays.asList("성능개선", "배포"))),
+    LEVEL5("레벨5", new ArrayList<>());
 
-    private String name;
+    private final String name;
+    private final List<String> missions;
 
-    Level(String name) {
+    Level(final String name, final List<String> missions) {
         this.name = name;
+        this.missions = missions;
     }
 
-    // 추가 기능 구현
+    public String getName() {
+        return name;
+    }
+
+    public List<String> getMissions() {
+        return Collections.unmodifiableList(missions);
+    }
 }
