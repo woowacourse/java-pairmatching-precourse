2e6cb82a931bdc7bf9d5b3db5f1c29011241501e
AlphaTechnic <alphatechnicist@gmail.com>
2021-12-18 14:08:11 +0900

    [feat] printUI 작성 - 사용자가 기능을 고르도록 안내

---
+++ src/main/java/outputcontroller/ApplicationUI.java
@@ -0,0 +1,12 @@
+package outputcontroller;
+
+public class ApplicationUI {
+
+    public static void printUI() {
+        System.out.println("기능을 선택하세요.");
+        System.out.println("1. 페어 매칭");
+        System.out.println("2. 페어 조회");
+        System.out.println("3. 페어 초기화");
+        System.out.println("Q. 종료");
+    }
+}
