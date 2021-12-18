6a3db3c44c2487ec4f6c1fe8bad88e0ed8b6dc82
jochungbeom <cndqjacndqja@gmail.com>
2021-12-18 17:58:51 +0900

    feat: 기능 선택 입력에서 올바르지 않은 기능 선택 입력 시, 에러 메시지 출력 후 다시 입력 추가 구현

--- src/main/java/pairmatching/controller/PairMachineController.java
+++ src/main/java/pairmatching/controller/PairMachineController.java
@@ -7,12 +7,17 @@ import pairmatching.controller.function.PairMatchingFunction;
 
 public class PairMachineController {
 	public void run() {
-		printMainScreen();
-		PairMatchingFunction pairMatchingFunction = PairMatchingFunction.findFunction(inputPairFunction());
-		pairMatchingFunction.apply();
-		if (pairMatchingFunction.isQuit()) {
-			return;
+		try{
+			printMainScreen();
+			PairMatchingFunction pairMatchingFunction = PairMatchingFunction.findFunction(inputPairFunction());
+			pairMatchingFunction.apply();
+			if (pairMatchingFunction.isQuit()) {
+				return;
+			}
+			run();
+		}catch (IllegalArgumentException illegalArgumentException) {
+			System.out.println(illegalArgumentException.getMessage());
+			run();
 		}
-		run();
 	}
 }
