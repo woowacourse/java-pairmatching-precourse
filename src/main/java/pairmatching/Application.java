c0dcfb8bc504da8b1ca387f7c6f17eaa72aac0fd
TaeHyeon Kim <kth990303@naver.com>
2021-12-18 13:22:43 +0900

    feat: 페어매칭 기본 틀 구현

--- src/main/java/pairmatching/Application.java
+++ src/main/java/pairmatching/Application.java
@@ -2,6 +2,9 @@ package pairmatching;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO 구현 진행
+        PairMatching pairMatching = new PairMatching();
+        do {
+            pairMatching.run();
+        } while (!pairMatching.quit());
     }
 }
---
+++ src/main/java/pairmatching/PairMatching.java
@@ -0,0 +1,16 @@
+package pairmatching;
+
+public class PairMatching {
+    public PairMatching() {
+
+    }
+
+    public void run() {
+
+    }
+
+    public boolean quit() {
+        // 입력받고 종료여부 확인
+        return false;
+    }
+}
