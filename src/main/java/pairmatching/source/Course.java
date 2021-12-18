e1c386923df05c41568e288ca398fab13c86c781
overnew <ehdls1303@gmail.com>
2021-12-18 17:01:38 +0900

    feat: Course의 존재 여부를 확인하는 기능
    void checkExistCourse(String name) 구현

--- src/main/java/pairmatching/source/Course.java
+++ src/main/java/pairmatching/source/Course.java
@@ -1,12 +1,28 @@
 package pairmatching.source;
 
+import pairmatching.constants.StringConstant;
+
 public enum Course {
     BACKEND("백엔드"),
     FRONTEND("프론트엔드");
+
+    private static final String NOT_FOUND_ERROR= "존재하지 않는 코스입니다.";
 
     private String name;
 
     Course(String name) {
         this.name = name;
+    }
+
+    public static void checkExistCourse(String name) throws IllegalArgumentException{
+        Course[] courses = Course.values();
+
+        for(Course course : courses){
+            if(course.name == name){
+                return;
+            }
+        }
+
+        throw new IllegalArgumentException(StringConstant.ERROR_MESSAGE_PREFIX +NOT_FOUND_ERROR);
     }
 }
