059b1ec45d70ba6916a143c80382acfd0a2eb8aa
DK <ldk980130@gmail.com>
2021-12-18 17:00:07 +0900

    feat : 페어 매칭 기능 흐름 생성

--- src/main/java/pairmatching/Application.java
+++ src/main/java/pairmatching/Application.java
@@ -1,7 +1,16 @@
 package pairmatching;
 
+import java.io.IOException;
+
+import pairmatching.controller.PairMatchingController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO 구현 진행
+        try {
+            PairMatchingController pairMatchingController = new PairMatchingController();
+            pairMatchingController.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
