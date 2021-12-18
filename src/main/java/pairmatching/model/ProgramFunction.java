aea9bc015cb27a7efee7e683a694982fc1fdd69c
minsulee <alkwen0996@naver.com>
2021-12-18 14:14:35 +0900

    feat: 프로그램 제공 기능 목록 제공

--- src/main/java/pairmatching/model/ProgramFunction.java
+++ src/main/java/pairmatching/model/ProgramFunction.java
@@ -1,2 +1,19 @@
-package pairmatching.model;public class ProgramFunction {
+package pairmatching.model;
+
+public enum ProgramFunction {
+    PAIR_MATCHING("1. 페어 매칭"),
+    PAIR_SEARCHING("2. 페어 조회"),
+    PAIR_INITIALIZING("3. 페어 초기화"),
+    PROGRAM_TERMINATE("Q. 종료");
+
+    private final String signal;
+
+    ProgramFunction(final String signal) {
+        this.signal = signal;
+    }
+
+    public String getSignal() {
+        return signal;
+    }
+
 }
