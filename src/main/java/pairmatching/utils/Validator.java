aac07c88f2bb47e468f34e811e563ea012cc1fac
zmfl1230 <zmfl123097@gmail.com>
2021-12-18 17:39:58 +0900

    feat[Validator]: define validate functions

---
+++ src/main/java/pairmatching/utils/Validator.java
@@ -0,0 +1,26 @@
+package pairmatching.utils;
+
+
+import pairmatching.domain.*;
+
+public class Validator {
+
+    public static void validateNumber(String target){
+        if(!target.matches("[0-9]+")) throw new IllegalArgumentException(ExceptionMessage.INVALID_NUMBER);
+    }
+
+    public static void validateInformationFormat(String information) {
+        String regex = "[가-힣]+,\\s[가-힣]+,\\s[가-힣]+";
+        if(!information.matches(regex)) throw  new IllegalArgumentException(ExceptionMessage.INCORRECT_INFORMATION_FORMAT);
+    }
+
+    public static void validateInformationContent(String[] information) {
+        if(Course.getCourseByName(information[0]) == null) throw new IllegalArgumentException(ExceptionMessage.INVALID_INFORMATION_DATA + information[0]);
+        if(Level.getLevelByName(information[1]) == null) throw new IllegalArgumentException(ExceptionMessage.INVALID_INFORMATION_DATA + information[1]);
+        if(Mission.existMission(Level.getLevelByName(information[1]), information[2]) == null) throw new IllegalArgumentException(ExceptionMessage.INVALID_INFORMATION_DATA + information[2]);
+    }
+
+    public static void ValidateMatchable(int count) {
+        if(count > 2) throw new IllegalArgumentException(ExceptionMessage.NOT_MATCHABLE);
+    }
+}
