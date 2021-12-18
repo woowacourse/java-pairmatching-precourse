e3048278ecdd45d09f7be51e4234fbf1ba7101bf
bonkwon <rnjsqhsrnjs@naver.com>
2021-12-18 13:53:36 +0900

    feature : 파일입력을 통해 크루 이름 받기

--- src/main/java/pairmatching/Crew.java
+++ src/main/java/pairmatching/Crew.java
@@ -1,6 +1,35 @@
 package pairmatching;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Crew {
 	private Course course;
 	private String name;
+
+	public static List<String> getCrewNames(Course course) throws IOException {
+		List<String> crewNames = new ArrayList<>();
+		Scanner scanner = null;
+
+		if(course == Course.BACKEND){
+			scanner = new Scanner(new File("../resources.backend-crew.md"));
+		}else if(course == Course.FRONTEND){
+			scanner = new Scanner(new File("../resources.frontend-crew.md"));
+		}
+
+		while (scanner.hasNext()) {
+			crewNames.add(scanner.next());
+		}
+		Crew._validateCrewNames(crewNames);
+		return crewNames;
+	}
+
+	private static void _validateCrewNames(List<String> crewNames){
+		if (crewNames.size() != crewNames.stream().distinct().count()) {
+			throw new IllegalArgumentException("[ERROR] 크루이름은 중복될 수 없습니다.");
+		}
+	}
 }
