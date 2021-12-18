650b5171e7a968d1ddf85e626b37627fcedd5d41
kanamycine <jinyeong5320@gmail.com>
2021-12-18 14:34:39 +0900

    feat(Crews) : 파일 입출력 크루 불러오기 구현

---
+++ src/main/java/pairmatching/domain/Crews.java
@@ -0,0 +1,73 @@
+package pairmatching.domain;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Crews {
+
+	private List<String> backEndCrews;
+	private List<String> FrontEndCrews;
+
+
+
+	public Crews() {
+		backEndCrews = new ArrayList<>();
+		FrontEndCrews = new ArrayList<>();
+		makeBackCrews();
+		makeFrontCrews();
+	}
+
+	public void makeBackCrews() {
+		String readLine = null;
+		int lineNumber = 0;
+		boolean hasMore = true;
+		try {
+			FileReader fileReader = new FileReader("src/main/resources/backend-crew.md");
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+			while (hasMore) {
+				if ((readLine = bufferedReader.readLine()) != null) {
+					backEndCrews.add(readLine);
+					lineNumber++;
+					hasMore = true;
+				} else
+					hasMore = false;
+			}
+			fileReader.close();
+			bufferedReader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}	public void makeFrontCrews() {
+		String readLine = null;
+		int lineNumber = 0;
+		boolean hasMore = true;
+		try {
+			FileReader fileReader = new FileReader("src/main/resources/frontend-crew.md");
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+			while (hasMore) {
+				if ((readLine = bufferedReader.readLine()) != null) {
+					FrontEndCrews.add(readLine);
+					lineNumber++;
+					hasMore = true;
+				} else
+					hasMore = false;
+			}
+			fileReader.close();
+			bufferedReader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public List<String> getBackEndCrews() {
+		return backEndCrews;
+	}
+
+	public List<String> getFrontEndCrews() {
+		return FrontEndCrews;
+	}
+}
