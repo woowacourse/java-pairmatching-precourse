ac8d015fbec8be081c170f23f9b7670f4f064a28
sure-why-not <pkse1213@gmail.com>
2021-12-18 15:07:57 +0900

    feat: 입력받은 메뉴 분기 처리 구현

--- src/main/java/pairmatching/controller/PairMatchingController.java
+++ src/main/java/pairmatching/controller/PairMatchingController.java
@@ -1,6 +1,7 @@
 package pairmatching.controller;
 
 import pairmatching.domain.CrewMaker;
+import pairmatching.function.PairFunction;
 import pairmatching.view.InputView;
 
 public class PairMatchingController {
@@ -8,6 +9,8 @@ public class PairMatchingController {
 	public void run() {
 		CrewMaker.run();
 		String menu = getFunctionMenuInput();
+		PairFunction pairFunction = PairFunction.getPairFunctionByMenu(menu);
+		pairFunction.operate();
 	}
 
 	private String getFunctionMenuInput() {
---
+++ src/main/java/pairmatching/function/PairFunction.java
@@ -0,0 +1,44 @@
+package pairmatching.function;
+
+import java.util.Arrays;
+
+public enum PairFunction {
+	MATCHING("1"){
+		@Override
+		public void operate() {
+		}
+	},
+	SEARCH("2"){
+		@Override
+		public void operate() {
+
+		}
+	},
+	RESET("3"){
+		@Override
+		public void operate() {
+
+		}
+	},
+	QUIT("Q"){
+		@Override
+		public void operate() {
+
+		}
+	},;
+
+	private String menu;
+
+	PairFunction(String menu) {
+		this.menu = menu;
+	}
+
+	public static PairFunction getPairFunctionByMenu(String menu) {
+		return Arrays.stream(PairFunction.values())
+			.filter(pairFunction -> pairFunction.menu.equals(menu))
+			.findFirst()
+			.orElseThrow(() -> new IllegalArgumentException("선택할 수 없는 기능입니다."));
+	}
+
+	public abstract void operate();
+}
