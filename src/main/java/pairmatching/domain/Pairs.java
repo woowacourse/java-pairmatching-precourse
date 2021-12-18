6478ee4001f4f39b84266e1fd8c22e4b3fe64f01
Hongdonggeon <ghd700@naver.com>
2021-12-18 15:16:43 +0900

    feat(pairs) : 페어 리스트를 가지고 있는 클래스, 페어 만들기 기능

--- src/main/java/pairmatching/domain/Pairs.java
+++ src/main/java/pairmatching/domain/Pairs.java
@@ -1,2 +1,19 @@
-package pairmatching.domain;public class Pairs {
+package pairmatching.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Pairs {
+	private static List<Pair> pairList = new ArrayList<>();
+
+	public static List<Pair> createPair(List<Crew> crewList, Mission mission) {
+		List<Crew> shuffle = Randoms.shuffle(crewList);
+		for (int i = 0; i < shuffle.size()-4; i+=2) {
+			pairList.add(new Pair(mission, crewList.subList(i, i + 2)));
+		}
+		pairList.add(new Pair(mission, crewList.subList(shuffle.size() - 3, shuffle.size())));
+		return pairList;
+	}
 }
