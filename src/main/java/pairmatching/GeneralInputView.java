793478455230460390336cfde849a0e3aa8fc38b
TaeHyeon Kim <kth990303@naver.com>
2021-12-18 14:13:47 +0900

    feat: 기능 선택 안내문 추가작성

--- src/main/java/pairmatching/GeneralInputView.java
+++ src/main/java/pairmatching/GeneralInputView.java
@@ -4,6 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class GeneralInputView {
     public static String inputFunctionByClient() {
+        System.out.println("기능을 선택하세요.");
         System.out.println("1. 페어 매칭");
         System.out.println("2. 페어 조회");
         System.out.println("3. 페어 초기화");
