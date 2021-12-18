671ed998fb346efadd2b1777484a5dc2f0d2f3d4
JunHyeok.Bang <bangvkf3@gmail.com>
2021-12-18 15:04:11 +0900

    feat(FunctionType): functionNumber로부터 해당하는 enum값 리턴 구현
    해당하는 값 없으면 IllegalArgumentException 에러 발생

---
+++ src/main/java/pairmatching/domain/input/FunctionType.java
@@ -0,0 +1,35 @@
+package pairmatching.domain.input;
+
+import java.util.Arrays;
+
+public enum FunctionType {
+	MATCH("1", "페어 매칭"),
+	VIEW("2", "페어 조회"),
+	RESET("3", "페어 초기화"),
+	EXIT("Q", "종료");
+
+	private final String functionNumber;
+	private final String functionName;
+
+	FunctionType(String functionNumber, String functionName) {
+		this.functionNumber = functionNumber;
+		this.functionName = functionName;
+	}
+
+	private String getFunctionNumber() {
+		return functionNumber;
+	}
+
+	public static FunctionType fromFunctionNumber(String functionNumber) {
+		FunctionType[] functionTypes = values();
+		return Arrays.stream(functionTypes)
+			.filter(functionType -> functionType.getFunctionNumber().equals(functionNumber))
+			.findFirst()
+			.orElseThrow(() -> new IllegalArgumentException("해당 기능은 존재하지 않습니다."));
+	}
+
+	@Override
+	public String toString() {
+		return functionNumber + ". " + functionName;
+	}
+}
