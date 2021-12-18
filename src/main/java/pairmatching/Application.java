ebef594989e99b8c074307197c5fda51d41d3e3c
최창준 <aowert6518@gmail.com>
2021-12-18 14:35:40 +0900

    [feat] 매칭 결과 반환하기
    - printMatchingResult(List<String> crewList) : crewList를 받으면 2명씩 짝지어 출력하는 기능
    - 홀수 일때는 마지막에 3명을 반환한다

--- src/main/java/pairmatching/Application.java
+++ src/main/java/pairmatching/Application.java
@@ -1,7 +1,19 @@
 package pairmatching;
 
+import pairmatching.controller.Controller;
+import pairmatching.domain.MatchingService;
+
+import java.io.IOException;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO 구현 진행
+        Controller controller = new Controller();
+        controller.choicePairMatching();
+        try {
+            MatchingService matchingService = new MatchingService();
+            matchingService.match("프론트엔드");
+        } catch (IOException e) {
+            System.out.println(e);
+        }
     }
 }
--- src/main/java/pairmatching/view/OutputView.java
+++ src/main/java/pairmatching/view/OutputView.java
@@ -1,9 +1,26 @@
 package pairmatching.view;
 
+import java.util.List;
+
 import static pairmatching.constant.OutputViewMessage.GUIDE_PROCESS_MESSAGE;
+import static pairmatching.constant.SystemMessage.*;
 
 public class OutputView {
     public static void printProcess() {
         System.out.println(GUIDE_PROCESS_MESSAGE);
+    }
+
+    public static void printMatchingResult(List<String> crewList) {
+        if (crewList.size() % EVEN == 0) {
+            for (int i = 0; i < crewList.size(); i = i + EVEN) {
+                System.out.println(String.join(" : ", crewList.subList(i, i + EVEN)));
+            }
+        }
+        if (crewList.size() % EVEN == 1) {
+            for (int i = 0; i < crewList.size() - ODD_THREE; i = i + EVEN) {
+                System.out.println(String.join(" : ", crewList.subList(i, i + EVEN)));
+            }
+            System.out.println(String.join(" : ", crewList.subList(crewList.size() - ODD_THREE, crewList.size())));
+        }
     }
 }
