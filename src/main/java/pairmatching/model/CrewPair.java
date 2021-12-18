51e8e0ca50b7604c83d6d52d5ca2d2b4e85f09b9
bcc0830 <bcc0830@naver.com>
2021-12-18 16:26:23 +0900

    refactor(CrewPair) : 인스턴스 변수 속성 변경

--- src/main/java/pairmatching/model/CrewPair.java
+++ src/main/java/pairmatching/model/CrewPair.java
@@ -3,14 +3,33 @@ package pairmatching.model;
 import java.util.HashSet;
 
 public class CrewPair {
-	private HashSet<String> crews;
+	private final String joinedName;
+	private static final String DELIMITER = " : ";
 
 	public CrewPair(String joinedName) {
-		this.crews = new HashSet<>();
-		crews.add(joinedName);
+		this.joinedName = joinedName;
 	}
 
 	public String toString() {
-		return String.join("", this.crews);
+		return this.joinedName;
+	}
+
+	public boolean isSame(String joinedName) {
+		HashSet<String> nowNameSet = new HashSet<>();
+		int size = this.joinedName.split(DELIMITER).length;
+		for (String eachName : this.joinedName.split(DELIMITER)) {
+			nowNameSet.add(eachName);
+		}
+		for (String eachName : joinedName.split(DELIMITER)) {
+			nowNameSet.add(eachName);
+		}
+		if (nowNameSet.size() == size) {
+			return true;
+		}
+		return false;
+	}
+
+	public String getJoinedName() {
+		return this.joinedName;
 	}
 }
