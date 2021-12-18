38ea2a3767efcc822d7e5b3aaa346c127e6531e8
JunHyeok.Bang <bangvkf3@gmail.com>
2021-12-18 15:54:08 +0900

    feat: Matchings 일급 컬렉션 & Matching 객체 추가

---
+++ src/main/java/pairmatching/domain/matching/Matching.java
@@ -0,0 +1,11 @@
+package pairmatching.domain.matching;
+
+import pairmatching.domain.input.Course;
+import pairmatching.domain.input.Level;
+import pairmatching.domain.mission.Mission;
+
+public class Matching {
+	private Course course;
+	private Level level;
+	private Mission mission;
+}
---
+++ src/main/java/pairmatching/domain/matching/Matchings.java
@@ -0,0 +1,12 @@
+package pairmatching.domain.matching;
+
+import java.util.List;
+
+public class Matchings {
+	private final List<Matching> matchingList;
+
+	public Matchings(List<Matching> matchingList) {
+		this.matchingList = matchingList;
+	}
+
+}
