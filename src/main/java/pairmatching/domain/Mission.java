7173c11096de26474bfe9d9a79a6e59ea4a604fc
DK <ldk980130@gmail.com>
2021-12-18 14:41:12 +0900

    feat : 과정, 미션, 레벨 정보 view 출력 기능 구현

---
+++ src/main/java/pairmatching/domain/Mission.java
@@ -0,0 +1,34 @@
+package pairmatching.domain;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public enum Mission {
+	RACING_CAR("자동차경주", Level.LEVEL1),
+	LOTTO("로또", Level.LEVEL1),
+	BASEBALL("숫자야구게임", Level.LEVEL1),
+	CART("장바구니", Level.LEVEL2),
+	PAYMENT("결제", Level.LEVEL2),
+	SUBWAY("지하철노선도", Level.LEVEL2),
+	IMPROVEMENT_PERFORMANCE("성능개선", Level.LEVEL4),
+	DISTRIBUTION("배포",Level.LEVEL4);
+
+	private String name;
+	private Level level;
+
+	Mission(String name, Level level) {
+		this.name = name;
+		this.level = level;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public static List<Mission> getByLevel(Level level) {
+		return Arrays.stream(Mission.values())
+			.filter(mission -> mission.level == level)
+			.collect(Collectors.toList());
+	}
+}
--- src/main/java/pairmatching/view/OutputView.java
+++ src/main/java/pairmatching/view/OutputView.java
@@ -2,15 +2,61 @@ package pairmatching.view;
 
 import java.util.Arrays;
 
+import pairmatching.domain.Course;
 import pairmatching.domain.Function;
+import pairmatching.domain.Level;
+import pairmatching.domain.Mission;
 
 public class OutputView {
 
 	private static final String FUNCTION_SELECT_MESSAGE = "기능을 선택하세요.";
 
+	private static final String DIVIDING_LINE = "################################";
+	private static final String COURSE = "과정";
+	private static final String MISSION = "미션";
+
+	private static String courseMessage;
+	private static String missionMessage;
+
 	public void printFunctionList() {
 		System.out.println(FUNCTION_SELECT_MESSAGE);
 		Arrays.stream(Function.values())
 			.forEach(function -> System.out.println(function.getMessage()));
+	}
+
+	public void printCourseAndLevelAndMission() {
+		System.out.println(DIVIDING_LINE);
+
+		printCourses();
+		printMissionOfLevels();
+
+		System.out.println(DIVIDING_LINE);
+	}
+
+	private void printMissionOfLevels() {
+		System.out.println(MISSION + ": ");
+		Arrays.stream(Level.values())
+				.forEach(level -> printMissionOfLevel(level));
+	}
+
+	private void printMissionOfLevel(Level level) {
+		missionMessage = "";
+
+		System.out.print("-" + level.getName() + ": ");
+		Mission.getByLevel(level)
+			.stream()
+			.forEach(mission -> missionMessage += " | " + mission.getName());
+
+		System.out.println(missionMessage.replaceFirst(" \\| ", ""));
+	}
+
+	private void printCourses() {
+		courseMessage = "";
+
+		System.out.print(COURSE + ": ");
+		Arrays.stream(Course.values())
+				.forEach(course -> courseMessage += " | " +  course.getName());
+
+		System.out.print(courseMessage.replaceFirst(" \\| ", "") + "\n");
 	}
 }
