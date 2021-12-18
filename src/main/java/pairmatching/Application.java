d0aca609530c64fdd8140d968eba580532f4334b
YJ <soltwood6862@naver.com>
2021-12-18 17:54:04 +0900

    feat : Application main함수 수정

--- src/main/java/pairmatching/Application.java
+++ src/main/java/pairmatching/Application.java
@@ -6,6 +6,8 @@ import pairmatching.Controller.Controller;
 public class Application {
     public static void main(String[] args) {
         // TODO 구현 진행
+        Controller controller = new Controller();
+        controller.start();
         /*
         String names = "백호 태웅 치수 태섭 대만 준호 대협 덕규 태산 경태 수겸 현준 준섭 한나 소연 호열 대남 용팔 구식 달재";
         List<String> crewNames = new ArrayList<String>(); // 파일에서 로드한 크루 이름 목록
