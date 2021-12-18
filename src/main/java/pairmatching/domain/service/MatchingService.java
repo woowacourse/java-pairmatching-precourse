a597fc2a66979578f636045e6a086ab5cb88d4ce
Wishoon <heonga2@gmail.com>
2021-12-18 17:37:20 +0900

    feat: 크루 목록 파일을 읽어오는 기능 추가

--- src/main/java/pairmatching/domain/service/MatchingService.java
+++ src/main/java/pairmatching/domain/service/MatchingService.java
@@ -1,19 +1,37 @@
 package pairmatching.domain.service;
 
+import java.io.BufferedReader;
+
+import java.io.FileReader;
 import java.io.IOException;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 
 import pairmatching.domain.Course;
 import pairmatching.domain.Level;
-
 import pairmatching.domain.MissionType;
 
 public class MatchingService {
 	public static void generateMatching(Course course, Level level, MissionType mission) throws IOException {
 		if (course.isValidateBackend()) {
-
 		}
 
 		if (course.isValidateFrontend()) {
+
 		}
 	}
+
+	private static void fileReader(List<String> names) throws IOException {
+		Path currentPath = Paths.get("");
+		String path = currentPath.toAbsolutePath().toString();
+		BufferedReader reader = new BufferedReader(new FileReader(path + "/src/main/resources/backend-crew.md"));
+		String str;
+		while ((str = reader.readLine()) != null) {
+			names.add(str);
+		}
+		reader.close();
+	}
 }
+
