8dfb63eef3d828683f3f172e99a1bc54039ab86f
Seungmin Lim <seungmin9066@gmail.com>
2021-12-18 16:37:08 +0900

    [추가] 예외처리메시지 상수 추가

--- src/main/java/pairmatching/constant/ErrorMessage.java
+++ src/main/java/pairmatching/constant/ErrorMessage.java
@@ -2,4 +2,6 @@ package pairmatching.constant;
 
 public class ErrorMessage {
 	public static final String NO_MATCHING_LOG = "[ERROR] 매칭 이력이 없습니다.";
+	public static final String NO_MATCHING_WAY = "[ERROR] 매칭을 할 수 있는 경우가 없습니다.";
+	public static final String DEFAULT = "[ERROR] 코드상의 결함";
 }
