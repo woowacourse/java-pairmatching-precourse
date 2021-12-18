eab05fc02a5366f06f256eaaf447a9e43c763810
hyeonic <dev.hyeonic@gmail.com>
2021-12-18 15:05:21 +0900

    feat: 매칭된 크루원 관리를 위한 Pair 추가

---
+++ src/main/java/pairmatching/domain/Pair.java
@@ -0,0 +1,11 @@
+package pairmatching.domain;
+
+import java.util.List;
+
+public class Pair {
+    private final List<Crew> crews;
+
+    public Pair(List<Crew> crews) {
+        this.crews = crews;
+    }
+}
