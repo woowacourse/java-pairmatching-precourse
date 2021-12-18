b5ee486b5c12c5febb4c9f675bbb7a7e04fb9e8d
jurlring <78091011+jurlring@users.noreply.github.com>
2021-12-18 14:31:52 +0900

    feat : 과정, 레벨, 미션 입력 받기 구현

--- src/main/java/pairmatching/controller/PairMatchingController.java
+++ src/main/java/pairmatching/controller/PairMatchingController.java
@@ -3,6 +3,7 @@ package pairmatching.controller;
 import static pairmatching.exception.EmptyException.*;
 import static pairmatching.exception.FunctionException.*;
 
+import pairmatching.service.PairService;
 import pairmatching.view.InputView;
 import pairmatching.view.OutputView;
 
@@ -11,20 +12,35 @@ public class PairMatchingController {
 	String function;
 	InputView inputView;
 	OutputView outputView;
+	PairService pairService;
 
 	public PairMatchingController() {
 		function = "";
 		inputView = new InputView();
 		outputView = new OutputView();
+		pairService = new PairService();
 	}
 
 	public void run() {
 		do {
 			try {
 				function = checkCorrectFunction(checkEmptyInput(inputView.requestFunction()));
+				proceedFunction(function);
 			} catch (Exception exception) {
 				outputView.reportException(exception.getMessage());
 			}
 		} while (!function.equals("Q"));
+	}
+
+	private void proceedFunction(String function) {
+		if (function.equals("1")) {
+			pairService.match(checkEmptyInput(inputView.requestSource()));
+		}
+		if (function.equals("2")) {
+			pairService.look(checkEmptyInput(inputView.requestSource()));
+		}
+		if (function.equals("3")) {
+			pairService.init();
+		}
 	}
 }
--- src/main/java/pairmatching/view/InputView.java
+++ src/main/java/pairmatching/view/InputView.java
@@ -5,7 +5,7 @@ import static camp.nextstep.edu.missionutils.Console.*;
 public class InputView {
 
 	static final String REQUEST_FUNCTION_MSG = "기능을 선택하세요.\n1. 페어 매칭\n2. 페어 조회\n3. 페어 초기화\nQ. 종료";
-	static final String REQUEST_PAIR_RESOURCE_MSG="\n#############################################\n"
+	static final String REQUEST_PAIR_RESOURCE_MSG = "\n#############################################\n"
 		+ "과정: 백엔드 | 프론트엔드\n"
 		+ "미션:\n"
 		+ "  - 레벨1: 자동차경주 | 로또 | 숫자야구게임\n"
@@ -16,8 +16,13 @@ public class InputView {
 		+ "############################################\n"
 		+ "과정, 레벨, 미션을 선택하세요.";
 
-	public String requestFunction(){
+	public String requestFunction() {
 		System.out.println(REQUEST_FUNCTION_MSG);
+		return readLine();
+	}
+
+	public String requestSource() {
+		System.out.println(REQUEST_PAIR_RESOURCE_MSG);
 		return readLine();
 	}
 }
