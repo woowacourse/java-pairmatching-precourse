c0e17961b1ef858bf6bb6c68a1b6ccc2f6307d4c
Hongdonggeon <ghd700@naver.com>
2021-12-18 13:43:00 +0900

    feat(crew) : 이름이 같으면 같은 크루로 식별하기

--- src/main/java/pairmatching/domain/Crew.java
+++ src/main/java/pairmatching/domain/Crew.java
@@ -1,6 +1,28 @@
 package pairmatching.domain;
 
+import java.util.Objects;
+
 public class Crew {
 	private Course course;
 	private String name;
+
+	public Crew(Course course, String name) {
+		this.course = course;
+		this.name = name;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		Crew crew = (Crew)o;
+		return Objects.equals(name, crew.name);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(name);
+	}
 }
