3e3b530bd88a9e05183af17441f8eebd0982a17b
iamsseaa <homingsea@gmail.com>
2021-12-18 13:46:28 +0900

    feat: 안내 문구 상수 클래스

--- src/main/java/pairmatching/view/Constant.java
+++ src/main/java/pairmatching/view/Constant.java
@@ -1,4 +1,34 @@
 package pairmatching.view;
 
 public class Constant {
+    public static final String ONE = "1";
+    public static final String TWO = "2";
+    public static final String THREE = "3";
+    public static final String Q = "Q";
+
+    public static final String ASK_CHOICE = "기능을 선택하세요.";
+    public static final String PAIR_MATCHING = ". 페어 매칭";
+    public static final String PAIR_VIEW = ". 페어 조회";
+    public static final String PAIR_RESET = ". 페어 초기화";
+    public static final String QUIT = ". 종료";
+
+    public static final String CHOICE_ONE = ONE + PAIR_MATCHING;
+    public static final String CHOICE_TWO = TWO + PAIR_VIEW;
+    public static final String CHOICE_THREE = THREE + PAIR_RESET;
+    public static final String CHOICE_FOUR = Q + QUIT;
+
+    public static final String DELIMITER = "#############################################";
+    public static final String COLON = ": ";
+    public static final String AND = " : ";
+    public static final String OR = " | ";
+
+    public static final String ASK_PAIR = "과정, 레벨, 미션을 선택하세요.";
+    public static final String EX = "ex) 백엔드, 레벨1, 자동차경주";
+
+    public static final String RESULT_PAIR_MATCHING = "페어 매칭 결과입니다.";
+
+    public static final String RE_PAIR_MATCHING = "매칭 정보가 있습니다. 다시 매칭하시겠습니까?";
+    public static final String Y_N = "네 | 아니오";
+
+    public static final String RESET = "초기화 되었습니다.";
 }
