359c41069f806222272333e83c50bab3b1a4023d
AlphaTechnic <alphatechnicist@gmail.com>
2021-12-18 17:38:10 +0900

    [feat] UI 로직을 별도의 패키지로 분리

--- src/main/java/outputcontroller/ApplicationUI.java
+++ src/main/java/outputcontroller/ApplicationUI.java
@@ -9,4 +9,26 @@ public class ApplicationUI {
         System.out.println("3. 페어 초기화");
         System.out.println("Q. 종료");
     }
+
+    public static void printCourseType() {
+        System.out.println("#############################################");
+        System.out.println("과정: 백엔드 | 프론트엔드");
+        System.out.println("미션:");
+        System.out.println("  - 레벨1: 자동차경주 | 로또 | 숫자야구게임");
+        System.out.println("  - 레벨2: 장바구니 | 결제 | 지하철노선도");
+        System.out.println("  - 레벨3:");
+        System.out.println("  - 레벨4: 성능개선 | 배포");
+        System.out.println("  - 레벨5:");
+        System.out.println("############################################");
+    }
+
+    public static void printUIforCourseTypeInput() {
+        System.out.println("과정, 레벨, 미션을 선택하세요.");
+        System.out.println("ex) 백엔드, 레벨1, 자동차경주");
+    }
+
+    public static void printPariMatchingResult() {
+        System.out.println("페어 매칭 결과입니다.");
+        for (int )
+    }
 }
