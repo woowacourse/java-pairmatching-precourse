e55d60319b7a42a92efaf8b13d8d859a8fb7650c
bonkwon <rnjsqhsrnjs@naver.com>
2021-12-18 16:29:38 +0900

    feature : 페어 초기화

--- src/main/java/pairmatching/ProgramManager.java
+++ src/main/java/pairmatching/ProgramManager.java
@@ -1,4 +1,6 @@
 package pairmatching;
+import java.util.List;
+
 import camp.nextstep.edu.missionutils.Console;
 
 public class ProgramManager {
@@ -10,13 +12,31 @@ public class ProgramManager {
 		if(selection == "1"){
 			//PairMatcher.pairMatch();
 		} else if(selection == "2"){
-			//ProgramManager.lookUpPair();
+			ProgramManager.lookUpPair();
 		} else if(selection == "3"){
-			//ProgramManger.initializePair();
+			ProgramManager.initializePair();
 		} else if(selection == "Q"){
 			return true;
 		}
 
 		return false;
+	}
+
+	public static void lookUpPair(Course course, Level level, String mission){
+		List<PairMatchRecorder> checkList = ProgramData.getMatchingHistory(course, level);
+
+		for(PairMatchRecorder history : checkList){
+			if(!history.isMatchedMission(mission)){
+				//ProgramPrinter.printPair();
+				return;
+			}
+		}
+
+		System.out.println("[ERROR] 매칭 이력이 없습니다.");
+		return;
+	}
+
+	public static void initializePair(){
+		ProgramData.deleteMatchingHistory();
 	}
 }
