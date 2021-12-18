7ea74164207601cd42e96e1f8307fa70c334854c
iamsseaa <homingsea@gmail.com>
2021-12-18 14:05:47 +0900

    feat: 레벨에 미션 추가

--- src/main/java/pairmatching/Level.java
+++ src/main/java/pairmatching/Level.java
@@ -1,16 +1,20 @@
 package pairmatching;
 
+import java.util.List;
+
 public enum Level {
-    LEVEL1("레벨1"),
-    LEVEL2("레벨2"),
-    LEVEL3("레벨3"),
-    LEVEL4("레벨4"),
-    LEVEL5("레벨5");
+    LEVEL1("레벨1", Mission.LEVEL1),
+    LEVEL2("레벨2", Mission.LEVEL2),
+    LEVEL3("레벨3", Mission.LEVEL3),
+    LEVEL4("레벨4", Mission.LEVEL4),
+    LEVEL5("레벨5", Mission.LEVEL5);
 
     private String name;
+    private Mission missions;
 
-    Level(String name) {
+    Level(String name, Mission missions) {
         this.name = name;
+        this.missions = missions;
     }
 
     // 추가 기능 구현
