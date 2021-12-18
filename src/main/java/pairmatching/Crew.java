ccf072c349e9dba39ceeefa73d7262f77f9e2fe1
soulfever <soulfever01@naver.com>
2021-12-18 16:19:43 +0900

    FEAT : 크루원이 해당 레벨에서 짝을 이미 만났었는지 확인하는 기능

--- src/main/java/pairmatching/Crew.java
+++ src/main/java/pairmatching/Crew.java
@@ -5,15 +5,37 @@ import java.util.ArrayList;
 public class Crew {
     private Course course;
     private static String name;
-    private ArrayList<String> level1Mission = new ArrayList<>();
-    private ArrayList<String> level2Mission = new ArrayList<>();
-    private ArrayList<String> level4Mission = new ArrayList<>();
+    private static ArrayList<String> LEVEL1Patner = new ArrayList<>();
+    private static ArrayList<String> LEVEL2Patner = new ArrayList<>();
+    private static ArrayList<String> LEVEL3Patner = new ArrayList<>();
+    private static ArrayList<String> LEVEL4Patner = new ArrayList<>();
+    private static ArrayList<String> LEVEL5Patner = new ArrayList<>();
 
-
-    Crew(String name) {
+    Crew(Course course, String name) {
         this.name = name;
+        this.course = course;
     }
 
+    public static boolean PartnerCheck(String level, String partnerName) {
+        if (level == "레벨1") {
+            return CheckNameInLevel(LEVEL1Patner,partnerName);
+        }
+        if (level == "레벨1") {
+            return CheckNameInLevel(LEVEL2Patner,partnerName);
+        }
+        if (level == "레벨1") {
+            return CheckNameInLevel(LEVEL3Patner,partnerName);
+        }
+        if (level == "레벨1") {
+            return CheckNameInLevel(LEVEL4Patner,partnerName);
+        }
+        return CheckNameInLevel(LEVEL5Patner,partnerName);
+    }
 
-
+    public static boolean CheckNameInLevel (ArrayList<String> partners,String partnerName) {
+        if (partners.contains(partnerName)) {
+            return true;
+        }
+        return false;
+    }
 }
