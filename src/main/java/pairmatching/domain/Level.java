166b95f0bef2de452af79821187ba6061a083f82
harinn1014 <gkfla7555@gmail.com>
2021-12-18 16:59:50 +0900

    feat(Level): 레벨에 따라 가능한 mission 타입을 저장한 enum class 생성
    <description>
    - enum class로 1레벨~5레벨까지의 level name과 mission array를 정의한다.
    - getter method 추가
    - mission에 따라 어느 레벨인지 찾아주는 함수 추가

---
+++ src/main/java/pairmatching/domain/Level.java
@@ -0,0 +1,39 @@
+package pairmatching.domain;
+
+import pairmatching.domain.Mission;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+public enum Level {
+	  LEVEL1("레벨1", Arrays.asList(Mission.GAME_RACINGCAR, Mission.GAME_LOTTO, Mission.GAME_BASEBALL)),
+	  LEVEL2("레벨2", Arrays.asList(Mission.SHOPPINGBAG, Mission.PAY, Mission.SUBWAY_LINEMAP)),
+	  LEVEL3("레벨3", Arrays.asList(Mission.EMPTY)),
+	  LEVEL4("레벨4", Arrays.asList(Mission.IMPROVEMENT, Mission.DISTRIBUTION)),
+	  LEVEL5("레벨5", Arrays.asList(Mission.EMPTY));
+
+	  private String name;
+	  private List<Mission> missionList;
+
+	  Level(String name, List<Mission> missionList) {
+	    this.name = name;
+	    this.missionList= missionList;
+	  }
+	  
+	  public String getLevel() {
+		  return name;
+	  }
+
+	  public Level findByMissionType(Mission missionName) {
+		  Optional<Level> level = Arrays.stream(Level.values())
+				  .filter(mission -> mission.hasMissionCode(missionName))
+				  .findAny();
+		  if(!level.isPresent()) throw new IllegalArgumentException();
+		  return level.get();
+	  }
+	  
+	  public boolean hasMissionCode(Mission missionName) {
+		  return missionList.stream().anyMatch(mission -> mission.equals(missionName));
+	  }
+	  
+}
