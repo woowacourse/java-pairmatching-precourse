17943d3d2da34175217aaee3c64782c53ee85044
iamsseaa <homingsea@gmail.com>
2021-12-18 15:22:16 +0900

    fix: 매칭했던 모든 크루 리스트 저장하는 메소드 수정

--- src/main/java/pairmatching/crew/CrewRepository.java
+++ src/main/java/pairmatching/crew/CrewRepository.java
@@ -3,30 +3,16 @@ package pairmatching.crew;
 import pairmatching.Level;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class CrewRepository {
-    static List<List<String>> levelOne = new ArrayList<>();
-    static List<List<String>> levelTwo = new ArrayList<>();
-    static List<List<String>> levelThree = new ArrayList<>();
-    static List<List<String>> levelFour = new ArrayList<>();
-    static List<List<String>> levelFive = new ArrayList<>();
+    static HashMap<Level,List<List<String>>> crews = new HashMap<>();
 
     public static void savePair(Level level, List<String> crew) {
-        if (level == Level.LEVEL1) {
-            levelOne.add(crew);
-        }
-        if (level == Level.LEVEL2) {
-            levelTwo.add(crew);
-        }
-        if (level == Level.LEVEL3) {
-            levelThree.add(crew);
-        }
-        if (level == Level.LEVEL4) {
-            levelFour.add(crew);
-        }
-        if (level == Level.LEVEL5) {
-            levelFive.add(crew);
-        }
+        List<List<String>> pairs = crews.get(level);
+        pairs.add(crew);
+        crews.put(level, pairs);
     }
+    
 }
