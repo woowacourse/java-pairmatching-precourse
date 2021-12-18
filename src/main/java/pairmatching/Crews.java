efcc404558d5d0ea49e7ce094d62497a253449df
inbriz <adoredia@naver.com>
2021-12-18 16:02:01 +0900

    feat: 크루 리스트를 페어로 맺는 기능 구현

--- src/main/java/pairmatching/Crews.java
+++ src/main/java/pairmatching/Crews.java
@@ -9,6 +9,8 @@ import java.util.List;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Crews {
+	private static final String DELIMITER = " : ";
+	private static final String MATCHING_RESULT = "페어 매칭 결과입니다.";
 
 	public static List<String> backEndCrewNames;
 	public static List<String> frontEndCrewNames;
@@ -18,6 +20,30 @@ public class Crews {
 
 	public static Crew[] backEndCrews;
 	public static Crew[] frontEndCrews;
+
+	public static void matchingOdd(List<String> crewNames) {
+		System.out.println(MATCHING_RESULT);
+		int index = 0;
+		for (int i = 0; i < crewNames.size() - 3; i += 2) {
+			System.out.println(crewNames.get(i) + DELIMITER + crewNames.get(i + 1));
+			index = i;
+		}
+		System.out.println(crewNames.get(index) + DELIMITER + crewNames.get(index + 1) + DELIMITER + crewNames.get(index + 2));
+	}
+
+	public static void matchingEven(List<String> crewNames) {
+		System.out.println(MATCHING_RESULT);
+		for (int i = 0; i < crewNames.size(); i += 2) {
+			System.out.println(crewNames.get(i) + DELIMITER + crewNames.get(i + 1));
+		}
+	}
+
+	public static int oddOrEven(List<String> crewNames) {
+		if (crewNames.size() % 2 == 0) {
+			return 0;
+		}
+		return 1;
+	}
 
 	private static void randomShuffleBackEnd() {
 		shuffledBackEndCrewNames = Randoms.shuffle(backEndCrewNames);
@@ -40,6 +66,11 @@ public class Crews {
 		randomShuffleBackEnd();
 		randomShuffleFrontEnd();
 
+		System.out.println();
+
+		matchingEven(shuffledBackEndCrewNames);
+		System.out.println();
+		matchingOdd(shuffledFrontEndCrewNames);
 	}
 
 	public static Crew[] makeBackEndCrews() throws IOException {
