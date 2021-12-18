95b7c9cc954a50cea8edd80742bd06ff210d6abc
Seungmin Lim <seungmin9066@gmail.com>
2021-12-18 15:27:16 +0900

    [추가] 컨트롤러의 기반 추가

--- src/main/java/pairmatching/controller/PairMatchingController.java
+++ src/main/java/pairmatching/controller/PairMatchingController.java
@@ -1,4 +1,44 @@
 package pairmatching.controller;
 
+import pairmatching.domain.Curriculum;
+import pairmatching.domain.PairMatching;
+import pairmatching.view.InputView;
+import pairmatching.view.OutputView;
+
 public class PairMatchingController {
+
+	private int number = 0;
+	private String input;
+	private PairMatching pairMatching = new PairMatching();
+
+	public PairMatchingController() {
+		do {
+			OutputView.printGuide();
+			input = InputView.readString();
+		} while (!quit());
+	}
+
+	private void matchPair() {
+		OutputView.printCourse();
+		String[] curriculumInfo = InputView.readString().split(", ");
+		Curriculum curriculum = new Curriculum(curriculumInfo[0], curriculumInfo[1], curriculumInfo[2]);
+		if (pairMatching.isEmpty(curriculum)) {
+
+		}
+
+	}
+
+	private void checkPair() {
+		OutputView.printCourse();
+		String[] curriculum = InputView.readString().split(", ");
+
+	}
+
+	private void cleanPair() {
+
+	}
+
+	private boolean quit() {
+		return input.equals("Q");
+	}
 }
