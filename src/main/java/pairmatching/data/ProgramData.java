1c91571dde9980a070ee84a89af2daeaa8119e7a
CptBluebear <cptbluebear@naver.com>
2021-12-18 14:36:00 +0900

    refactor(Level.java): Level 클래스가 미션에 대한 정보를 가지도록 수정
    Level 클래스가 스스로 미션에 대한 정보를 가지도록 소스를 수정함.
    그렇기에 기존에 있던 setMissionList 메소드는 삭제하고
    ProgramData에 미션에 대한 정보를 등록하였다.

--- src/main/java/pairmatching/data/ProgramData.java
+++ src/main/java/pairmatching/data/ProgramData.java
@@ -1,5 +1,8 @@
 package pairmatching.data;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class ProgramData {
 	public static final String BACKEND_CREW_FILEPATH = "src/main/resources/backend-crew.md";
 	public static final String FRONTEND_CREW_FILEPATH = "src/main/resources/frontend-crew.md";
@@ -7,5 +10,11 @@ public class ProgramData {
 	public static final String SELECT_MESSAGE = "기능을 선택하세요.\n1. 페어 매칭\n2. 페어 조회\n3. 페어 초기화\nQ. 종료";
 	public static final String PAIR_MATCH_RESULT_MESSAGE = "페어 매칭 결과입니다.";
 	public static final String PAIR_SEPARATOR = " : ";
+
+	public static final List<String> LEVEL1_MISSION = Arrays.asList("자동차경주", "로또", "숫자야구게임");
+	public static final List<String> LEVEL2_MISSION = Arrays.asList("장바구니", "결제", "지하철노선도");
+	public static final List<String> LEVEL3_MISSION = Arrays.asList();
+	public static final List<String> LEVEL4_MISSION = Arrays.asList("성능개선", "배포");
+	public static final List<String> LEVEL5_MISSION = Arrays.asList();
 
 }
--- src/main/java/pairmatching/type/Level.java
+++ src/main/java/pairmatching/type/Level.java
@@ -3,21 +3,20 @@ package pairmatching.type;
 import java.util.Arrays;
 import java.util.List;
 
+import pairmatching.data.ProgramData;
+
 public enum Level {
-	LEVEL1("레벨1"),
-	LEVEL2("레벨2"),
-	LEVEL3("레벨3"),
-	LEVEL4("레벨4"),
-	LEVEL5("레벨5");
+	LEVEL1("레벨1", ProgramData.LEVEL1_MISSION),
+	LEVEL2("레벨2", ProgramData.LEVEL2_MISSION),
+	LEVEL3("레벨3", ProgramData.LEVEL3_MISSION),
+	LEVEL4("레벨4", ProgramData.LEVEL4_MISSION),
+	LEVEL5("레벨5", ProgramData.LEVEL5_MISSION);
 
 	private String name;
 	private List<String> missionList;
 
-	Level(String name) {
+	Level(String name, List<String> missionList) {
 		this.name = name;
-	}
-
-	public void setMissionList(List<String> missionList) {
 		this.missionList = missionList;
 	}
 
