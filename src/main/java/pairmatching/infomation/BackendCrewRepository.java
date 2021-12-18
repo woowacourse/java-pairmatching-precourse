23dff56d3d7ac7a9b57bbe3bdbf3432794a08033
jaeseo yoo <wotj102@gmail.com>
2021-12-18 15:08:00 +0900

    refactor: 크루 repository 분리(backend, frontend)

---
+++ src/main/java/pairmatching/infomation/BackendCrewRepository.java
@@ -0,0 +1,30 @@
+package pairmatching.infomation;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class BackendCrewRepository {
+    private static final ArrayList<Crew> backendCrews = new ArrayList<>();
+
+    public static void initBackCrew () throws IOException {
+        BufferedReader br = new BufferedReader(
+                new FileReader("C:\\Users\\wotj1\\Desktop\\최종\\java-pairmatching-precourse\\" +
+                        "src\\main\\resources\\backend-crew.md"));
+
+        while(true) {
+            String name = br.readLine();
+            if (name == null) {
+                break;
+            }
+            backendCrews.add(new Crew("백엔드", name));
+        }
+        br.close();
+    }
+
+
+    public static ArrayList<Crew> getCrews() {
+        return backendCrews;
+    }
+}
---
+++ src/main/java/pairmatching/infomation/FrontendCrewRepository.java
@@ -0,0 +1,26 @@
+package pairmatching.infomation;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class FrontendCrewRepository {
+    private static final ArrayList<Crew> frontendCrews = new ArrayList<>();
+
+    public static void initFrontCrew () throws IOException {
+        BufferedReader br = new BufferedReader(
+                new FileReader(
+                        "C:\\Users\\wotj1\\Desktop\\최종\\java-pairmatching-precourse\\" +
+                                "src\\main\\resources\\frontend-crew.md"));
+
+        while(true) {
+            String name = br.readLine();
+            if (name == null) {
+                break;
+            }
+            frontendCrews.add(new Crew("프론트엔드", name));
+        }
+        br.close();
+    }
+}
