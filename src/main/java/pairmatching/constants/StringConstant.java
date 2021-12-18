7f51dde4b302ba74fa58d5b44178f5ee53020811
overnew <ehdls1303@gmail.com>
2021-12-18 16:25:52 +0900

    feat: StringConstant class 구현
    상수화 문자열을 저장

---
+++ src/main/java/pairmatching/constants/StringConstant.java
@@ -0,0 +1,7 @@
+package pairmatching.constants;
+
+public class StringConstant {
+    public static final String[] FUNCTION_SYMBOL = {"1","2","3","Q"};
+
+    public static final String ERROR_MESSAGE_PREFIX = "[ERROR]: ";
+}
