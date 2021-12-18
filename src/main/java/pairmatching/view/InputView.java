4e1110803e5c175e8f75c451f2acea9e9fc7d983
jochungbeom <cndqjacndqja@gmail.com>
2021-12-18 14:03:58 +0900

    feat: 매칭하고자 하는 과정, 레벨, 미션을 입력 구현

--- src/main/java/pairmatching/view/InputView.java
+++ src/main/java/pairmatching/view/InputView.java
@@ -7,4 +7,10 @@ public class InputView {
 	public static String inputPairFunction() {
 		return Console.readLine();
 	}
+
+	public static String inputPairMatching() {
+		System.out.println("과정, 레벨, 미션을 선택하세요.");
+		System.out.println("ex) 백엔드, 레벨1, 자동차경주");
+		return Console.readLine();
+	}
 }
