dabb2f19b76ba49adc15dd800f8073c07b703cdb
yeongunheo <gjduddns@gmail.com>
2021-12-18 14:09:20 +0900

    feat: 크루들의 이름 목록을 List<String> 형태로 불러오는 기능 구현

--- src/main/java/pairmatching/controller/PairMatchingController.java
+++ src/main/java/pairmatching/controller/PairMatchingController.java
@@ -1,8 +1,17 @@
 package pairmatching.controller;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 import pairmatching.View.InputView;
+import pairmatching.domain.Crew;
 
 public class PairMatchingController {
 	private final InputView inputView;
@@ -13,5 +22,39 @@ public class PairMatchingController {
 
 	public int scanFunctionNumber() {
 		return inputView.scanFunctionNumber();
+	}
+
+	public List<String> readBackendCrewNames() {
+		List<String> backendCrewNames = new ArrayList<>();
+		try {
+			File file = new File(getClass().getResource("/backend-crew.md").toURI());
+			BufferedReader br = new BufferedReader(new FileReader(file));
+			String crewName;
+			while ((crewName = br.readLine()) != null) {
+				backendCrewNames.add(crewName);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
+		return backendCrewNames;
+	}
+
+	public List<String> readFrontendCrewNames() {
+		List<String> frontendCrewNames = new ArrayList<>();
+		try {
+			File file = new File(getClass().getResource("/frontend-crew.md").toURI());
+			BufferedReader br = new BufferedReader(new FileReader(file));
+			String crewName;
+			while ((crewName = br.readLine()) != null) {
+				frontendCrewNames.add(crewName);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
+		return frontendCrewNames;
 	}
 }
