dd03b52401e79ba1a0171ddc8e337d68e8929c5d
Seungmin Lim <seungmin9066@gmail.com>
2021-12-18 14:01:05 +0900

    [추가] 버퍼를 이용한 입출력으로 파일에서 데이터 가져오는 뷰 추가

--- src/main/java/pairmatching/view/InputView.java
+++ src/main/java/pairmatching/view/InputView.java
@@ -1,4 +1,32 @@
 package pairmatching.view;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.LinkedList;
+import java.util.List;
+
 public class InputView {
+
+	public static List<String> getCrew(String path) {
+		List<String> crews = new LinkedList<>();
+		try {
+			BufferedReader reader = new BufferedReader(
+				new FileReader(path)
+			);
+			String crew;
+			while ((crew = reader.readLine()) != null) {
+				crews.add(crew);
+			}
+			reader.close();
+			return crews;
+		} catch (Exception e) {
+			logError(e);
+			return new LinkedList<>();
+		}
+	}
+
+	private static void logError(Exception error) {
+		System.out.println(error.getMessage());
+	}
+
 }
