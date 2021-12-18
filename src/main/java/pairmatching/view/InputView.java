1af1f3e157d1e582a07f11e586d0047c647b5092
LEE EUN JIN <lej4545@naver.com>
2021-12-18 17:04:15 +0900

    refactor(InputView, OutputView): UI 기능 리팩토링

--- src/main/java/pairmatching/view/InputView.java
+++ src/main/java/pairmatching/view/InputView.java
@@ -9,11 +9,22 @@ public class InputView {
             "2. 페어 조회\n" +
             "3. 페어 초기화\n" +
             "Q. 종료";
-    private static final String INPUT_COURSE_LEVEL_MISSION_MESSAGE = "과정, 레벨, 미션을 선택하세요.\n" +
+    private static final String INPUT_COURSE_LEVEL_MISSION_MESSAGE = "#############################################\n" +
+            "과정: 백엔드 | 프론트엔드\n" +
+            "미션:\n" +
+            "  - 레벨1: 자동차경주 | 로또 | 숫자야구게임\n" +
+            "  - 레벨2: 장바구니 | 결제 | 지하철노선도\n" +
+            "  - 레벨3: \n" +
+            "  - 레벨4: 성능개선 | 배포\n" +
+            "  - 레벨5: \n" +
+            "############################################\n" +
+            "과정, 레벨, 미션을 선택하세요.\n" +
             "ex) 백엔드, 레벨1, 자동차경주";
     private static final String REMATCHING_MESSAGE = "매칭 정보가 있습니다. 다시 매칭하시겠습니까?";
     public static final String YES = "예";
     public static final String NO = "아니오";
+    public static final String VERTICAL_VAR = " | ";
+
 
 
 
@@ -29,7 +40,7 @@ public class InputView {
 
     public static String askMatchingAgain() {
         System.out.println(REMATCHING_MESSAGE);
-        System.out.println(YES + OutputView.VERTICAL_VAR + NO);
+        System.out.println(YES + VERTICAL_VAR + NO);
         return Console.readLine();
     }
 }
