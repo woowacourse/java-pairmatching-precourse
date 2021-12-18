5749be9c513bbde7fab5f60c89152659bcb13342
jochungbeom <cndqjacndqja@gmail.com>
2021-12-18 13:37:17 +0900

    feat: 기능 선택 입력 구현

---
+++ src/main/java/pairmatching/view/InputView.java
@@ -0,0 +1,10 @@
+package pairmatching.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+	public static String inputPairFunction() {
+		return Console.readLine();
+	}
+}
