736197af8937424324930733fc46782aea040e8e
Seungmin Lim <seungmin9066@gmail.com>
2021-12-18 15:52:45 +0900

    [수정] 매칭 방법 수정

--- src/main/java/pairmatching/domain/PairMatching.java
+++ src/main/java/pairmatching/domain/PairMatching.java
@@ -1,42 +1,45 @@
 package pairmatching.domain;
 
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.stream.Collectors;
+import java.util.Set;
 
 public class PairMatching {
-	private HashMap<Curriculum, List<Crew>> matchingLog;
+	private HashMap<Curriculum, List<String>> matchingPair;
+	private HashMap<String, String> matchingLog;
 	private CrewGroup crewGroup = new CrewGroup();
 
 	public PairMatching() {
-
 	}
 
 	public void run(Curriculum curriculum) {
-		for (String name : crewGroup.getShuffledCrews()) {
-			List<Crew> crews = new LinkedList<>();
-			crews.add(new Crew(curriculum.getCourse(), name));
-			matchingLog.put(curriculum, crews);
+		if (isEmpty(curriculum)) {
+			matchingPair.put(curriculum, crewGroup.getShuffledCrews());
 		}
 	}
 
-	public List<List<Crew>> printMatching(Curriculum curriculum) {
+	public List<String> printMatching(Curriculum curriculum) {
 		if (isEmpty(curriculum)) {
 			throw new IllegalArgumentException();
 		}
-		return matchingLog.entrySet().stream()
+		return matchingPair.entrySet().stream()
 			.filter(cur -> cur.getKey().equals(curriculum))
 			.map(Map.Entry::getValue)
-			.collect(Collectors.toList());
+			.findAny()
+			.orElseThrow(() -> new IllegalArgumentException("못찾았습니다."));
 	}
 
 	public boolean isEmpty(Curriculum curriculum) {
-		Optional<Curriculum> curriculumOptional = matchingLog.keySet().stream()
+		Optional<Curriculum> curriculumOptional = matchingPair.keySet().stream()
 			.filter(cur -> cur.equals(curriculum))
 			.findAny();
 		return curriculumOptional.isPresent();
+	}
+
+	public void clearMatching() {
+		matchingPair = new HashMap<>();
+
 	}
 }
