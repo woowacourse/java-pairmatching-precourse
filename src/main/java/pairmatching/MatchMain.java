6f4962d1a2ca6d29f15560334a289f25e208092b
AlphaTechnic <alphatechnicist@gmail.com>
2021-12-18 17:39:56 +0900

    [feat] 매칭 로직의 주축을 담당하는 class 생성

--- src/main/java/pairmatching/MatchMain.java
+++ src/main/java/pairmatching/MatchMain.java
@@ -1,2 +1,56 @@
-package pairmatching;public class MatchMain {
+package pairmatching;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+
+import inputcontroller.inputValidator;
+import inputcontroller.modelGenerator;
+import models.Crew;
+import models.Pair;
+
+import static inputcontroller.inputGenerator.courseTypeInput;
+
+
+public class MatchMain {
+
+    public static void initMatch() {
+        if (!modelGenerator.readFromDisk()) {
+            return;
+        }
+        Crew.initBackMatch();
+        Crew.initFrontMatch();
+        inputValidator.initLevel2Missions();
+    }
+
+    public static void doFunction(int type) {
+        if (type == 1) {
+            pairMatch(courseTypeInput[0]);
+            return;
+        }
+        if (type == 2) {
+            pairPrint();
+            return;
+        }
+        if (type == 3) {
+            pairInit();
+            return;
+        }
+    }
+
+    public static void pairMatch(int type) {
+        int cnt = 3;
+        boolean flag = false;
+        while (cnt != 0) {
+            if (Crew.shuffleCrew(type)) {
+                flag = true;
+                break;
+            }
+            cnt--;
+        }
+
+        if (!flag) {
+            throw new IllegalArgumentException("[ERROR] 매칭 안 됨\n");
+        }
+    }
 }
