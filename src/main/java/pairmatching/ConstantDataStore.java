c2fbc4db5f71b9b6a87be785f7ab9701fc66b45d
KBY-TECH <soccer_by@naver.com>
2021-12-18 16:23:12 +0900

    feat: 초기 데이터 셋팅 기능 추가( readme 파일 읽기 데이터,단계별 미션 데이터)

---
+++ src/main/java/pairmatching/ConstantDataStore.java
@@ -0,0 +1,75 @@
+package pairmatching;
+
+import pairmatching.domain.Crew;
+import pairmatching.domain.Level;
+import pairmatching.utils.FileUtility;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class ConstantDataStore {
+    private final static List<String> readToListFromREADME = Arrays.asList("backend-crew.md", "frontend-crew.md");
+    private final static int BECK_END = 0;
+    private final static int FRONT_END = 1;
+
+    private final FileUtility fileReadUtility;
+
+    private Map<Level, List<String>> missionByLevel;
+    private List<Crew> backendCrews;
+    private List<Crew> frontedCrews;
+
+    public ConstantDataStore() {
+        fileReadUtility = FileUtility.getInstance();
+        injectMissionByLevelData();
+        readCrewsFromFile();
+    }
+
+    public static ConstantDataStore getInstance() {
+        return ConstantDataStore.LazyHolder.INSTANCE;
+    }
+
+    private static class LazyHolder {
+
+        public static final ConstantDataStore INSTANCE = new ConstantDataStore();
+    }
+
+    private void injectMissionByLevelData() {
+        List<String> level1 = Arrays.asList("자동차경주", "로또", "숫자야구게임");
+        missionByLevel.put(Level.LEVEL1, level1);
+        List<String> level2 = Arrays.asList("장바구니", "결제");
+        missionByLevel.put(Level.LEVEL2, level2);
+        List<String> level3 = new ArrayList<>();
+        missionByLevel.put(Level.LEVEL3, level3);
+        List<String> level4 = Arrays.asList("성능 개선", "배포");
+        missionByLevel.put(Level.LEVEL4, level4);
+        List<String> level5 = new ArrayList<>();
+        missionByLevel.put(Level.LEVEL5, level5);
+    }
+
+    private void readCrewsFromFile() {
+        backendCrews = new ArrayList<>();
+        frontedCrews = new ArrayList<>();
+        for (int group = 0; group < readToListFromREADME.size(); group++) {
+            if (group == BECK_END) {
+                backendCrews = fileReadUtility.readToListFromREADME(readToListFromREADME.get(group)).stream().map(Crew::new).collect(Collectors.toList());
+            } else if (group == FRONT_END) {
+                frontedCrews = fileReadUtility.readToListFromREADME(readToListFromREADME.get(group)).stream().map(Crew::new).collect(Collectors.toList());
+            }
+        }
+    }
+
+    public Map<Level, List<String>> getMissionByLevel() {
+        return missionByLevel;
+    }
+
+    public List<Crew> getBackendCrews() {
+        return backendCrews;
+    }
+
+    public List<Crew> getFrontedCrews() {
+        return frontedCrews;
+    }
+}
--- src/main/java/pairmatching/PairMatchingManagementApplication.java
+++ src/main/java/pairmatching/PairMatchingManagementApplication.java
@@ -1,42 +1,30 @@
 package pairmatching;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-
-import pairmatching.domain.Crew;
-import pairmatching.utils.FileUtility;
 import pairmatching.view.InputSystem;
 
 public class PairMatchingManagementApplication {
 
-    private final static List<String> readToListFromREADME = Arrays.asList("backend-crew.md", "frontend-crew.md");
-    private final static int BECK_END = 0;
-    private final static int FRONT_END = 1;
     private final static char PAIR_MATCHING = 1;
     private final static char PAIR_SELECT = 2;
     private final static char PAIR_RESET = 3;
     private final static char EXIT = 'Q';
 
-    private final FileUtility fileReadUtility;
     private final InputSystem inputSystem;
-
-    private List<Crew> backendCrews;
-    private List<Crew> frontedCrews;
+    private final ConstantDataStore constantDataStore;
 
     public PairMatchingManagementApplication() {
-        fileReadUtility = FileUtility.getInstance();
         inputSystem = InputSystem.getInstance();
+        constantDataStore = ConstantDataStore.getInstance();
     }
 
-    public void init() {
-        readCrewsFromFile();
-    }
 
     public void start() {
-        init();
         char inputFunctionNumber = inputSystem.inputFunctionList();
+        behaveFunction(inputFunctionNumber);
+        String courseAndLevelAndMission = inputSystem.inputPropertyInput();
+    }
+
+    private void behaveFunction(char inputFunctionNumber) {
         if (inputFunctionNumber == PAIR_MATCHING) {
 
         } else if (inputFunctionNumber == PAIR_SELECT) {
@@ -47,18 +35,5 @@ public class PairMatchingManagementApplication {
 
         }
     }
-
-    public void readCrewsFromFile() {
-        backendCrews = new ArrayList<>();
-        frontedCrews = new ArrayList<>();
-        for (int group = 0; group < readToListFromREADME.size(); group++) {
-            if (group == BECK_END) {
-                backendCrews = fileReadUtility.readToListFromREADME(readToListFromREADME.get(group)).stream().map(Crew::new).collect(Collectors.toList());
-            } else if (group == FRONT_END) {
-                frontedCrews = fileReadUtility.readToListFromREADME(readToListFromREADME.get(group)).stream().map(Crew::new).collect(Collectors.toList());
-            }
-        }
-    }
-
 
 }
