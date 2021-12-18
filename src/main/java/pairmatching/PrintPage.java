985fb42721ec50e74ad45f820b560e19277c6409
soulfever <soulfever01@naver.com>
2021-12-18 13:28:14 +0900

    REFACTOR : 화면 출력과 입력받는 부분을 나눔

--- src/main/java/pairmatching/PrintPage.java
+++ src/main/java/pairmatching/PrintPage.java
@@ -1,15 +1,13 @@
 package pairmatching;
 
-import camp.nextstep.edu.missionutils.Console;
-
 public class PrintPage {
-    public static String StartPage() {
+    public static void StartPage() {
         System.out.println("기능을 선택하세요.");
         System.out.println("1. 페어 매칭");
         System.out.println("2. 페어 조회");
         System.out.println("3. 페어 초기화");
         System.out.println("Q. 종료");
-        String answer = Console.readLine();
-        return answer;
+
     }
+
 }
--- src/main/java/pairmatching/Utils.java
+++ src/main/java/pairmatching/Utils.java
@@ -1,2 +1,9 @@
-package pairmatching;public class Utils {
+package pairmatching;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Utils {
+    public static void Input() {
+        String answer = Console.readLine();
+    }
 }
