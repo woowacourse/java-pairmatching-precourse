0ca78e444c015dea55c2b31737f232a55e440c94
pup-paw <wldusdl0310@gmail.com>
2021-12-18 13:34:36 +0900

    style: github 업로드시 인덴트 사이즈 설정

---
+++ src/main/java/pairmatching/util/HostMessage.java
@@ -0,0 +1,11 @@
+package pairmatching.util;
+
+public class HostMessage {
+	public static final String ASK_FUNCTION = "기능을 선택하세요.";
+	public static final String FUNCTION_1 = "1. 페어 매칭";
+	public static final String FUNCTION_2 = "2. 페어 조회";
+	public static final String FUNCTION_3 = "3. 페어 초기화";
+	public static final String FUNCTION_Q = "Q. 종료";
+	public static final String LINE_SEPARATOR = "#############################################";
+	public static
+}
