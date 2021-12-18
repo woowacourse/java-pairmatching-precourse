e626440d1f2db4dee91d47b75134e1552212365f
JunHyeok.Bang <bangvkf3@gmail.com>
2021-12-18 14:02:27 +0900

    feat: Crews 일급 컬렉션 구현
    Crew class와 Course enum 함께 초기화

---
+++ src/main/java/pairmatching/Crew.java
@@ -0,0 +1,13 @@
+package pairmatching;
+
+public class Crew {
+	private Course course;
+
+	public Crew(Course course, String name) {
+		this.course = course;
+		this.name = name;
+	}
+
+	private String name;
+
+}
---
+++ src/main/java/pairmatching/Crews.java
@@ -0,0 +1,11 @@
+package pairmatching;
+
+import java.util.List;
+
+public class Crews {
+	private final List<Crew> crewList;
+
+	public Crews(List<Crew> crewList) {
+		this.crewList = crewList;
+	}
+}
