38e5eb2211d3f66e5d96861b4069a219b81b1b20
kanamycine <jinyeong5320@gmail.com>
2021-12-18 17:14:12 +0900

    feat(Matching) : 매칭 코드 분리

--- src/main/java/pairmatching/domain/Matching.java
+++ src/main/java/pairmatching/domain/Matching.java
@@ -20,25 +20,33 @@ public class Matching {
 	public Set<List> matching(List<String> crewNames) {
 		Randoms.shuffle(crewNames);
 		if (crewNames.size() % 2 == 0) {
-			for (int i = 0; i < crewNames.size() / 2; i += 2) {
-				List<String> pair = new ArrayList<>();
-				pair.add(crewNames.get(i));
-				pair.add(crewNames.get(i + 1));
-				pairCrews.add(pair);
-			}
+			makeEvenNumberCrews(crewNames);
 		}
 		if (crewNames.size() % 2 != 0) {
-			for (int i = 0; i < (crewNames.size() / 2) - 1; i += 2) {
-				List<String> pair = new ArrayList<>();
-				pair.add(crewNames.get(i));
-				pair.add(crewNames.get(i + 1));
-				pairCrews.add(pair);
-			}
-			List<String> pair = new ArrayList<>();
-			pair.add(crewNames.get(crewNames.size() - 1));
-			pair.add(crewNames.get(crewNames.size() - 2));
-			pair.add(crewNames.get(crewNames.size() - 3));
+			makeOddNumberCrews(crewNames);
 		}
 		return pairCrews;
+	}
+
+	private void makeEvenNumberCrews(List<String> crewNames) {
+		for (int i = 0; i < crewNames.size() / 2; i += 2) {
+			List<String> pair = new ArrayList<>();
+			pair.add(crewNames.get(i));
+			pair.add(crewNames.get(i + 1));
+			pairCrews.add(pair);
+		}
+	}
+
+	private void makeOddNumberCrews(List<String> crewNames) {
+		for (int i = 0; i < (crewNames.size() / 2) - 1; i += 2) {
+			List<String> pair = new ArrayList<>();
+			pair.add(crewNames.get(i));
+			pair.add(crewNames.get(i + 1));
+			pairCrews.add(pair);
+		}
+		List<String> pair = new ArrayList<>();
+		pair.add(crewNames.get(crewNames.size() - 1));
+		pair.add(crewNames.get(crewNames.size() - 2));
+		pair.add(crewNames.get(crewNames.size() - 3));
 	}
 }
