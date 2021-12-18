b78411eae3646a67a08d9652b032380eba6efc4f
최창준 <aowert6518@gmail.com>
2021-12-18 14:02:19 +0900

    [fix] 함수명 변경

--- src/main/java/pairmatching/domain/MatchingService.java
+++ src/main/java/pairmatching/domain/MatchingService.java
@@ -13,7 +13,7 @@ import static pairmatching.constant.SystemMessage.FRONT_END_PROCESS_FILE_PATH;
 public class MatchingService {
     List<String> crewList = new ArrayList<>();
 
-    public void readCrewList123(String processName) throws IOException {
+    public void readCrewListBy(String processName) throws IOException {
         if (processName.equals("백엔드")) {
             readCrewList(BACK_END_PROCESS_FILE_PATH);
         }
