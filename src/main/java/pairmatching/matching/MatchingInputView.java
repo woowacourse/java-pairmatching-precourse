4c31be3710c90c2b5bde6718b6fef1c389a25630
TaeHyeon Kim <kth990303@naver.com>
2021-12-18 16:07:33 +0900

    fix: 매칭 중복 시 문구 수정

--- src/main/java/pairmatching/matching/MatchingInputView.java
+++ src/main/java/pairmatching/matching/MatchingInputView.java
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class MatchingInputView {
     public static String duplicateMatchingResult() {
         System.out.println("매칭 정보가 있습니다. 다시 매칭하시겠습니까?");
+        System.out.println("네 | 아니오");
         return Console.readLine();
     }
 }
