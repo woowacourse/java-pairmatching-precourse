110df8f17d4bd79c1f199d82c71f36d9d43cfcbe
yeon-06 <rnjstldus2@hs.ac.kr>
2021-12-18 15:56:19 +0900

    feat: 페어 매칭 기능 구현

--- src/main/java/pairmatching/Application.java
+++ src/main/java/pairmatching/Application.java
@@ -1,7 +1,27 @@
 package pairmatching;
 
+import pairmatching.controller.PairController;
+import pairmatching.model.enums.Option;
+
+import static pairmatching.view.InputView.selectOption;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO 구현 진행
+        PairController controller = new PairController();
+        Option input = null;
+        while (input != Option.QUIT) {
+            input = selectOption();
+            if (input == Option.MATCH_PAIR) {
+                controller.selectMatchPair();
+                continue;
+            }
+            if (input == Option.SELECT_PAIR) {
+                controller.printPair();
+                continue;
+            }
+            if (input == Option.INITIAL_PAIR) {
+                controller.initialPair();
+            }
+        }
     }
 }
--- src/main/java/pairmatching/controller/PairController.java
+++ src/main/java/pairmatching/controller/PairController.java
@@ -1,34 +1,39 @@
 package pairmatching.controller;
 
-import pairmatching.model.enums.Option;
+import camp.nextstep.edu.missionutils.Randoms;
+import pairmatching.model.MatchingInfo;
+import pairmatching.model.Pair;
+import pairmatching.model.enums.Course;
+import pairmatching.model.enums.Level;
+import pairmatching.utils.FileReader;
+import pairmatching.utils.Validator;
 
-import static pairmatching.view.InputView.selectOption;
+import java.util.ArrayList;
+import java.util.List;
+
+import static pairmatching.view.InputView.inputDetails;
+import static pairmatching.view.InputView.inputReMatch;
 
 public class PairController {
+    private final static String ERROR_OVER_TRY_CNT = "[ERROR] 매칭 시도 최대 횟수 초과";
+    private final static String SEPARATOR = ",";
+    private final static String ANSWER_YES = "네";
+    private final static int MAX_TRY_CNT = 3;
+    private final static int PAIR_CNT = 2;
+    private final static Validator validator = new Validator();
+
+    private List<MatchingInfo> matchingInfos;
+
     public PairController() {
     }
 
-    public void start() {
-        Option input = null;
-        while (input != Option.QUIT) {
-            input = selectOption();
-            if (input == Option.MATCH_PAIR) {
-                matchPair();
-                continue;
-            }
-            if (input == Option.SELECT_PAIR) {
-                printPair();
-                continue;
-            }
-            if (input == Option.INITIAL_PAIR) {
-                initialPair();
-                continue;
-            }
+    public void selectMatchPair() {
+        String[] arr = splitMatchingInfo(inputDetails());
+        MatchingInfo matchingInfo = new MatchingInfo(arr[0], arr[1], arr[2]);
+        if (!validator.isDuplicatedInfo(matchingInfos, matchingInfo)
+                || inputReMatch().equals(ANSWER_YES)) {
+            matchPair(matchingInfo);
         }
-    }
-
-    public void matchPair() {
-
     }
 
     public void printPair() {
@@ -37,5 +42,66 @@ public class PairController {
 
     public void initialPair() {
 
+    }
+
+    private void matchPair(MatchingInfo matchingInfo) {
+        FileReader fileReader = new FileReader(matchingInfo.getCourse().getName());
+        List<String> members = fileReader.getNameList();
+        List<Pair> pairHistory = getPairHistory(matchingInfo.getCourse(), matchingInfo.getLevel());
+
+        for (int i = 0; i < MAX_TRY_CNT; i++) {
+            List<Pair> pairs = getPairList(Randoms.shuffle(members), pairHistory);
+            if (pairs != null) {
+                matchingInfo.setMatchingPairs(pairs);
+                return;
+            }
+        }
+
+        throw new IllegalArgumentException(ERROR_OVER_TRY_CNT);
+    }
+
+    private List<Pair> getPairList(List<String> members, List<Pair> pairHistory) {
+        List<Pair> pairs = new ArrayList<>();
+        int size = members.size();
+        for (int i = 0; i < size; i += PAIR_CNT) {
+            Pair pair = new Pair(members.get(i), members.get(i + 1));
+            if (isContainedPair(pairHistory, pair)) {
+                return null;
+            }
+            pairs.add(pair);
+        }
+        if (size % PAIR_CNT != 0) {
+            pairs.get(pairs.size() - 1).addMember(members.get(size - 1));
+        }
+        return pairs;
+    }
+
+    private boolean isContainedPair(List<Pair> pairs, Pair nowPair) {
+        for (Pair pair : pairs) {
+            if (pair.isEquals(nowPair)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private List<Pair> getPairHistory(Course course, Level level) {
+        List<Pair> history = new ArrayList<>();
+        for (MatchingInfo matchingInfo : matchingInfos) {
+            if (!matchingInfo.isSameCourseAndLevel(course, level)) {
+                continue;
+            }
+            history.addAll(matchingInfo.getMatchingPairs());
+        }
+        return history;
+    }
+
+    private String[] splitMatchingInfo(String input) {
+        String[] arr = input.split(SEPARATOR);
+        int length = arr.length;
+        for (int i = 0; i < length; i++) {
+            arr[i] = arr[i].trim();
+        }
+        return arr;
     }
 }
