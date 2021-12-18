d5453d1132c9ea2ba7fa44819c49f89d805a52b8
devHudi <devhudi@gmail.com>
2021-12-18 17:47:11 +0900

    style: 개행 삽입

--- src/main/java/pairmatching/util/FormatChecker.java
+++ src/main/java/pairmatching/util/FormatChecker.java
@@ -3,7 +3,8 @@ package pairmatching.util;
 import java.util.regex.Pattern;
 
 public class FormatChecker {
-	private static final String COURSE_LEVEL_MISSION_INPUT_REGEX = "[a-zA-Z가-힣ㄱ-ㅎㅏ-ㅣ0-9]+, [a-zA-Z가-힣ㄱ-ㅎㅏ-ㅣ0-9]+, [a-zA-Z가-힣ㄱ-ㅎㅏ-ㅣ0-9]+";
+	private static final String COURSE_LEVEL_MISSION_INPUT_REGEX =
+		"[a-zA-Z가-힣ㄱ-ㅎㅏ-ㅣ0-9]+, [a-zA-Z가-힣ㄱ-ㅎㅏ-ㅣ0-9]+, [a-zA-Z가-힣ㄱ-ㅎㅏ-ㅣ0-9]+";
 
 	public static boolean validateCourseLevelMissionInputFormat(String input) {
 		return Pattern.matches(COURSE_LEVEL_MISSION_INPUT_REGEX, input);
