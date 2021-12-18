dab6db81c8c4712711c19de64cdc1697693b09db
PanghoChao <Sobrightlf@naver.com>
2021-12-18 14:37:19 +0900

    feat(FileReading): 프론트/백엔드  Crew 파일 가져와 읽기 구현

---
+++ src/main/java/pairmatching/FileReading.java
@@ -0,0 +1,43 @@
+package pairmatching;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class FileReading {
+    static String filepath = "C:\\Users\\shin\\java-pairmatching-precourse\\src\\main\\resources\\";
+    static String frontFileName = "frontend-crew.md";
+    static String backFileName = "backend-crew.md";
+    
+    public static List<String> frontCrewReading() {
+    	List<String> lines = null;
+    	try {
+    	    Path path = Paths.get(filepath + frontFileName);
+    	    lines = Files.readAllLines(path);
+    	    for(String line : lines) {
+    	        System.out.println(line);
+    	    }
+    	} catch (IOException e) {
+    	    e.printStackTrace();
+    	}
+
+    	return lines;
+    }
+    
+    public static List<String> backCrewReading() {
+    	List<String> lines = null;
+    	try {
+    	    Path path = Paths.get(filepath + backFileName);
+    	    lines = Files.readAllLines(path);
+    	    for(String line : lines) {
+    	        System.out.println(line);
+    	    }
+    	} catch (IOException e) {
+    	    e.printStackTrace();
+    	}
+
+    	return lines;
+    }
+}
