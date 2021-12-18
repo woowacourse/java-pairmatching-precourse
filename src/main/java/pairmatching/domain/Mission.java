8646b5d618e341cee0f5f0618ffc825dbddffa90
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2021-12-18 13:40:18 +0900

    feat: Mission 객체 생성 > Match 정보와 이름을 가짐

---
+++ src/main/java/pairmatching/domain/Mission.java
@@ -0,0 +1,19 @@
+package pairmatching.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Mission {
+
+    private final List<Match> matches;
+    private final String name;
+
+    public Mission(List<Match> matches, String name) {
+        this.matches = matches;
+        this.name = name;
+    }
+
+    public static Mission createEmptyMission(String name) {
+        return new Mission(new ArrayList<>(), name);
+    }
+}
