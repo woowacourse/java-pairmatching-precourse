4c3a5c8810caa9f456a989c9e68487a92420ba76
YuJeong <yjfflq4872@gmail.com>
2021-12-18 14:31:30 +0900

    feat: 매칭할 정보 입력 기능 구현

--- src/main/java/pairmatching/view/InputFormatValidator.java
+++ src/main/java/pairmatching/view/InputFormatValidator.java
@@ -1,9 +1,22 @@
 package pairmatching.view;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Pattern;
+
 public class InputFormatValidator {
 	private static final int MAIN_OPTION_COUNT = 3;
+	private static final int MATCHING_OPTION_COUNT = 3;
+	private static final int COURSE_INDEX = 0;
+	private static final int LEVEL_INDEX = 1;
+
+	private static final String DELIMITER = ", ";
+	private static final String BACKEND = "백엔드";
+	private static final String FRONTEND = "프론트엔드";
+	private static final String LEVEL_REGEX = "레벨[1-5]+";
 	private static final String NOT_INT_ERROR_MSG = "숫자 또는 Q를 입력해주세요.";
 	private static final String INVALID_RANGE_ERROR_MSG = "기능 번호를 확인해주세요.";
+	private static final String INVALID_MATCHING_INPUT_ERROR_MSG = "입력이 올바르지 않습니다.";
 
 	public static String validateOptionFormat(String option) throws IllegalArgumentException {
 		if (!option.equals("Q")) {
@@ -25,5 +38,31 @@ public class InputFormatValidator {
 		if (optionNum <= 0 || optionNum > MAIN_OPTION_COUNT) {
 			throw new IllegalArgumentException(INVALID_RANGE_ERROR_MSG);
 		}
+	}
+
+	public static List<String> validateMatchingOptionFormat(String option) throws IllegalArgumentException {
+		List<String> matchingOptions = splitOption(option);
+		if (matchingOptions.size() != MATCHING_OPTION_COUNT) {
+			throw new IllegalArgumentException(INVALID_MATCHING_INPUT_ERROR_MSG);
+		}
+		validateCourse(matchingOptions.get(COURSE_INDEX));
+		validateLevel(matchingOptions.get(LEVEL_INDEX));
+		return matchingOptions;
+	}
+
+	private static void validateCourse(String course) throws IllegalArgumentException {
+		if (!Pattern.matches(BACKEND, course) && !Pattern.matches(FRONTEND, course)) {
+			throw new IllegalArgumentException(INVALID_MATCHING_INPUT_ERROR_MSG);
+		}
+	}
+
+	private static void validateLevel(String level) throws IllegalArgumentException {
+		if (!Pattern.matches(LEVEL_REGEX, level)) {
+			throw new IllegalArgumentException(INVALID_MATCHING_INPUT_ERROR_MSG);
+		}
+	}
+
+	private static List<String> splitOption(String option) {
+		return Arrays.asList(option.split(DELIMITER));
 	}
 }
--- src/main/java/pairmatching/view/InputView.java
+++ src/main/java/pairmatching/view/InputView.java
@@ -1,9 +1,13 @@
 package pairmatching.view;
 
+import java.util.List;
+
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 	private static final String INPUT_OPTION_GUIDE = "기능을 선택하세요.";
+	private static final String INPUT_MATCHING_OPTION_GUIDE
+		= "과정, 레벨, 미션을 선택하세요.\n" + "ex) 백엔드, 레벨1, 자동차경주";
 
 	public static String inputOption() {
 		System.out.println(INPUT_OPTION_GUIDE);
@@ -13,7 +17,19 @@ public class InputView {
 			return InputFormatValidator.validateOptionFormat(option);
 		} catch (IllegalArgumentException e) {
 			OutputView.displayErrorMsg(e.getMessage());
+			return inputOption();
 		}
-		return inputOption();
+	}
+
+	public static List<String> inputMatchingOption() {
+		System.out.println(INPUT_MATCHING_OPTION_GUIDE);
+		String option = Console.readLine();
+
+		try {
+			return InputFormatValidator.validateMatchingOptionFormat(option);
+		} catch (IllegalArgumentException e) {
+			OutputView.displayErrorMsg(e.getMessage());
+			return inputMatchingOption();
+		}
 	}
 }
