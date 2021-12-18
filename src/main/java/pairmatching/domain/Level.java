c7c0e4204ed7773ad3d9c5bc1aa984d691c9d820
Hongdonggeon <ghd700@naver.com>
2021-12-18 13:55:33 +0900

    feat(level) : 입력한 레벨에 따 레벨 enum 객체 반환해주기

--- src/main/java/pairmatching/domain/Level.java
+++ src/main/java/pairmatching/domain/Level.java
@@ -1,5 +1,7 @@
 package pairmatching.domain;
 
+import java.util.Arrays;
+
 public enum Level {
 	LEVEL1("레벨1"),
 	LEVEL2("레벨2"),
@@ -13,5 +15,14 @@ public enum Level {
 		this.name = name;
 	}
 
-	// 추가 기능 구현
+	public String getName() {
+		return name;
+	}
+
+	public static Level mappingStringValue(String levelName) {
+		return Arrays.stream(Level.values())
+			.filter(level -> level.getName().equals(levelName))
+			.findFirst()
+			.orElseThrow( () -> new IllegalArgumentException("[ERROR] 레벨을 잘 못 입력하셨습니다."));
+	}
 }
