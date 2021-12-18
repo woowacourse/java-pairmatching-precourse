c3b3ade7a654cb06d076a9f3f7fbc5ca3d07ce6b
cjlee38 <gptpem38@gmail.com>
2021-12-18 17:01:10 +0900

    feat: 페어 매칭 대상이 홀수인경우 한 페어는 3인으로 구성한다.

--- src/main/java/pairmatching/domain/Pair.java
+++ src/main/java/pairmatching/domain/Pair.java
@@ -1,5 +1,6 @@
 package pairmatching.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -7,7 +8,11 @@ public class Pair {
 	private List<Crew> crews;
 
 	public Pair(List<Crew> crews) {
-		this.crews = crews;
+		this.crews = new ArrayList<>(crews);
+	}
+
+	public void add(Crew crew) {
+		this.crews.add(crew);
 	}
 
 	@Override
--- src/main/java/pairmatching/service/PairMatchingService.java
+++ src/main/java/pairmatching/service/PairMatchingService.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import pairmatching.domain.Crew;
@@ -16,7 +15,7 @@ public class PairMatchingService {
 	// todo : 같은 레벨에서 이미 페어를 맺은 크루와는 다시 페어로 매칭될 수 없다.
 	// todo : 3회 시도까지 매칭이 되지 않거나 매칭을 할 수 있는 경우의 수가 없으면 에러메시지를 출력한다.
 	public List<Pair> pairMatch(WoowaCourse woowaCourse) {
-		List<String> load = woowaCourse.load();
+		List<String> load = woowaCourse.loadCrewInfo();
 		List<String> shuffle = Randoms.shuffle(load);
 		List<Crew> collect = shuffle.stream().map(Crew::new).collect(Collectors.toList());
 		List<Pair> pairs = doMatch(collect);
@@ -30,6 +29,13 @@ public class PairMatchingService {
 			Crew secondCrew = shuffle.get(i + 1);
 			pairs.add(new Pair(Arrays.asList(firstCrew, secondCrew)));
 		}
+		addLastIfCrewNumberOdd(shuffle, pairs);
 		return pairs;
+	}
+
+	private void addLastIfCrewNumberOdd(List<Crew> shuffle, List<Pair> pairs) {
+		if (shuffle.size() % 2 != 0) {
+			pairs.get(pairs.size() - 1).add(shuffle.get(shuffle.size() - 1));
+		}
 	}
 }
