96c964707ddfeda48ce0b727735a4e0c6f9d7bb8
SuyeonChoi <tndus970120@gmail.com>
2021-12-18 15:23:23 +0900

    feat(PairMatchingOutputView): 과정과 미션 출력 기능 구현

--- src/main/java/pairmatching/view/PairMatchingOutputView.java
+++ src/main/java/pairmatching/view/PairMatchingOutputView.java
@@ -6,6 +6,20 @@ public class PairMatchingOutputView {
 	private static final String PAIR_FINDING_FUNCTION = "2. 페어 조회";
 	private static final String PAIR_INITIALIZING_FUNCTION = "3. 페어 초기화";
 	private static final String QUIT_FUNCTION = "Q. 종료";
+	private static final String COURSE_LEVEL_MISSION_INPUT = "과정, 레벨, 미션을 선택하세요.";
+	private static final String COURSE = "과정";
+	private static final String BACKEND = "백엔드";
+	private static final String FRONTEND = "프론트엔드";
+	private static final String MISSION = "미션";
+	private static final String LEVEL_ONE = "- 레벨1: 자동차경주 | 로또 | 숫자야구게임";
+	private static final String LEVEL_TWO = "- 레벨2: 장바구니 | 결제 | 지하철노선도";
+	private static final String LEVEL_THREE = "- 레벨3: ";
+	private static final String LEVEL_FOUR = "- 레벨4: 성능개선 | 배포";
+	private static final String LEVEL_FIVE = "- 레벨5: ";
+	private static final String LINE_HASH = "#############################################";
+	private static final String DELIMITER = " | ";
+	private static final String COLON = ":";
+	private static final String WHITESPACE = " ";
 	private static final String LINE_WRAPPING = "\n";
 
 	private PairMatchingOutputView() {
@@ -23,5 +37,45 @@ public class PairMatchingOutputView {
 			.append(LINE_WRAPPING)
 			.append(QUIT_FUNCTION);
 		System.out.println(functionInputMessage.toString());
+	}
+
+	public static void printCourseAndMissionInputMessage() {
+		printCourseAndMission();
+		System.out.println(COURSE_LEVEL_MISSION_INPUT);
+	}
+	public static void printCourseAndMission() {
+		StringBuilder courseAndMissionMessage = new StringBuilder();
+		courseAndMissionMessage.append(LINE_WRAPPING)
+			.append(LINE_HASH)
+			.append(LINE_WRAPPING)
+			.append(printCourses())
+			.append(LINE_WRAPPING)
+			.append(printMissions())
+			.append(LINE_HASH);
+		System.out.println(courseAndMissionMessage.toString());
+	}
+
+	private static String printCourses() {
+		StringBuilder course = new StringBuilder();
+		return course.append(COURSE)
+			.append(COLON)
+			.append(WHITESPACE)
+			.append(BACKEND)
+			.append(DELIMITER)
+			.append(FRONTEND)
+			.toString();
+	}
+
+	private static String printMissions() {
+		StringBuilder mission = new StringBuilder();
+		return mission.append(MISSION)
+			.append(COLON)
+			.append(LINE_WRAPPING)
+			.append(LEVEL_ONE + LINE_WRAPPING)
+			.append(LEVEL_TWO + LINE_WRAPPING)
+			.append(LEVEL_THREE + LINE_WRAPPING)
+			.append(LEVEL_FOUR + LINE_WRAPPING)
+			.append(LEVEL_FIVE + LINE_WRAPPING)
+			.toString();
 	}
 }
