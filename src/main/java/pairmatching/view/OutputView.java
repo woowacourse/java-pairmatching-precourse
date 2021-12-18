2f3a5a2e2809e6734fb44afc062c5b19c01f7364
iamsseaa <homingsea@gmail.com>
2021-12-18 13:55:15 +0900

    feat: 안내 문구 출력 클래스

--- src/main/java/pairmatching/view/OutputView.java
+++ src/main/java/pairmatching/view/OutputView.java
@@ -1,4 +1,19 @@
 package pairmatching.view;
 
 public class OutputView {
+    public static void printMain() {
+        System.out.println(Constant.ASK_CHOICE);
+        System.out.println(Constant.CHOICE_ONE);
+        System.out.println(Constant.CHOICE_TWO);
+        System.out.println(Constant.CHOICE_THREE);
+        System.out.println(Constant.CHOICE_FOUR);
+    }
+
+    public static void printPairMatching() {
+        System.out.println(Constant.RESULT_PAIR_MATCHING);
+    }
+
+    public static void reset() {
+        System.out.println(Constant.RESET);
+    }
 }
