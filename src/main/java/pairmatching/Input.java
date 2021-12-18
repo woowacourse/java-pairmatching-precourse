6b7900478f412c0eec65739c99cf87f6b468ce61
wannagohome <iwwuf7@gmail.com>
2021-12-18 15:29:03 +0900

    refactor: separate printing logic from input class
     - input 클래스에 있는 print 로직들을 새로운 Printer class로 분리

--- src/main/java/pairmatching/Input.java
+++ src/main/java/pairmatching/Input.java
@@ -1,25 +1,12 @@
 package pairmatching;
 
 import camp.nextstep.edu.missionutils.Console;
-import pairmatching.domain.Course;
 import pairmatching.domain.Function;
-import pairmatching.domain.Level;
 
 public class Input {
-	private static final String FUNCTION_SELECT_TEXT = "기능을 선택하세요.";
-	private static final String BORDER_LINE_TEXT = "#############################################";
-	private static final String COURSE_TEXT = "과정: ";
-	private static final String MISSION_TEXT = "미션: ";
-	private static final String NEW_LINE = "\n";
-
 	public static Function requestToSelectFunction() {
-		printFunctions();
+		Printer.printFunctions();
 		return readFunctionSelect();
-	}
-
-	private static void printFunctions() {
-		System.out.println(FUNCTION_SELECT_TEXT);
-		Function.getFunctionList().forEach(System.out::println);
 	}
 
 	private static Function readFunctionSelect() {
@@ -34,26 +21,6 @@ public class Input {
 	}
 
 	public static void requestToSelectMission() {
-		System.out.println(buildMissionList());
-	}
-
-	private static String buildMissionList() {
-		String result = "";
-		result = appendCourse(result);
-		result = appendMission(result);
-		result = enclosedWithBorder(result);
-		return result;
-	}
-
-	private static String appendCourse(String str) {
-		return str + COURSE_TEXT + Course.listAsString() + NEW_LINE;
-	}
-
-	private static String appendMission(String str) {
-		return str + MISSION_TEXT + NEW_LINE + Level.listAsString();
-	}
-
-	private static String enclosedWithBorder(String str) {
-		return BORDER_LINE_TEXT + NEW_LINE + str + NEW_LINE + BORDER_LINE_TEXT + NEW_LINE;
+		Printer.printMissions();
 	}
 }
---
+++ src/main/java/pairmatching/Printer.java
@@ -0,0 +1,42 @@
+package pairmatching;
+
+import pairmatching.domain.Course;
+import pairmatching.domain.Function;
+import pairmatching.domain.Level;
+
+public class Printer {
+	private static final String FUNCTION_SELECT_TEXT = "기능을 선택하세요.";
+	private static final String BORDER_LINE_TEXT = "#############################################";
+	private static final String COURSE_TEXT = "과정: ";
+	private static final String MISSION_TEXT = "미션: ";
+	private static final String NEW_LINE = "\n";
+
+	public static void printFunctions() {
+		System.out.println(FUNCTION_SELECT_TEXT);
+		Function.getFunctionList().forEach(System.out::println);
+	}
+
+	public static void printMissions() {
+		System.out.println(buildMissionList());
+	}
+
+	private static String buildMissionList() {
+		String result = "";
+		result = appendCourse(result);
+		result = appendMission(result);
+		result = enclosedWithBorder(result);
+		return result;
+	}
+
+	private static String appendCourse(String str) {
+		return str + COURSE_TEXT + Course.listAsString() + NEW_LINE;
+	}
+
+	private static String appendMission(String str) {
+		return str + MISSION_TEXT + NEW_LINE + Level.listAsString();
+	}
+
+	private static String enclosedWithBorder(String str) {
+		return BORDER_LINE_TEXT + NEW_LINE + str + NEW_LINE + BORDER_LINE_TEXT + NEW_LINE;
+	}
+}
