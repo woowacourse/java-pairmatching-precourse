94c0feae8438eecd4146d6eac9cb7b54841ea70d
monk <2012.monk@gmail.com>
2021-12-18 13:23:05 +0900

    feat: add print error message with prefix

---
+++ src/main/java/pairmatching/crew/ErrorPrinter.java
@@ -0,0 +1,11 @@
+package pairmatching.crew;
+
+public class ErrorPrinter {
+
+    private static final String ERR_PREFIX = "[ERROR]";
+
+    public static void print(Throwable e) {
+        System.out.println(String.join(ERR_PREFIX, e.getMessage()));
+    }
+}
+
