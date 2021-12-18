d53d5b67d3c67a49802c77e8fdd18cf78384b220
Hongdonggeon <ghd700@naver.com>
2021-12-18 13:43:45 +0900

    feat(course) : 입력한 과정으로 존재하는 코스 매핑해주기

--- src/main/java/pairmatching/domain/Course.java
+++ src/main/java/pairmatching/domain/Course.java
@@ -1,5 +1,7 @@
 package pairmatching.domain;
 
+import java.util.Arrays;
+
 public enum Course {
 	BACKEND("백엔드"),
 	FRONTEND("프론트엔드");
@@ -10,5 +12,15 @@ public enum Course {
 		this.name = name;
 	}
 
+	public String getName() {
+		return name;
+	}
+
 	// 추가 기능 구현
+	public Course mappingStringValue(String courseName) {
+		return Arrays.stream(Course.values())
+			.filter(course -> course.getName().equals(courseName))
+			.findFirst()
+			.orElseThrow( () -> new IllegalArgumentException("[ERROR] 과정을 잘 못 입력하셨습니다."));
+	}
 }
