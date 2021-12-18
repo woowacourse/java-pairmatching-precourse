7db72d4ec42828dcdba5c2d85e45246dd81a0224
KIM <km0192@naver.com>
2021-12-18 14:53:48 +0900

    feat(utils.IsCommands) : 명령어인지 확인하는 유효성검사 클래스

--- src/main/java/utils/IsCommand.java
+++ src/main/java/utils/IsCommand.java
@@ -1,2 +1,13 @@
-package utils;public class IsCommand {
+package utils;
+
+public class IsCommand implements Validator {
+	public boolean run(String input) {
+		try {
+			Commands.getCommandByKey(input);
+		} catch (IllegalArgumentException error) {
+			System.out.println(error);
+			return true;
+		}
+		return false;
+	}
 }
