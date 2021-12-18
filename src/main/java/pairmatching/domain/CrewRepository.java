aea166ae3abe492fed177a6d5384f295cf584abc
sure-why-not <pkse1213@gmail.com>
2021-12-18 16:38:52 +0900

    feat: (페어매칭) 매칭시킬 크루 순서를 랜덤으로 섞기 구현

--- src/main/java/pairmatching/domain/CrewRepository.java
+++ src/main/java/pairmatching/domain/CrewRepository.java
@@ -2,11 +2,28 @@ package pairmatching.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class CrewRepository {
-	public static final List<Crew> crews = new ArrayList<>();
+	public static final List<Crew> backEndCrews = new ArrayList<>();
+	public static final List<Crew> frontEndCrews = new ArrayList<>();
 
-	public static void addCrew(Crew crew) {
-		crews.add(crew);
+	public static void addBackEndCrew(Crew crew) {
+		backEndCrews.add(crew);
+	}
+
+	public static void addFrontEndCrew(Crew crew) {
+		frontEndCrews.add(crew);
+	}
+
+	public static List<Crew> getBackEndCrews() {
+		List<String> names = backEndCrews.stream()
+			.map(crew -> crew.getName())
+			.collect(Collectors.toList());
+		return Randoms.shuffle(names).stream()
+			.map(name -> Crew.from(Course.BACKEND, name))
+			.collect(Collectors.toList());
 	}
 }
