cfd2a52d8197314abd495926776711492f704771
pup-paw <wldusdl0310@gmail.com>
2021-12-18 16:25:11 +0900

    feat: 프로젝트 내애서 사용되는 미션 객체 생성

---
+++ src/main/java/pairmatching/model/Mission.java
@@ -0,0 +1,27 @@
+package pairmatching.model;
+
+import java.util.ArrayList;
+
+public class Mission {
+	Level level;
+	String name;
+	public ArrayList<String> pairs;
+
+	public Mission(Level level, String name) {
+		this.level = level;
+		this.name = name;
+		this.pairs = new ArrayList<>();
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public ArrayList<String> getPairs() {
+		return this.pairs;
+	}
+
+	public Level getLevel() {
+		return this.level;
+	}
+}
