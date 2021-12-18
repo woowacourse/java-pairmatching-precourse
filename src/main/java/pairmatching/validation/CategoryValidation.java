9c62ccb96d8bbe5961ef8fa6a41bb82c50bc298b
iamsseaa <homingsea@gmail.com>
2021-12-18 17:42:10 +0900

    feat: 카테고리 구분이 되었는지 확인

--- src/main/java/pairmatching/validation/CategoryValidation.java
+++ src/main/java/pairmatching/validation/CategoryValidation.java
@@ -5,11 +5,18 @@ import pairmatching.view.Constant;
 public class CategoryValidation {
     public static void validateCategory(String input) {
         isEmpty(input);
+        isContains(input);
     }
 
     public static void isEmpty(String input) {
         if (input.length() == 0) {
             throw new IllegalArgumentException(Constant.ERROR_IS_EMPTY);
+        }
+    }
+
+    public static void isContains(String input) {
+        if (!input.contains(Constant.INPUT_DELIMITER)) {
+            throw new IllegalArgumentException(Constant.ERROR_IS_NO_DELIMITER);
         }
     }
 }
