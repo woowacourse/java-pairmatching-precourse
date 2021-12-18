10ef9e18d0e669a3d47a2f29addeb8811e092067
Seungmin Lim <seungmin9066@gmail.com>
2021-12-18 16:12:21 +0900

    [추가] 만난것을 기록하는 메서드 추가

--- src/main/java/pairmatching/domain/PairMatching.java
+++ src/main/java/pairmatching/domain/PairMatching.java
@@ -4,7 +4,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.Set;
 
 public class PairMatching {
 	private HashMap<Curriculum, List<String>> matchingPair;
@@ -15,8 +14,43 @@ public class PairMatching {
 	}
 
 	public void run(Curriculum curriculum) {
+		List<String> crews = getCrews();
 		if (isEmpty(curriculum)) {
-			matchingPair.put(curriculum, crewGroup.getShuffledCrews());
+			matchingPair.put(curriculum, crews);
+			logMeet(crews, curriculum.getLevel());
+		}
+	}
+
+	private List<String> getCrews() {
+		for (int i = 0; i < 3; i++) {
+			List<String> shuffledCrews = crewGroup.getShuffledCrews();
+			if (!checkMetBefore(shuffledCrews)) {
+				return shuffledCrews;
+			}
+		}
+		throw new IllegalArgumentException("3회 초과");
+	}
+
+	public boolean isEmpty(Curriculum curriculum) {
+		Optional<Curriculum> curriculumOptional = matchingPair.keySet().stream()
+			.filter(cur -> cur.equals(curriculum))
+			.findAny();
+		return curriculumOptional.isPresent();
+	}
+
+	private void logMeet(List<String> crews, String level) {
+		for (int i = 0; i < crews.size(); ) {
+			matchingLog.put(crews.get(i) + level, crews.get(i + 1));
+			matchingLog.put(crews.get(i + 1) + level, crews.get(i));
+			i += 2;
+		}
+		if (crews.size() / 2 == 1) {
+			matchingLog.put(crews.get(crews.size() - 1) + level, crews.get(crews.size() - 2));
+			matchingLog.put(crews.get(crews.size() - 1) + level, crews.get(crews.size() - 3));
+			matchingLog.put(crews.get(crews.size() - 2) + level, crews.get(crews.size() - 1));
+			matchingLog.put(crews.get(crews.size() - 2) + level, crews.get(crews.size() - 1));
+			matchingLog.put(crews.get(crews.size() - 3) + level, crews.get(crews.size() - 1));
+			matchingLog.put(crews.get(crews.size() - 3) + level, crews.get(crews.size() - 1));
 		}
 	}
 
@@ -31,11 +65,8 @@ public class PairMatching {
 			.orElseThrow(() -> new IllegalArgumentException("못찾았습니다."));
 	}
 
-	public boolean isEmpty(Curriculum curriculum) {
-		Optional<Curriculum> curriculumOptional = matchingPair.keySet().stream()
-			.filter(cur -> cur.equals(curriculum))
-			.findAny();
-		return curriculumOptional.isPresent();
+	private boolean checkMetBefore(List<String> crews) {
+
 	}
 
 	public void clearMatching() {
