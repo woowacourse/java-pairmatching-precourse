1b7a6c9f8119267f41d557c11256648a69366f45
eonju97 <eonju97.dev@gmail.com>
2021-12-18 17:46:43 +0900

    feat: 에러메세지 추가

--- src/main/java/pairmatching/ErrorMessage.java
+++ src/main/java/pairmatching/ErrorMessage.java
@@ -1,9 +1,9 @@
 package pairmatching;
 
 public enum ErrorMessage {
-    WRONG_INPUT_MESSAGE("[ERROR] 입력형식이 잘못되었습니다."),
-    MISSION_NOT_FOUND_MESSAGE("[ERROR] 없는 미션입니다."),
-    EMPTY_ERROR_MESSAGE("[ERROR] 매칭 이력이 없습니다.");
+    WRONG_INPUT_MESSAGE("입력형식이 잘못되었습니다."),
+    MISSION_NOT_FOUND_MESSAGE("없는 미션입니다."),
+    EMPTY_ERROR_MESSAGE("매칭 이력이 없습니다.");
 
     private final String message;
 
