55a11b7293c6bc3f459de7a60fccb1f9d9b36856
BeomsooKim <kbsat@naver.com>
2021-12-18 14:49:16 +0900

    refactor: 출력부분 OutputView로 이동 및 PairMatchingController 리팩토링

--- src/main/java/pairmatching/controller/PairMatchingController.java
+++ src/main/java/pairmatching/controller/PairMatchingController.java
@@ -9,14 +9,27 @@ import pairmatching.domain.Level;
 import pairmatching.domain.Mission;
 import pairmatching.domain.Missions;
 import pairmatching.view.InputView;
+import pairmatching.view.OutputView;
 
 public class PairMatchingController {
 	public void run() {
 		// 크루 이름 획득
 		List<String> backCrewNames = InputView.getCrewNameByFile("backend-crew.md");
 		List<String> frontCrewNames = InputView.getCrewNameByFile("frontend-crew.md");
+		Missions missions = initMissions();
+		Crews crews = initCrews(backCrewNames, frontCrewNames);
+		// 사전 제공 정보 출력
+		OutputView.printCourseAndMissionStatus(missions);
+	}
 
-		// 미션 등록
+	private Crews initCrews(List<String> backCrewNames, List<String> frontCrewNames) {
+		Crews crews = new Crews();
+		crews.addByCrewName(backCrewNames, Course.BACKEND);
+		crews.addByCrewName(frontCrewNames, Course.FRONTEND);
+		return crews;
+	}
+
+	private Missions initMissions() {
 		List<Mission> missionList = new ArrayList<>();
 		missionList.add(new Mission(Level.LEVEL1, "자동차경주"));
 		missionList.add(new Mission(Level.LEVEL1, "로또"));
@@ -26,64 +39,6 @@ public class PairMatchingController {
 		missionList.add(new Mission(Level.LEVEL2, "지하철노선도"));
 		missionList.add(new Mission(Level.LEVEL4, "성능개선"));
 		missionList.add(new Mission(Level.LEVEL4, "배포"));
-		Missions missions = new Missions(missionList);
-
-		// 크루 등록
-		Crews crews = new Crews();
-		crews.addByCrewName(backCrewNames, Course.BACKEND);
-		crews.addByCrewName(frontCrewNames, Course.FRONTEND);
-
-		// 사전 제공 정보 출력
-		printSeparator();
-		printCourseNames();
-		printMissionsNames(missions);
-		printSelectMatchingOption();
-		printSeparator();
-	}
-
-	private void printSeparator() {
-		System.out.println("#############################################");
-	}
-
-	private void printSelectMatchingOption() {
-		System.out.println("과정, 레벨, 미션을 선택하세요.");
-		System.out.println("ex) 백엔드, 레벨1, 자동차경주");
-	}
-
-	private void printMissionsNames(Missions missions) {
-		StringBuilder sb = new StringBuilder();
-		System.out.println("미션: ");
-		for (Level level : Level.values()) {
-			sb.append("  - ");
-			sb.append(level.getName());
-			sb.append(": ");
-			Missions nowLevelMissions = missions.getByLevel(level);
-			if (nowLevelMissions.isEmpty()) {
-				System.out.println(sb.toString());
-				sb.setLength(0);
-				continue;
-			}
-			List<String> missionsNames = nowLevelMissions.getNames();
-			for (String missionName : missionsNames) {
-				sb.append(missionName);
-				sb.append(" | ");
-			}
-			String s2 = sb.toString();
-			String substring1 = sb.substring(0, s2.length() - 3);
-			System.out.println(substring1);
-			sb.setLength(0);
-		}
-	}
-
-	private void printCourseNames() {
-		System.out.print("과정: ");
-		StringBuilder sb = new StringBuilder();
-		for (Course course : Course.values()) {
-			sb.append(course.getName());
-			sb.append(" | ");
-		}
-		String s = sb.toString();
-		String substring = s.substring(0, s.length() - 3);
-		System.out.println(substring);
+		return new Missions(missionList);
 	}
 }
--- src/main/java/pairmatching/view/OutputView.java
+++ src/main/java/pairmatching/view/OutputView.java
@@ -1,4 +1,63 @@
 package pairmatching.view;
 
+import java.util.List;
+
+import pairmatching.domain.Course;
+import pairmatching.domain.Level;
+import pairmatching.domain.Missions;
+
 public class OutputView {
+	public static void printCourseAndMissionStatus(Missions missions) {
+		printSeparator();
+		printCourseNames();
+		printMissionsNames(missions);
+		printSelectMatchingOption();
+		printSeparator();
+	}
+
+	private static void printSeparator() {
+		System.out.println("#############################################");
+	}
+
+	private static void printSelectMatchingOption() {
+		System.out.println("과정, 레벨, 미션을 선택하세요.");
+		System.out.println("ex) 백엔드, 레벨1, 자동차경주");
+	}
+
+	private static void printMissionsNames(Missions missions) {
+		StringBuilder sb = new StringBuilder();
+		System.out.println("미션: ");
+		for (Level level : Level.values()) {
+			sb.append("  - ");
+			sb.append(level.getName());
+			sb.append(": ");
+			Missions nowLevelMissions = missions.getByLevel(level);
+			if (nowLevelMissions.isEmpty()) {
+				System.out.println(sb.toString());
+				sb.setLength(0);
+				continue;
+			}
+			List<String> missionsNames = nowLevelMissions.getNames();
+			for (String missionName : missionsNames) {
+				sb.append(missionName);
+				sb.append(" | ");
+			}
+			String s2 = sb.toString();
+			String substring1 = sb.substring(0, s2.length() - 3);
+			System.out.println(substring1);
+			sb.setLength(0);
+		}
+	}
+
+	private static void printCourseNames() {
+		System.out.print("과정: ");
+		StringBuilder sb = new StringBuilder();
+		for (Course course : Course.values()) {
+			sb.append(course.getName());
+			sb.append(" | ");
+		}
+		String s = sb.toString();
+		String substring = s.substring(0, s.length() - 3);
+		System.out.println(substring);
+	}
 }
