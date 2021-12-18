01997bde0a91f7a30be16d785f58604849368222
GYUTAEKIM <kkt219a@gmail.com>
2021-12-18 17:25:20 +0900

    feat: 페어들 초기화 구현
    - 페어들의 초기화하는 기능을 구현한다.

---
+++ src/main/java/pairmatching/domain/Pairs.java
@@ -0,0 +1,12 @@
+package pairmatching.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Pairs {
+	private final List<Pair> pairs = new ArrayList<>();
+
+	public void init() {
+		pairs.clear();
+	}
+}
