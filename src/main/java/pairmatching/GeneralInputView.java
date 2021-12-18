8338032b085a33e9314f510b9b989c503c59d96f
TaeHyeon Kim <kth990303@naver.com>
2021-12-18 13:30:21 +0900

    feat: 기능 선택 입력받는 메소드 구현

--- src/main/java/pairmatching/GeneralInputView.java
+++ src/main/java/pairmatching/GeneralInputView.java
@@ -3,6 +3,14 @@ package pairmatching;
 import camp.nextstep.edu.missionutils.Console;
 
 public class GeneralInputView {
+    public static String inputFunctionByClient() {
+        System.out.println("1. 페어 매칭");
+        System.out.println("2. 페어 조회");
+        System.out.println("3. 페어 초기화");
+        System.out.println("Q. 종료");
+        return Console.readLine();
+    }
+
     public static String inputCourseByClient() {
         GeneralOutputView.preCourseInfoPrint();
         System.out.println("과정, 레벨, 미션을 선택하세요.");
