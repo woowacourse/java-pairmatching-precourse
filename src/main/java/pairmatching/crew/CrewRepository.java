9a31f2db5a39bdc966665fb97b361b24224a8d80
iamsseaa <homingsea@gmail.com>
2021-12-18 15:42:57 +0900

    fix: 매칭 성공시 저장하는 메소드 수정

--- src/main/java/pairmatching/crew/CrewRepository.java
+++ src/main/java/pairmatching/crew/CrewRepository.java
@@ -9,19 +9,17 @@ import java.util.List;
 public class CrewRepository {
     static HashMap<Level,List<List<String>>> crews = new HashMap<>();
 
-    public static void savePair(Level level, List<String> crew) {
-        List<List<String>> pairs = crews.get(level);
-        pairs.add(crew);
-        crews.put(level, pairs);
+    public static void savePair(Level level, List<List<String>> crew) {
+        crews.put(level, crew);
     }
 
     public static boolean checkDuplicated(Level level, List<String> crew) {
         List<List<String>> pairs = crews.get(level);
         for (List<String> pair : pairs) {
             if (pair.containsAll(crew)) {
-                return false;
+                return true;
             }
         }
-        return true;
+        return false;
     }
 }
--- src/main/java/pairmatching/crew/PairMatching.java
+++ src/main/java/pairmatching/crew/PairMatching.java
@@ -1,15 +1,23 @@
 package pairmatching.crew;
 
+import pairmatching.Level;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class PairMatching {
-    public static List<List<String>> getPairedCrew(List<String> crew) {
+    static List<List<String>> pairCrew = new ArrayList<>();
+
+    public static boolean isPairedCrew(Level level, List<String> crew) {
         if (isOdd(crew)) {
-            return matchOdd(crew);
+            return matchOdd(level, crew);
         }
-        return matchEven(crew);
+        return matchEven(level, crew);
+    }
+
+    public static List<List<String>> getPairCrew() {
+        return pairCrew;
     }
 
     public static boolean isOdd(List<String> crew) {
@@ -19,35 +27,34 @@ public class PairMatching {
         return true;
     }
 
-    public static List<List<String>> matchOdd(List<String> crew) {
-        List<List<String>> pairCrew = new ArrayList<>();
+    public static boolean matchOdd(Level level, List<String> crew) {
         List<String> crews = crew;
-        while (true) {
-            if (crews.size() == 3) {
-                List<String> pair = Arrays.asList(crews.get(0), crews.get(1), crews.get(2));
-                pairCrew.add(pair);
-                break;
-            }
+        while (crews.size() != 1) {
             List<String> pair = Arrays.asList(crews.get(0), crews.get(1));
+            if (CrewRepository.checkDuplicated(level, pair)) {
+                return false;
+            }
             pairCrew.add(pair);
             crews.remove(0);
             crews.remove(1);
         }
-        return pairCrew;
+        pairCrew.get(-1).add(crews.get(0));
+        CrewRepository.savePair(level, pairCrew);
+        return true;
     }
 
-    public static List<List<String>> matchEven(List<String> crew) {
-        List<List<String>> pairCrew = new ArrayList<>();
+    public static boolean matchEven(Level level, List<String> crew) {
         List<String> crews = crew;
-        while (true) {
-            if (crews.size() == 0) {
-                break;
-            }
+        while (crews.size() != 0) {
             List<String> pair = Arrays.asList(crews.get(0), crews.get(1));
+            if (CrewRepository.checkDuplicated(level, pair)) {
+                return false;
+            }
             pairCrew.add(pair);
             crews.remove(0);
             crews.remove(1);
         }
-        return pairCrew;
+        CrewRepository.savePair(level, pairCrew);
+        return true;
     }
 }
