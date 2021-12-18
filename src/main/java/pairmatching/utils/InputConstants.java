d889e46b36b4f8af021494edf3f674352050f55f
yeon-06 <rnjstldus2@hs.ac.kr>
2021-12-18 16:33:28 +0900

    style: 오타 수정 및 줄바꿈 추가

--- src/main/java/pairmatching/utils/InputConstants.java
+++ src/main/java/pairmatching/utils/InputConstants.java
@@ -1,13 +1,13 @@
 package pairmatching.utils;
 
 public class InputConstants {
-    public static final String OPTIONS = "기능을 선택하세요.\n" +
+    public static final String OPTIONS = "\n기능을 선택하세요.\n" +
             "1. 페어 매칭\n" +
             "2. 페어 조회\n" +
             "3. 페어 초기화\n" +
             "Q. 종료";
 
-    public static final String PRECOURSE_AND_MISSIONS = "#############################################\n" +
+    public static final String PRECOURSE_AND_MISSIONS = "\n#############################################\n" +
             "과정: 백엔드 | 프론트엔드\n" +
             "미션:\n" +
             "  - 레벨1: 자동차경주 | 로또 | 숫자야구게임\n" +
@@ -19,9 +19,8 @@ public class InputConstants {
             "과정, 레벨, 미션을 선택하세요.\n" +
             "ex) 백엔드, 레벨1, 자동차경주";
 
-    public static final String RE_MATCHING = "매칭 정보가 있습니다. 다시 매칭하시겠습니까?\n" +
-            "네 | 아니오\n" +
-            "네";
+    public static final String RE_MATCHING = "\n매칭 정보가 있습니다. 다시 매칭하시겠습니까?\n" +
+            "네 | 아니오";
 
-    public static final String MATCHING_RESULT_GUIDE = "페어 매칭 결과입니다.";
+    public static final String MATCHING_RESULT_GUIDE = "\n페어 매칭 결과입니다.";
 }
