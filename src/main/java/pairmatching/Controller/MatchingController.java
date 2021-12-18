3286438408e83bf59e8253d0f6c34de88ea888ae
Gunwoong Kim <rjsdnd980316@naver.com>
2021-12-18 14:10:19 +0900

    feature: 미션 초기화

--- src/main/java/pairmatching/Controller/MatchingController.java
+++ src/main/java/pairmatching/Controller/MatchingController.java
@@ -1,16 +1,21 @@
 package pairmatching.Controller;
 
+import pairmatching.Model.Level;
+import pairmatching.Model.Mission;
+
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 public class MatchingController {
+    private static List<Mission> missions;
     public MatchingController() {
-
+        missions = new ArrayList<>();
     }
 
     public static void run() {
@@ -19,6 +24,21 @@ public class MatchingController {
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
+        missionInit();
+
+    }
+
+    private static void missionInit() {
+
+        missions.add(new Mission(Level.valueOf("LEVEL1"), "자동차경주"));
+        missions.add(new Mission(Level.valueOf("LEVEL1"), "로또"));
+        missions.add(new Mission(Level.valueOf("LEVEL1"), "숫자야구게임"));
+        missions.add(new Mission(Level.valueOf("LEVEL2"), "장바구니"));
+        missions.add(new Mission(Level.valueOf("LEVEL2"), "결제"));
+        missions.add(new Mission(Level.valueOf("LEVEL2"), "지하철노선도"));
+        missions.add(new Mission(Level.valueOf("LEVEL4"), "성능개선"));
+        missions.add(new Mission(Level.valueOf("LEVEL4"), "배포"));
+
     }
 
     private static void readCrewData() throws IOException {
---
+++ src/main/java/pairmatching/Model/Mission.java
@@ -0,0 +1,11 @@
+package pairmatching.Model;
+
+public class Mission {
+    private Level level;
+    private String name;
+
+    public Mission(Level level, String name) {
+        this.level = level;
+        this.name = name;
+    }
+}
