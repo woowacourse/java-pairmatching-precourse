0d9c510683daf0acc8757852e11550905405734a
JunHyeok.Bang <bangvkf3@gmail.com>
2021-12-18 14:19:16 +0900

    feat(Crews): 파일 읽고 인스턴스 생성하기 기능 추가

--- src/main/java/pairmatching/Application.java
+++ src/main/java/pairmatching/Application.java
@@ -1,7 +1,9 @@
 package pairmatching;
 
 public class Application {
-    public static void main(String[] args) {
-        // TODO 구현 진행
-    }
+	public static void main(String[] args) {
+		Crews frontEndCrews = Crews.of("src/main/resources/frontend-crew.md", Course.FRONTEND);
+		Crews backEndCrews = Crews.of("src/main/resources/backend-crew.md", Course.BACKEND);
+	}
+
 }
--- src/main/java/pairmatching/Crews.java
+++ src/main/java/pairmatching/Crews.java
@@ -1,11 +1,37 @@
 package pairmatching;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Crews {
 	private final List<Crew> crewList;
 
-	public Crews(List<Crew> crewList) {
+	private Crews(List<Crew> crewList) {
 		this.crewList = crewList;
+	}
+
+	public static Crews of(String pathName, Course course) {
+		List<Crew> crewList = new ArrayList<>();
+
+		try {
+			File file = new File(pathName);
+
+			Scanner scan = new Scanner(file);
+			while (scan.hasNextLine()) {
+				String name = scan.nextLine();
+				Crew crew = new Crew(course, name);
+				crewList.add(crew);
+
+				return new Crews(crewList);
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("파일을 읽을 수 없습니다.");
+		}
+		return null;
+
+		// TODO: null 처리, try with resource 구문?
 	}
 }
