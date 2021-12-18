4fa536f8c542af7a6cca4c6a8c4dfe1fb63d9cfc
GYUTAEKIM <kkt219a@gmail.com>
2021-12-18 14:54:15 +0900

    feat: 크루들의 이름과 과정을 통해 크루정보를 반환
    - 크루들의 이름과 과정을 통해 크루에 대한 정보를 생성하여 반환한다.

---
+++ src/main/java/pairmatching/domain/Crew.java
@@ -0,0 +1,15 @@
+package pairmatching.domain;
+
+public class Crew {
+	private final Course course;
+	private final String name;
+
+	public Crew(Course course, String name) {
+		this.course = course;
+		this.name = name;
+	}
+
+	public static Crew of(Course course, String name) {
+		return new Crew(course, name);
+	}
+}
