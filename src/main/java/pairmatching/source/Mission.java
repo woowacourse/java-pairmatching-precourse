2fe9021929adc6df8add3a2c60fcfb6e13eb6ecf
overnew <ehdls1303@gmail.com>
2021-12-18 15:07:09 +0900

    feat: Mission class 구현
    미션마다 매칭된 Pair를 리스트로 저장
    
    - Pair리스트 출력 기능
    - 크루원의 이름으로 매칭 기능
     - 홀, 짝에 따라 매칭 분리
    - 매칭된 여부를 저장

---
+++ src/main/java/pairmatching/source/Mission.java
@@ -0,0 +1,68 @@
+package pairmatching.source;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class Mission {
+    private final String name;
+    private boolean isMatched = false;
+    private List<Pair> missionPairs = new ArrayList<>();
+
+    public Mission(String name){
+        this.name = name;
+    }
+
+    public void matchingPair(List<String> crewNames, HashMap<String, Crew> crewMap ){
+        List<String> shuffledNames = Randoms.shuffle(crewNames);
+
+        if(shuffledNames.size() % 2 == 0){
+            matchEvenPair(shuffledNames,crewMap);
+            return;
+        }
+
+        matchOddPair(shuffledNames,crewMap);
+        isMatched = true;
+    }
+
+    private void matchOddPair(List<String> shuffledNames, HashMap<String, Crew> crewMap){
+        List<Crew> pairCrews  = new ArrayList<>();
+
+        for(int i=0; i< shuffledNames.size() -1 ; i+=2){
+            pairCrews = new ArrayList<>();
+            pairCrews.add(crewMap.get(shuffledNames.get(i)));
+            pairCrews.add(crewMap.get(shuffledNames.get(i+1)));
+            if(i < shuffledNames.size() -3){
+                missionPairs.add(new Pair(pairCrews));
+            }
+        }
+
+        pairCrews.add(crewMap.get(shuffledNames.get(shuffledNames.size() -1))); // 마지막 크루 삽입
+        missionPairs.add(new Pair(pairCrews));
+    }
+
+    private void matchEvenPair(List<String> shuffledNames, HashMap<String, Crew> crewMap){
+        List<Crew> pairCrews;
+
+        for(int i=0; i< shuffledNames.size(); i+=2){
+            pairCrews = new ArrayList<>();
+            pairCrews.add(crewMap.get(shuffledNames.get(i)));
+            pairCrews.add(crewMap.get(shuffledNames.get(i+1)));
+            missionPairs.add(new Pair(pairCrews));
+        }
+
+    }
+
+    public boolean getIsMatched(){
+        return isMatched;
+    }
+
+    public void printMatchedPairs(){
+
+        for(Pair currentPair : missionPairs){
+            System.out.println(currentPair);
+        }
+    }
+}
