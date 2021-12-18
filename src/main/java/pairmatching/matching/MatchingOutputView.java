f4d2e25699c7880d5b64dd0689e2f0ac92db8c5f
TaeHyeon Kim <kth990303@naver.com>
2021-12-18 13:52:13 +0900

    feat: 사용자에게 안내문을 출력하는 MatchingOutputView 클래스 구현

---
+++ src/main/java/pairmatching/matching/MatchingOutputView.java
@@ -0,0 +1,11 @@
+package pairmatching.matching;
+
+public class MatchingOutputView {
+    public static void seeMatchingResult(){
+        System.out.println("페어 매칭 결과입니다.");
+    }
+
+    public static void resetFinishedPrint() {
+        System.out.println("초기화 되었습니다. ");
+    }
+}
