02c7adac305f5d12fc9bd0479325093fd6eb559c
Hongdonggeon <ghd700@naver.com>
2021-12-18 15:15:26 +0900

    feat(outputview) : 결과를 출력하는 기능 추가

--- src/main/java/pairmatching/view/OutputView.java
+++ src/main/java/pairmatching/view/OutputView.java
@@ -1,2 +1,23 @@
-package pairmatching.view;public class OutputView {
+package pairmatching.view;
+
+import java.util.List;
+
+import pairmatching.domain.Crew;
+
+public class OutputView {
+	private static final String PAIR_MATCHING_RESULT = "페어 매칭 결과입니다.";
+	private static final String CREW_SPLIT_LINE = " : ";
+
+	public static void alarmPairMatchingResult() {
+		System.out.println();
+		System.out.println(PAIR_MATCHING_RESULT);
+	}
+
+	public static void showPairMatchingResult(List<Crew> crewList) {
+		StringBuilder stringBuilder = new StringBuilder();
+		for (Crew crew : crewList) {
+			stringBuilder.append(crew.getName()).append(CREW_SPLIT_LINE);
+		}
+		System.out.println(stringBuilder.toString());
+	}
 }
