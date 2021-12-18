e0d49c3166524311e43313b85aeccf463960f1d6
Jeong Satchel <bugod96@gmail.com>
2021-12-18 15:17:04 +0900

    feat(domain): Crew 도메인에서 같은 레벨을 함께 한 파트너들의 정보 관리

--- src/main/java/pairmatching/domain/Crew.java
+++ src/main/java/pairmatching/domain/Crew.java
@@ -1,11 +1,24 @@
 package pairmatching.domain;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class Crew {
     private Course course;
     private String name;
+    private HashMap<Level, List<Crew>> partnerHistory = new HashMap<>();
 
     public Crew(String name, Course course) {
         this.name = name;
         this.course = course;
+
+        for (Level level : Level.values()) {
+            partnerHistory.put(level, new ArrayList<>());
+        }
+    }
+
+    public void addPartnerHistory(Level level, Crew partner) {
+        partnerHistory.get(level).add(partner);
     }
 }
