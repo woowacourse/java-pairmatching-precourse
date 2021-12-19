6f95d29329a189123fd9fd6f2118929b97a7df49
skennetho <thrkd613@naver.com>
2021-12-19 20:27:44 +0900

    마무리 하지못했습니다. 죄송합니다...

--- src/main/java/pairmatching/view/UserView.java
+++ src/main/java/pairmatching/view/UserView.java
@@ -1,16 +1,12 @@
 package pairmatching.view;
 
-import java.util.List;
-
 import pairmatching.controller.mainmenu.Option;
-import pairmatching.domain.course.Course;
 
 import static camp.nextstep.edu.missionutils.Console.*;
 
 public class UserView {
     public static final String DOT = ".";
     public static final String MAIN_MENU = "기능을 선택하세요.";
-    public static final String DIVIDER = " | ";
 
     public void printMainMenu() {
         System.out.println(MAIN_MENU);
@@ -24,29 +20,11 @@ public class UserView {
         System.out.println(e.getMessage());
     }
 
-    public void printBlankLine() {
+    public void printBlankLine(){
         System.out.println();
     }
 
-    public String readFromUser() {
+    public String readFromUser(){
         return readLine();
-    }
-
-    public void printCourses(List<Course> courseList) {
-        StringBuilder sb = new StringBuilder("과정: ");
-        if(courseList.isEmpty()) {
-            System.out.println(sb);
-            return;
-        }
-        for(int i = 0 ; i< courseList.size() -1 ; i++) {
-            sb.append(courseList.get(i).toString());
-            sb.append(DIVIDER);
-        }
-        sb.append(courseList.get(courseList.size()-1).toString());
-        System.out.println(courseList);
-    }
-
-    public void printMessage(String message) {
-        System.out.println(message);
     }
 }
