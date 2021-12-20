8ed1b15d34dfa9fa07de8322f8f8e53bbff85f9d
0chil <0@chll.it>
2021-12-20 19:12:38 +0900

    refactor(Hint): 과정, 레벨, 미션 동적으로 표시

--- src/main/java/pairmatching/constant/Hint.java
+++ src/main/java/pairmatching/constant/Hint.java
@@ -1,5 +1,8 @@
 package pairmatching.constant;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public enum Hint {
 	HINT_FUNCTION_SELECTION("기능을 선택하세요.\n"
 		+ "1. 페어 매칭\n"
@@ -7,20 +10,22 @@ public enum Hint {
 		+ "3. 페어 초기화\n"
 		+ "Q. 종료"),
 	HINT_MATCH_INFO_INPUT("#############################################\n"
-		+ "과정: 백엔드 | 프론트엔드\n"
-		+ "미션:\n"
-		+ "  - 레벨1: 자동차경주 | 로또 | 숫자야구게임\n"
-		+ "  - 레벨2: 장바구니 | 결제 | 지하철노선도\n"
-		+ "  - 레벨3: \n"
-		+ "  - 레벨4: 성능개선 | 배포\n"
-		+ "  - 레벨5: \n"
-		+ "############################################\n"
+		+ "과정: "
+		+ Arrays.stream(Course.values()).map(Course::getName).collect(Collectors.joining(" | "))
+		+ "\n미션:\n"
+		+ Arrays.stream(Level.values())
+		.map(level -> String.format("  - %s: %s", level.getName(), String.join(" | ", MissionName.ofLevel(level))))
+		.collect(Collectors.joining("\n"))
+		+ "\n############################################\n"
 		+ "과정, 레벨, 미션을 선택하세요.\n"
 		+ "ex) 백엔드, 레벨1, 자동차경주"),
 	HINT_PAIR_MATCHING_RESULT("페어 매칭 결과입니다."),
 	HINT_CLEARED("초기화 되었습니다."),
 	HINT_REMATCHING("매칭 정보가 있습니다. 다시 매칭하시겠습니까?\n"
 		+ "네 | 아니오");
+
+	private static final String MISSION_FORMAT = "  - %s: %s\n";
+	private static final String COURSE_FORMAT = "과정: %s\n";
 
 	private final String hint;
 
