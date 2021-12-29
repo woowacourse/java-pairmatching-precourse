c2f523b8cc0221ff7a51838fb1250721da51cca9
qbang <vhfhddl0101@naver.com>
2021-12-29 23:20:22 +0900

    fix: 과정, 미션 정보 로직 수정 - printService에서 메시지를 만들지 않고, 만들어진 메시지를 가져오는 것으로 변경

--- src/main/java/pairmatching/LevelService.java
+++ src/main/java/pairmatching/LevelService.java
@@ -1,52 +1,38 @@
 package pairmatching;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import static pairmatching.Message.DIVIDE_LINE;
 
 public class LevelService {
     private ArrayList<String> levelList = new ArrayList();
-    private ArrayList<String>[] missionList = new ArrayList[5];
-    private static final String CAR_RACE = "자동차경주";
-    private static final String LOTTO = "로또";
-    private static final String BALL_BASEBALL = "숫자야구게임";
-    private static final String SHOPPING_BASKET = "장바구니";
-    private static final String PAYMENT = "결제";
-    private static final String SUBWAY = "지하철노선도";
-    private static final String IMPROVEMENT = "성능개선";
-    private static final String DEPLOYMENT = "배포";
-
-    LevelService() {
-        init();
-    }
+    private String[][] missionList = {{"자동차경주", "로또", "숫자야구게임"},
+            {"장바구니", "결제", "지하철노선도"}, {}, {"성능개선", "배포"}, {}};
 
     public ArrayList<String> getLevelList() {
         return levelList;
     }
 
-    public ArrayList<String>[] getMissionList() {
-        return missionList;
-    }
-
-    private void init() {
-        for (Level l : Level.values()) {
-            levelList.add(l.getName());
-        }
+    public String getMissionList() {
+        StringBuilder result = new StringBuilder();
+        result.append("미션:\n");
         for (int i = 0; i < missionList.length; i++) {
-            missionList[i] = new ArrayList<>();
+            result.append("  - 레벨:" + (i + 1) + " ");
+            result.append(Arrays.stream(missionList[i])
+                    .map(String::valueOf)
+                    .collect(Collectors.joining(" | ")));
+            result.append("\n");
         }
-        missionList[0].add(CAR_RACE);
-        missionList[0].add(LOTTO);
-        missionList[0].add(BALL_BASEBALL);
-        missionList[1].add(SHOPPING_BASKET);
-        missionList[1].add(PAYMENT);
-        missionList[1].add(SUBWAY);
-        missionList[3].add(IMPROVEMENT);
-        missionList[3].add(DEPLOYMENT);
+        result.append(DIVIDE_LINE);
+        return result.toString();
     }
 
     public void isExistMission(String level, String mission) throws IllegalArgumentException {
         int index = levelList.indexOf(level);
-        if(!missionList[index].contains(mission)) {
-            throw new IllegalArgumentException("[ERROR] 존재하지 않는 미션입니다.");
-        }
+//        if(!missionList[index].contains(mission)) {
+//            throw new IllegalArgumentException("[ERROR] 존재하지 않는 미션입니다.");
+//        }
     }
 }
