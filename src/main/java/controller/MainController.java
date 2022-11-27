7197b4e232933255baa3ec64eb02cae5cd55e432
junyong <jymoon1121@gmail.com>
2022-11-27 14:06:20 +0900

    feat(MainController): 기능 선택하는 입력 기능 구현

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -1,4 +1,18 @@
 package controller;
 
+import static view.InputView.getMainSelect;
+
 public class MainController {
+    private boolean flag = false;
+    private void mainSelect() {
+        while(!flag) {
+            try {
+                String input = getMainSelect();
+                flag = true;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+
+        }
+    }
 }
