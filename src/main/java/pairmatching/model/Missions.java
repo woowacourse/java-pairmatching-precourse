b4cb13959e66ae08317f386d3cb3336654598cfc
junyong <jymoon1121@gmail.com>
2022-11-27 16:39:26 +0900

    refactor(Missions): 타입 String에서 Mission으로 변경

--- src/main/java/pairmatching/model/Missions.java
+++ src/main/java/pairmatching/model/Missions.java
@@ -1,9 +1,10 @@
 package pairmatching.model;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Missions {
-    private static Map<Level, List<String>> missionMap;
+    private static Map<Level, List<Mission>> missionMap;
 
     public Missions() {
         missionMap = new TreeMap<>();
@@ -16,23 +17,26 @@ public class Missions {
         addInfo();
     }
     private void addInfo() {
-        missionMap.get(Level.LEVEL1).add("자동차경주");
-        missionMap.get(Level.LEVEL1).add("로또");
-        missionMap.get(Level.LEVEL1).add("숫자야구게임");
+        missionMap.get(Level.LEVEL1).add(Mission.CAR_RACE);
+        missionMap.get(Level.LEVEL1).add(Mission.LOTTO);
+        missionMap.get(Level.LEVEL1).add(Mission.BASEBALL);
 
-        missionMap.get(Level.LEVEL2).add("장바구니");
-        missionMap.get(Level.LEVEL2).add("결제");
-        missionMap.get(Level.LEVEL2).add("지하철노선도");
+        missionMap.get(Level.LEVEL2).add(Mission.BASKET);
+        missionMap.get(Level.LEVEL2).add(Mission.PAY);
+        missionMap.get(Level.LEVEL2).add(Mission.SUBWAY);
 
-        missionMap.get(Level.LEVEL4).add("성능개선");
-        missionMap.get(Level.LEVEL4).add("배포");
+        missionMap.get(Level.LEVEL4).add(Mission.IMPROVEMENT_PERFORMANCE);
+        missionMap.get(Level.LEVEL4).add(Mission.DEPLOYMENT);
     }
     public List<String> getValue(Level level) {
-        return Collections.unmodifiableList(missionMap.get(level));
+        return Collections.unmodifiableList(missionMap.get(level).stream()
+                .map(mission -> mission.getName())
+                .collect(Collectors.toList()));
     }
 
     public boolean isExist(String level, String mission) {
-        if (missionMap.get(Level.getEnumLevel(level)).contains(mission)) {
+        if (missionMap.get(Level.getEnumLevel(level))
+                .contains(Mission.getEnumMission(mission))) {
             return true;
         }
         return false;
