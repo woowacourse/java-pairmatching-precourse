14ba6333e931964ae74b2ecb96a3d3b61b969096
junyong <jymoon1121@gmail.com>
2022-11-27 17:31:17 +0900

    fix : 매칭 결과 출력하는 내용 수정

--- src/main/java/pairmatching/view/OutputView.java
+++ src/main/java/pairmatching/view/OutputView.java
@@ -15,17 +15,15 @@ public class OutputView {
             System.out.println(String.join(" | ", missions.getValue(level)));
         }
         System.out.println("#############################################");
-        System.out.println("과정, 레벨, 미션을 선택해주세요.");
-        System.out.println("ex) 백엔드, 레벨1, 자동차경주");
     }
 
     public static void printMatchPair(MatchInfo matchInfo) {
-        System.out.println("\n페어 매칭 결과입니다. ");
-        for(List<Crew> pair : matchInfo.getPairInfo()) {
-            for(int i = 0; i < pair.size()-1; i++) {
-                System.out.println(pair.get(i) + " : ");
+        System.out.println("\n페어 매칭 결과입니다.");
+        for (List<Crew> pair : matchInfo.getPairInfo()) {
+            for (int i = 0; i < pair.size() - 1; i++) {
+                System.out.print(pair.get(i) + " : ");
             }
-            System.out.println(pair.get(pair.size()-1));
+            System.out.println(pair.get(pair.size() - 1));
         }
     }
 }
