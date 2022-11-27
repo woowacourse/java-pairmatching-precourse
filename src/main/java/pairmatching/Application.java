6b6526f2364a4d7db5c69bc6e6ee0cea70fe0ad5
junyong <jymoon1121@gmail.com>
2022-11-27 14:20:50 +0900

    feat(Outputview): 과정과 미션에 대한 내용을 출력

--- src/main/java/pairmatching/Application.java
+++ src/main/java/pairmatching/Application.java
@@ -1,11 +1,14 @@
 package pairmatching;
 
 import controller.MainController;
+import view.OutputView;
 
 public class Application {
+    private static final OutputView outputview = new OutputView();
     public static void main(String[] args) {
         // TODO 구현 진행
         MainController mainController = new MainController();
         mainController.run();
+        outputview.printCourseAndMission();
     }
 }
---
+++ src/main/java/view/OutputView.java
@@ -0,0 +1,18 @@
+package view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class OutputView {
+    public void printCourseAndMission() {
+        System.out.println("#############################################\n" +
+                "과정: 백엔드 | 프론트엔드\n" +
+                "미션:\n" +
+                "  - 레벨1: 자동차경주 | 로또 | 숫자야구게임\n" +
+                "  - 레벨2: 장바구니 | 결제 | 지하철노선도\n" +
+                "  - 레벨3: \n" +
+                "  - 레벨4: 성능개선 | 배포 \n" +
+                "  - 레벨5: \n" +
+                "#############################################\n" +
+                "과정, 레벨, 미션을 선택하세요.");
+    }
+}
