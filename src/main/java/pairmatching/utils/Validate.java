3c1a578a1d782972761f965003eb93d76b537f4f
junyong <jymoon1121@gmail.com>
2022-11-27 16:15:42 +0900

    refactor: 함수명 이름 수정

--- src/main/java/pairmatching/utils/Validate.java
+++ src/main/java/pairmatching/utils/Validate.java
@@ -6,11 +6,34 @@ import java.util.List;
 public class Validate {
     private final static String ERROR_MESSAGE = "[ERROR]";
 
-    public static String validateCheckNumber(String input) {
+    public static String CheckMissionInfo(String input) {
         List<String> candidateNumAndQ = Arrays.asList(new String[]{"1", "2", "3", "Q"});
         if (!candidateNumAndQ.contains(input)) {
             throw new IllegalArgumentException(ERROR_MESSAGE + "보기에 있는 기능 선택만 할 수 있습니다.");
         }
         return input;
+    }
+
+    public static void checkEachInfo(String input) {
+        checkEmptyValue(input);
+        checkFrontBlank(input);
+    }
+
+    private static void checkEmptyValue(String input) {
+        if (input.isEmpty()) {
+            throw new IllegalArgumentException(ERROR_EMPTY_INPUT);
+        }
+    }
+
+    private static void checkFrontBlank(String input) {
+        if (input.charAt(0) == ' ') {
+            throw new IllegalArgumentException(ERROR_FRONT_BLANK);
+        }
+    }
+
+    public static void checkInfoCount(String[] input) {
+        if (input.length != 3) {
+            throw new IllegalArgumentException(ERROR_LESS_INFO);
+        }
     }
 }
