afd31a53e3bbd4f6c4077fd9ab7f6eb1d07a246c
dldbdud314 <dldbdud314@naver.com>
2022-12-09 15:31:27 +0900

    feat(PairMatcher, MatchingChoice): 페어 매칭 입력, 매칭 선택지 생성

--- src/main/java/pairmatching/controller/PairMatcher.java
+++ src/main/java/pairmatching/controller/PairMatcher.java
@@ -1,8 +1,14 @@
 package pairmatching.controller;
 
 import pairmatching.domain.Crews;
+import pairmatching.domain.MatchingChoice;
+import pairmatching.ui.InputView;
+import pairmatching.ui.OutputView;
 
 public class PairMatcher implements Controller {
+
+    private final InputView inputView = new InputView();
+    private final OutputView outputView = new OutputView();
     private final Crews crews;
 
     public PairMatcher(Crews crews) {
@@ -11,5 +17,17 @@ public class PairMatcher implements Controller {
 
     @Override
     public void execute() {
+        MatchingChoice matchingChoice = getPairMatchingInput();
+        // runPairMatcher(matchingChoice);
+    }
+
+    private MatchingChoice getPairMatchingInput() {
+        try {
+            String input = inputView.inputPairMatching();
+            return new MatchingChoice(input); // 맞는 input인가? && 이미 record에 존재하는건지
+        } catch (IllegalArgumentException exception) {
+            outputView.printErrorMessage(exception.getMessage());
+            return getPairMatchingInput();
+        }
     }
 }
---
+++ src/main/java/pairmatching/domain/MatchingChoice.java
@@ -0,0 +1,14 @@
+package pairmatching.domain;
+
+public class MatchingChoice {
+    private final Course course;
+    private final Level level;
+    private final String mission;
+
+    public MatchingChoice(String choiceInput) {
+        // parsing + validation
+        this.course = course;
+        this.level = level;
+        this.mission = mission;
+    }
+}
