182a5779f059ff5f81ebe8b4c667cb93d3e8b24c
dldbdud314 <dldbdud314@naver.com>
2022-12-09 14:18:00 +0900

    fix(InputView): 빠진 안내 멘트 추가

--- src/main/java/pairmatching/InputView.java
+++ src/main/java/pairmatching/InputView.java
@@ -4,9 +4,11 @@ import static pairmatching.InputReader.getUserInput;
 
 public class InputView {
     private static final String CHOOSE_MENU_OPENING = "기능을 선택하세요.";
+    private static final String MAIN_MENU = "1. 페어 매칭/n2. 페어 조회/n3. 페어 초기화/nQ. 종료";
 
     public String inputMenuChoice() {
         System.out.println(CHOOSE_MENU_OPENING);
+        System.out.println(MAIN_MENU);
         return getUserInput();
     }
 }
