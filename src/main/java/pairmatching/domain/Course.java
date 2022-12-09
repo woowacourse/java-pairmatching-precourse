4d270076cee8f1a466ed45b507ce03441b0acba0
MoonJeWoong <munjin0201@naver.com>
2022-12-09 19:06:26 +0900

    feat(domain): 과정에 대한 정보를 저장하고 관리한다.

--- src/main/java/pairmatching/domain/Course.java
+++ src/main/java/pairmatching/domain/Course.java
@@ -1,2 +1,27 @@
-package pairmatching.domain;public enum Course {
+package pairmatching.domain;
+
+import java.util.Arrays;
+
+public enum Course {
+    BACKEND("백엔드"),
+    FRONTEND("프론트엔드");
+
+    private static final String COURSE_ERROR_MESSAGE = "[ERROR] 과정명을 잘못 입력하셨습니다.";
+
+    private final String name;
+
+    Course(String name){
+        this.name = name;
+    }
+
+    public static Course ofName(String name){
+        return Arrays.stream(values())
+                .filter(course -> course.name.equals(name))
+                .findFirst()
+                .orElseThrow(()-> new IllegalArgumentException(COURSE_ERROR_MESSAGE));
+    }
+
+    public String getName(){
+        return name;
+    }
 }
