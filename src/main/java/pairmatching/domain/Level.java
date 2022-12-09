02ce8796a5893195a868abf3573a668142d01f87
MoonJeWoong <munjin0201@naver.com>
2022-12-09 19:06:52 +0900

    feat(domain): 레벨에 대한 정보를 저장하고 관리한다.

--- src/main/java/pairmatching/domain/Level.java
+++ src/main/java/pairmatching/domain/Level.java
@@ -1,2 +1,30 @@
-package pairmatching.domain;public enum Level {
+package pairmatching.domain;
+
+import java.util.Arrays;
+
+public enum Level {
+    LEVEL1("레벨1"),
+    LEVEL2("레벨2"),
+    LEVEL3("레벨3"),
+    LEVEL4("레벨4"),
+    LEVEL5("레벨5");
+
+    private static final String LEVEL_ERROR_MESSAGE = "[ERROR] 레벨 값은 1~5로 이루어져 있습니다."
+
+    private final String name;
+
+    public static Level ofName(String name){
+        return Arrays.stream(values())
+                .filter(level->level.name.equals(name))
+                .findFirst()
+                .orElseThrow();
+    }
+
+    Level(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
 }
