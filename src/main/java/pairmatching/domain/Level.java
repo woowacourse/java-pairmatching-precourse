fe1a7b30be01f9ec724a80598212ab9e49cc4b26
dldbdud314 <dldbdud314@naver.com>
2022-12-09 15:07:13 +0900

    fix(Level): 레벨 도메인에 미션 정보 추가

--- src/main/java/pairmatching/domain/Level.java
+++ src/main/java/pairmatching/domain/Level.java
@@ -1,16 +1,21 @@
 package pairmatching.domain;
 
+import java.util.Arrays;
+import java.util.List;
+
 public enum Level {
-    LEVEL1("레벨1"),
-    LEVEL2("레벨2"),
-    LEVEL3("레벨3"),
-    LEVEL4("레벨4"),
-    LEVEL5("레벨5");
+    LEVEL1("레벨1", Arrays.asList("자동차경주", "로또", "숫자야구게임")),
+    LEVEL2("레벨2", Arrays.asList("장바구니", "결제", "지하철노선도")),
+    LEVEL3("레벨3", null),
+    LEVEL4("레벨4", Arrays.asList("성능개선", "배포")),
+    LEVEL5("레벨5", null);
 
     private String name;
+    private List<String> missions;
 
-    Level(String name) {
+    Level(String name, List<String> missions) {
         this.name = name;
+        this.missions = missions;
     }
 
     // 추가 기능 구현
