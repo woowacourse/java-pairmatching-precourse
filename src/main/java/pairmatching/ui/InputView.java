af32ce14492da58995c5737b2a03a02a37d769b3
dldbdud314 <dldbdud314@naver.com>
2022-12-09 14:53:48 +0900

    fix(InputView): 잘못된 출력 수정

--- src/main/java/pairmatching/ui/InputView.java
+++ src/main/java/pairmatching/ui/InputView.java
@@ -4,7 +4,8 @@ import static pairmatching.ui.InputReader.getUserInput;
 
 public class InputView {
     private static final String CHOOSE_MENU_OPENING = "기능을 선택하세요.";
-    private static final String MAIN_MENU = "1. 페어 매칭/n2. 페어 조회/n3. 페어 초기화/nQ. 종료";
+    private static final String MAIN_MENU = "1. 페어 매칭\n" + "2. 페어 조회\n"
+            + "3. 페어 초기화\n" + "Q. 종료";
 
     public String inputMenuChoice() {
         System.out.println(CHOOSE_MENU_OPENING);
