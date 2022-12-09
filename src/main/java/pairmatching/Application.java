7d267599a8c6b9125bcc994e08d54c356adca772
dldbdud314 <dldbdud314@naver.com>
2022-12-09 13:08:31 +0900

    feat(InputView, OutputView, Controller): ui, controller 클래스 생성

--- src/main/java/pairmatching/Application.java
+++ src/main/java/pairmatching/Application.java
@@ -3,5 +3,7 @@ package pairmatching;
 public class Application {
     public static void main(String[] args) {
         // TODO 구현 진행
+        PairMatchingController pairMatchingController = new PairMatchingController();
+        pairMatchingController.execute();
     }
 }
---
+++ src/main/java/pairmatching/PairMatchingController.java
@@ -0,0 +1,14 @@
+package pairmatching;
+
+public class PairMatchingController {
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public PairMatchingController() {
+        inputView = new InputView();
+        outputView = new OutputView();
+    }
+
+    public void execute() {
+    }
+}
