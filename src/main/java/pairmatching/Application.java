eec48bc4c738a2914508ed227214599906b3257f
chanminmaxscaler <jcm04@naver.com>
2022-12-17 00:07:32 +0900

    chore: 클래스명 변경, 패키지 구조 변경을 반영한다

--- src/main/java/pairmatching/Application.java
+++ src/main/java/pairmatching/Application.java
@@ -1,12 +1,12 @@
 package pairmatching;
 
-import pairmatching.controller.Controller;
+import pairmatching.controller.MainController;
 import pairmatching.view.InputView;
 import pairmatching.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
-        Controller controller = new Controller(new InputView(), new OutputView());
-        controller.runMain();
+        MainController mainController = new MainController(new InputView(), new OutputView());
+        mainController.runMain();
     }
 }
