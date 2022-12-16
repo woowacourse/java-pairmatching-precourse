abef6370e4ac9c5abfdbbe8e928590df70cc8050
chanminmaxscaler <jcm04@naver.com>
2022-12-16 16:32:56 +0900

    feat: 애플리케이션 실행 기능을 구현한다

--- src/main/java/pairmatching/Application.java
+++ src/main/java/pairmatching/Application.java
@@ -1,7 +1,12 @@
 package pairmatching;
 
+import pairmatching.controller.Controller;
+import pairmatching.view.InputView;
+import pairmatching.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO 구현 진행
+        Controller controller = new Controller(new InputView(), new OutputView());
+        controller.runMain();
     }
 }
