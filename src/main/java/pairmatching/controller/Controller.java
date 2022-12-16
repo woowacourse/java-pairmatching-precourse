036c63d339cb8fd13c74a9d493257b941f3fe4f1
chanminmaxscaler <jcm04@naver.com>
2022-12-16 10:45:35 +0900

    feat: 기능 명령어 검증 기능을 구현한다
    Controller 객체는 InputView 객체로부터 기능 명령어 입력 값을 받는다.
    Command 객체를 통해 입력 값이 명령어가 맞는지 검증한다.
    명령어가 아닐 경우 오류를 발생시킨다.

---
+++ src/main/java/pairmatching/controller/Controller.java
@@ -0,0 +1,42 @@
+package pairmatching.controller;
+
+import pairmatching.domain.Command;
+import pairmatching.view.InputView;
+import pairmatching.view.OutputView;
+
+import java.util.function.Supplier;
+
+public class Controller {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public Controller(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void runMain() {
+        executeCommand();
+    }
+
+    private void executeCommand() {
+        Command command = readValidValueBy(this::readCommand);
+    }
+
+    private Command readCommand() {
+        outputView.printCommandGuide();
+        String inputCommand = inputView.readCommand();
+        return Command.valueOfCommand(inputCommand);
+    }
+
+    private <T> T readValidValueBy(Supplier<T> inputReader) {
+        while (true) {
+            try {
+                return inputReader.get();
+            } catch (IllegalArgumentException e) {
+                outputView.printErrorMessage(e.getMessage());
+            }
+        }
+    }
+}
---
+++ src/main/java/pairmatching/domain/Command.java
@@ -0,0 +1,27 @@
+package pairmatching.domain;
+
+import java.util.Arrays;
+
+public enum Command {
+    MATCHING("1"),
+    CHECKING("2"),
+    INITIALIZING("3"),
+    QUITTING("Q");
+
+    private final String command;
+
+    Command(String command) {
+        this.command = command;
+    }
+
+    public static Command valueOfCommand(String inputCommand) {
+        return Arrays.stream(values())
+                .filter(value -> inputCommand.equals(value.getCommand()))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException(String.format("[ERROR] %s는 기능이 아닙니다.", inputCommand)));
+    }
+
+    public String getCommand() {
+        return command;
+    }
+}
