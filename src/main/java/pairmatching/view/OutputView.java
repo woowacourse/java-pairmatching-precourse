a01b46cdc50026ce38de525eef4e2a01825e7a9d
inpink <inpink@kakao.com>
2023-11-27 20:45:03 +0900

    feat: 과정와 미션을 출력

---
+++ src/main/java/pairmatching/view/OutputView.java
@@ -0,0 +1,51 @@
+package pairmatching.view;
+
+import static pairmatching.messages.IOMessages.COURSE;
+import static pairmatching.messages.IOMessages.MISSION;
+import static pairmatching.messages.IOMessages.OUTPUT_DIVIDING_LINE;
+import static pairmatching.messages.IOMessages.OUTPUT_PAIRMATCHING_RESULT;
+
+import pairmatching.domain.entity.Course;
+import pairmatching.domain.entity.Level;
+import pairmatching.domain.entity.Mission;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class OutputView {
+
+    public void outputCourseMission() {
+        System.out.println(OUTPUT_DIVIDING_LINE.getMessage());
+
+        outputCourses();
+        outputMissions();
+
+        System.out.println(OUTPUT_DIVIDING_LINE.getMessage());
+    }
+
+    public void outputPairMatchingResult() {
+        System.out.println(OUTPUT_PAIRMATCHING_RESULT.getMessage());
+    }
+
+    private void outputCourses() {
+        String courses = Arrays.stream(Course.values())
+                .map(Course::getDescription)
+                .collect(Collectors.joining(" | "));
+        System.out.println(COURSE.getMessage() + ": " + courses);
+    }
+
+    private void outputMissions() {
+        System.out.println(MISSION.getMessage() + ": ");
+        Arrays.stream(Level.values())
+                .map(this::formatMissionsForLevel)
+                .forEach(System.out::println);
+    }
+
+    private String formatMissionsForLevel(Level level) {
+        String missions = level.getMissions()
+                .stream()
+                .map(Mission::getDescription)
+                .collect(Collectors.joining(" | "));
+
+        return "  - " + level.getDescription() + ": " + missions;
+    }
+}
