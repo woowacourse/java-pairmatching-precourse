a56846df4f1921b5432f43cd63ea6cc05354a270
inpink <inpink@kakao.com>
2023-11-27 19:06:18 +0900

    feat: 페어 매칭에 필요한 크루들의 이름을 파일 입출력을 통해 불러옴

---
+++ src/main/java/util/FileUtil.java
@@ -0,0 +1,12 @@
+package util;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class FileUtil {
+    public static List<String> readFileAsList(String filePath) throws IOException {
+        return Files.readAllLines(Paths.get(filePath));
+    }
+}
