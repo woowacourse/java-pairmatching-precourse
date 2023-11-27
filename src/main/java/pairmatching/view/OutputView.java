033616a678b3a2cddfe9d411a041e5ea7ee5c17a
jhon3242 <rldotnr2@gmail.com>
2023-11-27 20:10:22 +0900

    refactor : 출력 띄어쓰기 수정

--- src/main/java/pairmatching/view/OutputView.java
+++ src/main/java/pairmatching/view/OutputView.java
@@ -2,15 +2,16 @@ package pairmatching.view;
 
 import java.util.List;
 import pairmatching.Pair;
+import pairmatching.Pairs;
 
 public class OutputView {
     public static void printException(Exception error) {
         System.out.println("[ERROR] : " + error.getMessage());
     }
 
-    public static void printMatchingResult(List<Pair> matchingResult) {
+    public static void printMatchingResult(Pairs matchingResult) {
         System.out.println("페어 매칭 결과입니다.");
-        for (Pair pair : matchingResult) {
+        for (Pair pair : matchingResult.getPairs()) {
             String pairString = String.join(" : ", pair.getCrews());
             System.out.println(pairString);
         }
