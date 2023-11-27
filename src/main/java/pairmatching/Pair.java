885ceb413e330a80021f1f21866fda8fdd8c93fc
jhon3242 <rldotnr2@gmail.com>
2023-11-27 19:57:46 +0900

    refactor : Utils 객체 추가

--- src/main/java/pairmatching/Pair.java
+++ src/main/java/pairmatching/Pair.java
@@ -39,24 +39,6 @@ public class Pair {
             set.addAll(pair.crews);
             return set.size() == 3;
         }
-        return countDuplicateElements(crews, pair.crews) > 2;
-    }
-
-    private int countDuplicateElements(List<String> list1, List<String> list2) {
-        Map<String, Integer> countMap = new HashMap<>();
-
-        for (String str : list1) {
-            countMap.put(str, countMap.getOrDefault(str, 0) + 1);
-        }
-
-        int duplicateCount = 0;
-
-        for (String str : list2) {
-            if (countMap.containsKey(str) && countMap.get(str) > 0) {
-                duplicateCount++;
-            }
-        }
-
-        return duplicateCount;
+        return Utils.countDuplicateElements(crews, pair.crews) > 2;
     }
 }
