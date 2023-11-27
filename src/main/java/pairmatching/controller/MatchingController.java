96e9b6b87907a08e78f29c0d43a91e375d12e14c
zangsu <zangsu_@naver.com>
2023-11-27 18:51:01 +0900

    feat (controller) : Controller에서 전체 흐름관련 주석 작성

---
+++ src/main/java/pairmatching/controller/MatchingController.java
@@ -0,0 +1,37 @@
+package pairmatching.controller;
+
+import org.junit.jupiter.params.shadow.com.univocity.parsers.common.processor.OutputValueSwitch;
+import pairmatching.view.InputView;
+
+public class MatchingController {
+    private final InputView inputView = new InputView();
+    //OutputView
+
+    public MatchingController (){
+        resetPair();
+    }
+
+    public void createPair(){
+        //while
+            //입력을 받는다.
+            //매칭 정보가 있는지 확인
+            //if 매칭 정보가 없다면
+                //출력 - 생성된 페어 출력
+        //try
+    }
+
+    //매칭 정보가 있는지 확인
+    private boolean isMatchedMission(){
+
+    }
+
+    public void readPair(){
+        //매칭 정보가 있는지 확인
+        //있으면 매칭 정보 출력
+        //없으면 없다고 출력
+    }
+
+    public void resetPair(){
+        //모든 매칭 정보 리셋
+    }
+}
