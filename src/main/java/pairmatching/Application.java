8f61e8f692a37d41b1df04192d64226d0cf6aea7
zangsu <zangsu_@naver.com>
2023-11-27 19:01:14 +0900

    feat : 어플리케이션을 동작시킬 `Application.main()` 작성

--- src/main/java/pairmatching/Application.java
+++ src/main/java/pairmatching/Application.java
@@ -1,7 +1,28 @@
 package pairmatching;
 
+import pairmatching.controller.MatchingController;
+import pairmatching.view.InputView;
+
 public class Application {
+    private static final MatchingController matchingController = new MatchingController();
+    private static final InputView inputView = new InputView();
+
+
     public static void main(String[] args) {
-        // TODO 구현 진행
+        String menu;
+        do {
+            menu = inputView.getMenu();
+            if (menu.equals("1")) {
+                matchingController.createPair();
+            }
+            if (menu.equals("2")) {
+                matchingController.readPair();
+            }
+            if (menu.equals("3")) {
+                matchingController.resetPair();
+            }
+            //todo 어짜피 보장된 값만 받을거임
+
+        } while (!menu.equals("Q"));
     }
 }
