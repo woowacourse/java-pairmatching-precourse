8be899b17871f3932fa3516d32b5695a245f4ea7
MingyeomKim <mingyum119@naver.com>
2023-12-03 00:57:57 +0900

    feat: Application에서 함수 실행 코드 작성

--- src/main/java/pairmatching/Application.java
+++ src/main/java/pairmatching/Application.java
@@ -1,7 +1,23 @@
 package pairmatching;
 
+import org.xml.sax.SAXException;
+import pairmatching.controller.PairController;
+import pairmatching.domain.Crews;
+import pairmatching.domain.Matching;
+import pairmatching.service.CrewService;
+import pairmatching.service.MatchingService;
+
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO 구현 진행
+    public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
+        CrewService crewService = new CrewService(new Crews());
+        PairController pairController = new PairController(
+                crewService,
+                new MatchingService(crewService, new Matching())
+        );
+
+        pairController.run();
     }
 }
