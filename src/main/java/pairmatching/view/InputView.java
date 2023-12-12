84dfa0c4599dddcf157373ad3d6c10f39f70cc3e
kcw0360 <kcw0360@naver.com>
2023-12-12 14:58:53 +0900

    feat(InputView): 정보 입력 메소드 작성

--- src/main/java/pairmatching/view/InputView.java
+++ src/main/java/pairmatching/view/InputView.java
@@ -6,8 +6,12 @@ import pairmatching.domain.OptionCommand;
 public class InputView {
 
     public String readCommand() {
-        String input = input();
+        String input = input().trim();
         return OptionCommand.from(input).getCommand();
+    }
+
+    public String readInfo() {
+        return input();
     }
 
     private String input() {
