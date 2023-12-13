55a11a9eb56e42148f9c941d7ecf0303d94bf9dc
kcw0360 <kcw0360@naver.com>
2023-12-13 19:30:50 +0900

    feat(MatchingRepository): 페어 존재여부 메소드 작성

--- src/main/java/pairmatching/domain/MatchingRepository.java
+++ src/main/java/pairmatching/domain/MatchingRepository.java
@@ -2,6 +2,7 @@ package pairmatching.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import pairmatching.exception.ExceptionMessage;
 
 public class MatchingRepository {
@@ -14,6 +15,38 @@ public class MatchingRepository {
 
     public void add(Pairs pairs) {
         matchingRepository.add(pairs);
+    }
+
+    public boolean hasPairs(MatchingConditions conditions, Pairs makePairs) {
+        List<Pairs> sameLevelPairs = matchingRepository.stream()
+                .filter(pairs -> pairs.getMatchingConditions().getLevel() == conditions.getLevel())
+                .collect(Collectors.toList());
+
+        for (Pairs sameLevel : sameLevelPairs) {
+            if (checkPair(makePairs, sameLevel)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean checkPair(Pairs makePairs, Pairs sameLevel) {
+        for (Pair pair : sameLevel.getPairs()) {
+            if (hasPair(makePairs, pair)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean hasPair(Pairs makePairs, Pair pair) {
+        for (Pair makePair : makePairs.getPairs()) {
+            boolean result = pair.isCrews(makePair);
+            if (result) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public Pairs search(MatchingConditions conditions) {
